5abc2c1df47f508d879cc113083e7c2edacbbe1e
nicholavila <nicholavila1@outlook.com>
2023-05-18 12:29:00 +0000

    feat(login): social login

--- auth.config.ts
+++ auth.config.ts
@@ -3,6 +3,8 @@ import type { NextAuthOptions } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import Github from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
+import Discord from "next-auth/providers/discord";
+import Apple from "next-auth/providers/apple";
 
 import { LoginSchema } from "@/schemas";
 import { getUserByEmail } from "@/data/user";
@@ -10,12 +12,20 @@ import { getUserByEmail } from "@/data/user";
 export default {
   providers: [
     Google({
-      clientId: process.env.GOOGLE_CLIENT_ID || "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET || ""
     }),
     Github({
-      clientId: process.env.GITHUB_CLIENT_ID || "",
-      clientSecret: process.env.GITHUB_CLIENT_SECRET || ""
+      clientId: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.NEXT_PUBLIC_GITHUB_CLIENT_SECRET as string
+    }),
+    Discord({
+      clientId: process.env.NEXT_PUBLIC_DISCORD_CLIENT_ID || "",
+      clientSecret: process.env.NEXT_PUBLIC_DISCORD_CLIENT_SECRET || ""
+    }),
+    Apple({
+      clientId: process.env.NEXT_PUBLIC_APPLE_CLIENT_ID || "",
+      clientSecret: process.env.NEXT_PUBLIC_APPLE_CLIENT_SECRET || ""
     }),
     Credentials({
       credentials: {
