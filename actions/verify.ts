6868c41bb2429c5b92a3a88d29683e6c913a728a
nicholavila <nicholavila1@outlook.com>
2023-05-23 14:26:36 +0000

    feat(auth): add EmialVerified

--- actions/verify.ts
+++ actions/verify.ts
@@ -11,6 +11,7 @@ import { getUserByEmail } from "@/data/user";
 import { getTwoFactorTokenByEmail } from "@/data/two-factor-token";
 import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 import { sendVerificationEmail, sendTwoFactorTokenEmail } from "@/lib/mail";
+import { PutCommand, UpdateCommand } from "@aws-sdk/lib-dynamodb";
 import {
   generateVerificationToken,
   generateTwoFactorToken
@@ -35,7 +36,18 @@ export const verify = async (
   }
 
   if (!existingUser.emailVerified) {
-    // Verify
+		const command = new UpdateCommand({
+			TableName: process.env.DYNAMODB_TABLE_NAME,
+			Key: {
+				partitionId: 'user',
+				sortId: email
+			},
+			Item: {
+				emailVerified: time(),
+				// isTwoFactorEnabled: false
+			}
+		});
+		db.send(command);
   }
 
   return { success: "Data is Valid, Message Received!" };
