2fd78dee47636f4815888e07c3023acb237672af
nicholavila <nicholavila1@outlook.com>
2023-05-24 12:09:03 +0000

    fix(auth): getTwoFactorTokenByToken

--- data/two-factor-token.ts
+++ data/two-factor-token.ts
@@ -1,10 +1,20 @@
-import { db } from "@/lib/db";
+import { GetCommand, db } from "@/lib/db";
 
 export const getTwoFactorTokenByToken = async (token: string) => {
   try {
-    const twoFactorToken = await db.twoFactorToken.findUnique({
-      where: { token }
+    // const twoFactorToken = await db.twoFactorToken.findUnique({
+    //   where: { token }
+    // });
+    const command = new GetCommand({
+      TableName: process.env.DYNAMODB_TABLE_NAME,
+      Key: {
+        partition: "user",
+        token: token
+      }
     });
+
+    const response = await db.send(command);
+    const twoFactorToken = response.Item.twoFactorToken;
 
     return twoFactorToken;
   } catch {
