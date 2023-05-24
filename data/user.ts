a7df2add8c70076c1fb6650b8e2afba77470c20a
nicholavila <nicholavila1@outlook.com>
2023-05-24 13:52:58 +0000

    feat(db): getUserById

--- data/user.ts
+++ data/user.ts
@@ -23,12 +23,3 @@ export const getUserByEmail = async (email: string) => {
     return null;
   }
 };
-
-export const getUserById = async (id: string) => {
-  try {
-    const user = await db.user.findUnique({ where: { id } });
-    return user;
-  } catch {
-    return null;
-  }
-};
