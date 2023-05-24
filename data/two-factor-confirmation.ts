a62777ca0ff13bac7731f995ccd39307c3259e6b
nicholavila <nicholavila1@outlook.com>
2023-05-24 11:21:27 +0000

    fix(auth): Fix getTwoFactorConfirmation

--- data/two-factor-confirmation.ts
+++ data/two-factor-confirmation.ts
@@ -1,10 +1,20 @@
-import { db } from "@/lib/db";
+import { GetCommand, db } from "@/lib/db";
 
-export const getTwoFactorConfirmationByUserId = async (userId: string) => {
+export const getTwoFactorConfirmationByUserId = async (email: string) => {
   try {
-    const twoFactorConfirmation = await db.twoFactorConfirmation.findUnique({
-      where: { userId }
+    // const twoFactorConfirmation = await db.twoFactorConfirmation.findUnique({
+    //   where: { userId }
+    // });
+
+    const command = new GetCommand({
+      TableName: process.env.DYNAMODB_TABLE_NAME,
+      Key: {
+        partition: "user",
+        email: email
+      }
     });
+    const response = await db.send(command);
+    const twoFactorConfirmation = response.Item.token;
 
     return twoFactorConfirmation;
   } catch {
