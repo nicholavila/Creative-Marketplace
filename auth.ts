cfd6fcd540ea76c4556f48b50623a8d3aa95e9f7
nicholavila <nicholavila1@outlook.com>
2023-05-25 13:58:36 +0000

    fix(db): Fixing email issue

--- auth.ts
+++ auth.ts
@@ -59,15 +59,15 @@ export const { handlers } = NextAuth({
       // Allow OAuth without email verification
       if (account?.provider !== "credentials") return true;
 
-      const existingUser = await getUserById(user.id as string);
+      // const existingUser = await getUserById(user.id as string);
 
       // Prevent sign in without email verification
-      if (!existingUser?.emailVerified) return false;
+      // if (!existingUser?.emailVerified) return false;
 
       if (existingUser.isTwoFactorEnabled) {
-        const twoFactorConfirmation = await getTwoFactorConfirmationByUserId(
-          existingUser.id
-        );
+        // const twoFactorConfirmation = await getTwoFactorConfirmationByUserId(
+        //   existingUser.id
+        // );
 
         if (!twoFactorConfirmation) return false;
 
