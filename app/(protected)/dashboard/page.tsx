1d476949b5ba947efe611c8d19e153f007a978b3
nicholavila <nicholavila1@outlook.com>
2023-05-26 15:57:55 +0000

    feat(dashboard): add logo

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -1,15 +1,27 @@
 "use client"
 
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Card } from "@/components/ui/card"
+import { FaUser } from "react-icons/fa";
 
 const Dashboard = () => {
     return (
         <div className="h-full w-full flex flex-col items-center">
             <div className="w-full flex">
-                Header
+                <Avatar>
+                    <AvatarImage src="" />
+                        <AvatarFallback className="bg-sky-500">
+                            <FaUser className="text-white" />
+                        </AvatarFallback>
+                </Avatar>
             </div>
-            <div className="w-full flex">
-                Content
+            <div className="w-full flex flex-col items-center">
+                <p>
+                This site is scheduled to launch officially on May 15th
+                </p>
+                <p>
+                You can sign-up now as either a Seller, Affiliate or a User. 
+                </p>
             </div>
             <div className="w-full flex">
                 Footer
