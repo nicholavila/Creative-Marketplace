5e2c2c422ad5253734916b3ffb0e113b1b811125
nicholavila <nicholavila1@outlook.com>
2023-05-26 13:15:38 +0000

    Update(routing): Add dashboard page

---
+++ app/(protected)/dashboard/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { Card } from "@/components/ui/card"
+
+const DashBoard = () => {
+    return (
+        <div>
+            DashBoard
+        </div>
+    )
+}
+
+export default DashBoard;
