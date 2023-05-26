b794592a74eb10321477f37c2a7f9b8683adbb02
nicholavila <nicholavila1@outlook.com>
2023-05-26 14:38:15 +0000

    style(dashboard): build template for page

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -4,8 +4,16 @@ import { Card } from "@/components/ui/card"
 
 const DashBoard = () => {
     return (
-        <div>
-            DashBoard
+        <div className="h-full w-full flex flex-col items-center">
+            <div className="w-full flex">
+                Header
+            </div>
+            <div className="w-full flex">
+                Content
+            </div>
+            <div className="w-full flex">
+                DashBoard
+            </div>        
         </div>
     )
 }
