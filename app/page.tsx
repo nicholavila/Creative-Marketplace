141271db8380bbf93fc5055d0b901418173d9ebc
nicholavila <nicholavila1@outlook.com>
2023-05-29 13:05:37 +0000

    fix(style): fix style of homepage

--- app/page.tsx
+++ app/page.tsx
@@ -14,28 +14,29 @@ export default function Home() {
       <div className="w-full flex">
         <div className="w-full flex items-end gap-4">
           <img src="./logo.svg" width="60" height="60" />
-          <p className="text-4xl font-semibold text-white drop-shadow-md">Kre8tive Logo</p>
+          <p className="text-4xl font-semibold text-black drop-shadow-md">Kre8tive Logo</p>
         </div>
       </div>
-      <div className="w-full h-full flex justify-center pt-64">
-        <div className="space-y-6 text-center">
-        <h1
-          className={cn(
-            "text-4xl font-semibold text-white drop-shadow-md",
-            font.className
-          )}
-        >
-          ⌛ Coming Soon
-        </h1>
-        <p className="text-white text-lg">A simple authentication service</p>
-        <p className="text-white text-lg">A simple authentication service</p>
-        <div>
-          <SignupButton>
-            <Button variant="secondary" size="lg">
-              Sign-Up Now
-            </Button>
-          </SignupButton>
-        </div>
+      <div className="w-full h-full flex justify-center pt-48">
+        <div className="space-y-32 text-center">
+          <h1
+            className={cn(
+              "text-5xl font-semibold text-white drop-shadow-md",
+              font.className
+            )}
+          >
+            This site is scheduled to launch officially on May 15th
+          </h1>
+          <div className="space-y-6 text-center">
+            <p className="text-white text-lg">You can sign-up now as either a Seller, Affiliate or a User.</p>
+            <div>
+              <SignupButton>
+                <Button variant="secondary" size="lg">
+                  Sign-Up Now
+                </Button>
+              </SignupButton>
+            </div>
+          </div>
       </div>
       </div>
     </main>
