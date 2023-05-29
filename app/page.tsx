e60088516429f6cf09093f547f9ec803c76aead6
nicholavila <nicholavila1@outlook.com>
2023-05-29 10:59:46 +0000

    feat(homepage): add logo

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,13 @@ const font = Poppins({
 
 export default function Home() {
   return (
-    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+    <main className="flex h-full flex-col p-10 bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+      <div className="w-full flex">
+        <div className="">
+          <img src="./vercel.svg" />
+          <p>Kre8tive</p>
+        </div>
+      </div>
       <div className="space-y-6 text-center">
         <h1
           className={cn(
