79d451b5b1965d782278bdbde25db387f745b0a4
nicholavila <nicholavila1@outlook.com>
2023-05-30 11:53:34 +0000

    doc(db): comment customized adapter

--- adapter.ts
+++ adapter.ts
@@ -1,3 +1,5 @@
+type Awaitable<T> = T | PromiseLike<T>;
+
 import type {
   Adapter,
   AdapterAccount,
@@ -5,26 +7,34 @@ import type {
   AdapterSession
 } from "next-auth/adapters";
 
-type Awaitable<T> = T | PromiseLike<T>;
-
 export default {
-  async createUser(user: AdapterUser): Awaitable<AdapterUser> {
-    console.log("CREATE_USER", user);
-    return user;
-  },
-  async getUser(id: string): Awaitable<AdapterUser | null> {
-    console.log("GET_USER", id);
-    return null;
-  },
-  async getUserByEmail(email: string): Awaitable<AdapterUser | null> {
-    console.log("GET_USER_BY_EMAIL", email);
-    return null;
-  },
   async getUserByAccount(
     providerAccountId: Pick<AdapterAccount, "provider" | "providerAccountId">
   ): Awaitable<AdapterUser | null> {
-    console.log("GET_USER_BY_ACCOUNT", providerAccountId);
+    console.log("getUserByAccount", providerAccountId);
+    // { providerAccountId: '126603430', provider: 'github' }
     return null;
+  },
+  async getUser(id: string): Awaitable<AdapterUser | null> {
+    console.log("getUser", id);
+    // 8876a5dc-7248-4682-a255-5412417373dd
+    return null;
+  },
+  async getUserByEmail(email: string): Awaitable<AdapterUser | null> {
+    console.log("getUserByEmail", email);
+    // sacreddevking@gmail.com
+    return null;
+  },
+  async createUser(user: AdapterUser): Awaitable<AdapterUser> {
+    // console.log("createUser", user);
+    // {
+    //   id: '2e79e340-fb52-4e65-a535-a656734b5b3f',
+    //   name: 'DevKing',
+    //   email: 'sacreddevking@gmail.com',
+    //   image: 'https://avatars.githubusercontent.com/u/126603430?v=4',
+    //   emailVerified: null
+    // }
+    return user;
   },
   async updateUser(
     user: Partial<AdapterUser> & Pick<AdapterUser, "id">
