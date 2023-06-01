92879fb7373cb83c67bb76e3b4448a666f4ff47d
nicholavila <nicholavila1@outlook.com>
2023-06-01 13:12:26 +0000

    update(login): suspend email verification

--- actions/register.ts
+++ actions/register.ts
@@ -5,8 +5,8 @@ import bcrypt from "bcryptjs";
 
 import { RegisterSchema } from "@/schemas";
 import { createUser, getUserByEmail } from "@/data/user";
-import { sendVerificationEmail } from "@/lib/mail";
-import { generateVerificationToken } from "@/lib/tokens";
+// import { sendVerificationEmail } from "@/lib/mail";
+// import { generateVerificationToken } from "@/lib/tokens";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validateFields = RegisterSchema.safeParse(values);
