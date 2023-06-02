5742a2f523af8b0841ec4149613c0974175088ac
nicholavila <nicholavila1@outlook.com>
2023-06-02 13:48:22 +0000

    track resend response

--- lib/mail.ts
+++ lib/mail.ts
@@ -18,12 +18,14 @@ export const sendVerificationEmail = async (email: string, token: string) => {
 export const sendPasswordResetEmail = async (email: string, token: string) => {
   const resetLink = `${domain}/auth/new-password?token=${token}`;
 
-  await resend.emails.send({
+  const response = await resend.emails.send({
     from: "mail@auth-masterclass-tutorial.com",
     to: email,
     subject: "Reset your password",
     html: `<p>Click <a href="${resetLink}">here</a> to reset password.</p>`
   });
+
+  console.log("__sendPasswordResetEmail__RESPONSE", response);
 };
 
 export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
