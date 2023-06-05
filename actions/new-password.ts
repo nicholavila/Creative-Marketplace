e89cb97adc83de850b75885ec4048f0c6ca8f72e
nicholavila <nicholavila1@outlook.com>
2023-06-05 09:48:03 +0000

    get new password

--- actions/new-password.ts
+++ actions/new-password.ts
@@ -5,13 +5,14 @@ import bcrypt from "bcryptjs";
 
 import { NewPasswordSchema } from "@/schemas";
 import { getPasswordResetTokenByToken } from "@/data/password-reset-token";
-import { getUserByEmail } from "@/data/user";
+import { getUserByEmail, getUserById } from "@/data/user";
 
 export const newPassword = async (
   values: z.infer<typeof NewPasswordSchema>,
   token?: string | null
 ) => {
-  if (!token) {
+  if (!token || token.length !== 36 * 2) {
+    // twice of length of uuidv4
     return { error: "Missing token!" };
   }
 
@@ -20,36 +21,41 @@ export const newPassword = async (
     return { error: "Invalid fields!" };
   }
 
+  const userId = token.slice(0, 36); // length of uuidv4
+  const tokenValue = token.slice(36); // length of uuidv4
+
+  const existingUser = getUserById(userId);
+
   const { password } = validatedFields.data;
 
-  const existingToken = await getPasswordResetTokenByToken(token);
+  // const existingToken = await getPasswordResetTokenByToken(token);
 
-  if (!existingToken) {
-    return { error: "Invalid token!" };
-  }
+  // if (!existingToken) {
+  //   return { error: "Invalid token!" };
+  // }
 
-  const hasExpired = new Date(existingToken.expires) < new Date();
+  // const hasExpired = new Date(existingToken.expires) < new Date();
 
-  if (hasExpired) {
-    return { error: "Token has expired!" };
-  }
+  // if (hasExpired) {
+  //   return { error: "Token has expired!" };
+  // }
 
-  const existingUser = await getUserByEmail(existingToken.email);
+  // const existingUser = await getUserByEmail(existingToken.email);
 
-  if (!existingUser) {
-    return { error: "Email does not exist!" };
-  }
+  // if (!existingUser) {
+  //   return { error: "Email does not exist!" };
+  // }
 
-  const hashedPassword = await bcrypt.hash(password, 10);
+  // const hashedPassword = await bcrypt.hash(password, 10);
 
-  await db.user.update({
-    where: { id: existingUser.id },
-    data: { password: hashedPassword }
-  });
+  // await db.user.update({
+  //   where: { id: existingUser.id },
+  //   data: { password: hashedPassword }
+  // });
 
-  await db.passwordResetToken.delete({
-    where: { id: existingToken.id }
-  });
+  // await db.passwordResetToken.delete({
+  //   where: { id: existingToken.id }
+  // });
 
   return { success: "Password updated!" };
 };
