3f24afd1315238d0c9aaebcb284cd3eaf85e841d
nicholavila <nicholavila1@outlook.com>
2023-06-06 12:49:55 +0000

    adjust item names on sidebar

--- app/(protected)/user/_components/sidebar.tsx
+++ app/(protected)/user/_components/sidebar.tsx
@@ -9,26 +9,27 @@ import { Separator } from "@/components/ui/separator";
 
 export const Sdiebar = () => {
 	const pathname = usePathname();
+	console.log(pathname);
 
 	const sidebarItems = [
 		{
-			name: "Dashboard",
-			path: "/dashboard"
+			name: "Profile",
+			path: "/user/profile"
 		},
 		{
-			name: "Profile",
-			path: "/profile"
+			name: "Account",
+			path: "/user/account"
 		},
 		{
 			name: "Settings",
-			path: "/settings"
+			path: "/user/settings"
 		}
 	];
 
 	return (
 		<section className="w-96 h-full flex flex-col gap-y-2 items-start">
 			{sidebarItems.map((item) => (
-				<Button asChild variant="ghost">
+				<Button asChild variant="link">
 					<Link href="/login">Login</Link>
 				</Button>
 			))}
