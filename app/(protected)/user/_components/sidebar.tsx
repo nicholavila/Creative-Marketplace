a2ce1333b36c614e84b7b6a5911d8631855d2f7a
nicholavila <nicholavila1@outlook.com>
2023-06-06 11:09:23 +0000

    add menu item in sidebar

--- app/(protected)/user/_components/sidebar.tsx
+++ app/(protected)/user/_components/sidebar.tsx
@@ -6,13 +6,16 @@ import { usePathname } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { UserButton } from "@/components/auth/user-button";
 import { Separator } from "@/components/ui/separator";
+import { NavigationMenuItem, NavigationMenuLink, navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
 
 export const Sdiebar = () => {
 	const pathname = usePathname();
 
 	return (
 		<section className="bg-secondary w-96 h-full">
-
+			<Link href="/docs" legacyBehavior passHref>
+				Documentation
+			</Link>
 		</section>
 	);
 };
