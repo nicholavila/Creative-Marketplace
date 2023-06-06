ed60085bf3e268b77821b5b1613276245dbf7d89
nicholavila <nicholavila1@outlook.com>
2023-06-06 13:37:16 +0000

    fix style of sidebar items

--- app/(protected)/user/_components/sidebar.tsx
+++ app/(protected)/user/_components/sidebar.tsx
@@ -29,11 +29,9 @@ export const Sdiebar = () => {
 		<section className="w-96 h-full flex flex-col gap-y-2 items-start">
 			{sidebarItems.map((item) =>
 				pathName === item.path ? (
-					<div key={item.name} className="w-full flex text-left">
-						<Link href={item.path} className="font-semibold">
-							{item.name}
-						</Link>
-					</div>
+					<Button key={item.name} asChild variant="secondary" className="w-full bg-gray-200 justify-start">
+						<Link href={item.path} className="font-semibold">{item.name}</Link>
+					</Button>
 				) : (
 					<Button key={item.name} asChild variant="link">
 						<Link href={item.path} className="font-semibold">{item.name}</Link>
