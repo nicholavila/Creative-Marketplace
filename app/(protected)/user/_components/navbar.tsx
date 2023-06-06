ded0a138f569e157b0870c576e47369d31a3ae73
nicholavila <nicholavila1@outlook.com>
2023-06-06 15:09:28 +0000

    fix tags for proper ones

--- app/(protected)/user/_components/navbar.tsx
+++ app/(protected)/user/_components/navbar.tsx
@@ -13,12 +13,12 @@ export const Navbar = () => {
   return (
     <nav className="w-full pb-6 flex flex-col gap-y-6">
       <div className="flex justify-between items-center w-full">
-        <div className="flex flex-col gap-y-2">
+        <header className="flex flex-col gap-y-2">
           <p className="text-3xl text-black font-medium drop-shadow-md">Settings</p>
           <p className="text-md text-gray-600">
             Manage your account settings set preferences.
           </p>
-        </div>
+        </header>
         <UserButton />
       </div>
       <Separator className="h-[1px]" />
--- app/(protected)/user/page.tsx
+++ app/(protected)/user/page.tsx
@@ -11,7 +11,12 @@ const font = Poppins({
 export default function Home() {
 	return (
 		<main className="w-full h-full p-6">
-
+			<header className="flex flex-col gap-y-2">
+				<p className="text-3xl text-black font-medium drop-shadow-md">Settings</p>
+				<p className="text-md text-gray-600">
+					Manage your account settings set preferences.
+				</p>
+			</header>
 		</main>
 	);
 }
