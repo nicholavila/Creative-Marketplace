bcdd12da6568e394cb081e4c499bdb95a82731e1
nicholavila <nicholavila1@outlook.com>
2023-06-07 09:31:30 +0000

    remove comment

--- app/(protected)/user/page.tsx
+++ app/(protected)/user/page.tsx
@@ -130,29 +130,3 @@ export default function Profile() {
 		</main>
 	);
 };
-
-// import { Poppins } from "next/font/google";
-// import { cn } from "@/lib/utils";
-// import { Button } from "@/components/ui/button";
-// import { SignupButton } from "@/components/auth/signup-button";
-// import { Separator } from "@/components/ui/separator";
-
-// const font = Poppins({
-// 	subsets: ["latin"],
-// 	weight: ["600"]
-// });
-
-// export default function Home() {
-// 	return (
-// 		<main className="w-full h-full px-8 flex flex-col gap-y-5">
-// 			<header className="flex flex-col gap-y-1">
-// 				<p className="text-xl text-black font-medium drop-shadow-md">Profile</p>
-// 				<p className="text-sm text-gray-600">
-// 					This is how others will see you on the site
-// 				</p>
-// 			</header>
-// 			<Separator />
-
-// 		</main>
-// 	);
-// }
