0c760dd09c4c1349de9d7d41a6197756fc3445ca
nicholavila <nicholavila1@outlook.com>
2023-06-08 14:38:15 +0000

    add account information page

--- app/(protected)/user/account/page.tsx
+++ app/(protected)/user/account/page.tsx
@@ -1,12 +1,19 @@
 "use client";
 
-import { UserInfo } from "@/components/user-info";
-import { useCurrentUser } from "@/hooks/use-current-user";
+import { Separator } from "@/components/ui/separator";
 
-const ClientPage = () => {
-  const user = useCurrentUser();
-
-  return <UserInfo label="📱 Client component" user={user} />;
+const Account = () => {
+  return (
+    <main className="w-full h-full pl-8 flex flex-col gap-y-5">
+      <header className="flex flex-col gap-y-1">
+        <p className="text-xl text-black font-medium drop-shadow-md">Account</p>
+        <p className="text-sm text-gray-600">
+          Your account information here
+        </p>
+      </header>
+      <Separator />
+    </main>
+  )
 };
 
-export default ClientPage;
+export default Account;
