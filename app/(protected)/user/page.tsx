44515676e6992798b4d592887b7393e04740f460
nicholavila <nicholavila1@outlook.com>
2023-06-08 10:27:19 +0000

    fix profile schema

--- app/(protected)/user/page.tsx
+++ app/(protected)/user/page.tsx
@@ -33,8 +33,8 @@ export default function Profile() {
 	const form = useForm<z.infer<typeof ProfileSchema>>({
 		resolver: zodResolver(ProfileSchema),
 		defaultValues: {
-			email: "",
-			password: ""
+			username: "",
+			bio: ""
 		}
 	});
 
