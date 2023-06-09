cad2c8cfe9364e50deabc139e5940a6535aef20d
nicholavila <nicholavila1@outlook.com>
2023-06-09 13:47:48 +0000

    fix sign out

--- auth.ts
+++ auth.ts
@@ -2,7 +2,7 @@ import NextAuth from "next-auth";
 import authConfig from "@/auth.config";
 import adapter from "@/adapter";
 
-export const { handlers, auth, signIn } = NextAuth({
+export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter,
   session: { strategy: "jwt" },
   // pages: {
