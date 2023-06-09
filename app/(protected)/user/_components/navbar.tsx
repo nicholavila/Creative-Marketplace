6214beb582ba859d510f65d1319a17d08ba8b3e4
nicholavila <nicholavila1@outlook.com>
2023-06-09 10:17:20 +0000

    add user settings

--- app/(protected)/user/_components/navbar.tsx
+++ app/(protected)/user/_components/navbar.tsx
@@ -8,13 +8,11 @@ import { UserButton } from "@/components/auth/user-button";
 import { Separator } from "@/components/ui/separator";
 
 export const Navbar = () => {
-  const pathname = usePathname();
-
   return (
     <nav className="w-full pb-6 flex flex-col gap-y-6">
       <div className="flex justify-between items-center w-full">
         <header className="flex flex-col gap-y-2">
-          <p className="text-3xl text-black font-medium drop-shadow-md">Settings</p>
+          <p className="text-3xl text-black font-medium drop-shadow-md">User Settings</p>
           <p className="text-md text-gray-600">
             Manage your account settings set preferences.
           </p>
