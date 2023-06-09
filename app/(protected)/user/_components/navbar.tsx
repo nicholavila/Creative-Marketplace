2b4dbfc8e72827fc669ae35e0bdaab8850fd6238
nicholavila <nicholavila1@outlook.com>
2023-06-09 11:30:54 +0000

    remove UserButton from user settings navbar

--- app/(protected)/user/_components/navbar.tsx
+++ app/(protected)/user/_components/navbar.tsx
@@ -6,14 +6,11 @@ import { Separator } from "@/components/ui/separator";
 export const Navbar = () => {
   return (
     <nav className="w-full pb-6 flex flex-col gap-y-6">
-      <div className="flex justify-between items-center w-full">
-        <header className="flex flex-col gap-y-2">
-          <p className="text-3xl text-black font-medium drop-shadow-md">User Settings</p>
-          <p className="text-md text-gray-600">
-            Manage your account settings set preferences.
-          </p>
-        </header>
-        <UserButton />
+      <div className="flex flex-col gap-y-2">
+        <p className="text-3xl text-black font-medium drop-shadow-md">User Settings</p>
+        <p className="text-md text-gray-600">
+          Manage your account settings set preferences.
+        </p>
       </div>
       <Separator className="h-[1px]" />
     </nav >
