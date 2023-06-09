8dc7bb4ad6ec4c6d079bce0941d627f74f4ed136
nicholavila <nicholavila1@outlook.com>
2023-06-09 09:49:09 +0000

    modify global navBar

--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -1,27 +1,17 @@
 "use client";
 
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-
-import { Button } from "@/components/ui/button";
 import { UserButton } from "@/components/auth/user-button";
-import { Separator } from "@/components/ui/separator";
 
 export const Navbar = () => {
-  const pathname = usePathname();
-
   return (
-    <nav className="w-full pb-6 flex flex-col gap-y-6">
-      <div className="flex justify-between items-center w-full">
-        <header className="flex flex-col gap-y-2">
-          <p className="text-3xl text-black font-medium drop-shadow-md">Settings</p>
-          <p className="text-md text-gray-600">
-            Manage your account settings set preferences.
-          </p>
-        </header>
-        <UserButton />
+    <nav className="w-full pb-6 flex items-center">
+      <div className="w-full flex">
+        <div className="w-full flex items-end gap-4">
+          <img src="./logo.svg" width="60" height="60" />
+          <p className="text-4xl font-semibold text-black drop-shadow-md">Kre8tive</p>
+        </div>
       </div>
-      <Separator className="h-[1px]" />
-    </nav >
+      <UserButton />
+    </nav>
   );
 };
