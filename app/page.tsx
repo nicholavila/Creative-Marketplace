45196603799de2ec6e66de10dea6e9d16fb1f71c
nicholavila <nicholavila1@outlook.com>
2023-06-12 12:46:41 +0000

    chore: update homepage

--- app/page.tsx
+++ app/page.tsx
@@ -17,20 +17,25 @@ const Home = () => {
 
   return (
     <main className="w-full h-full">
-      <div className="space-y-24 text-center pt-24">
+      <div className="flex flex-col items-center gap-y-24 pt-24">
         <h1 className={cn("text-5xl font-semibold text-black drop-shadow-md", font.className)}>
           This site is scheduled to launch officially on May 15th
         </h1>
-        <div className='space-y-4'>
+        <div className='space-y-4 text-center'>
           <p className="text-black text-lg">
             //** Fields to let them know creative features will be coming and to give them the ability to Opt In to those programs. **//
           </p>
           <p className="text-black text-lg">You can sign-up now as either a Seller or a User!</p>
         </div>
-        <div>
+        <div className='flex gap-x-4'>
           <SignupButton>
             <Button variant="default" size="lg">
-              Sign-Up Now
+              Sign-Up Now as a Creator
+            </Button>
+          </SignupButton>
+          <SignupButton>
+            <Button variant="default" size="lg">
+              Sign-Up Now as a User
             </Button>
           </SignupButton>
         </div>
