290de6448fb010e5dd5bd19e48306fb127f23c6f
nicholavila <nicholavila1@outlook.com>
2023-06-13 09:35:15 +0000

    feat: add creator registration page

---
+++ app/auth/_components/navbar.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { Separator } from "@/components/ui/separator";
+
+interface PropsParams {
+    title: string;
+    content: string
+}
+
+export const Navbar = ({ title, content }: PropsParams) => {
+    return (
+        <nav className="w-full pb-6 flex flex-col gap-y-6">
+            <div className="flex flex-col gap-y-2">
+                <p className="text-3xl text-black font-medium drop-shadow-md">{title}</p>
+                <p className="text-md text-gray-600">{content}</p>
+            </div>
+            <Separator className="h-[1px]" />
+        </nav >
+    );
+};
--- app/auth/signup-creator/page.tsx
+++ app/auth/signup-creator/page.tsx
@@ -2,10 +2,12 @@ import { Poppins } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { LoginButton } from "@/components/auth/login-button";
+import { Navbar } from "../_components/navbar";
 
 export default function SignUpCreator() {
 	return (
-		<main className="flex h-full flex-col items-center justify-center">
+		<main className="flex h-full flex-col">
+			<Navbar title="Creator Registration" content="Register as a creator" />
 		</main>
 	);
 }
