fe7c1b897439c7d15982b9f48c16d3564c04b150
nicholavila <nicholavila1@outlook.com>
2023-06-13 10:17:38 +0000

    feat: add affiliate, user registration page

---
+++ app/auth/signup-affiliate/page.tsx
@@ -0,0 +1,13 @@
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { LoginButton } from "@/components/auth/login-button";
+import { Navbar } from "../_components/navbar";
+
+export default function SignUpAffiliate() {
+	return (
+		<main className="flex h-full flex-col">
+			<Navbar title="Affiliate Registration" content="Register as an affiliate" />
+		</main>
+	);
+}
---
+++ app/auth/signup-user/page.tsx
@@ -0,0 +1,13 @@
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { LoginButton } from "@/components/auth/login-button";
+import { Navbar } from "../_components/navbar";
+
+export default function SignUpUser() {
+	return (
+		<main className="flex h-full flex-col">
+			<Navbar title="User Registration" content="Register as a user" />
+		</main>
+	);
+}
