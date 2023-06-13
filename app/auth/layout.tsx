bd2cff17ce75d524a210709daafd710ee22f55e2
nicholavila <nicholavila1@outlook.com>
2023-06-13 08:40:38 +0000

    fix: fix auth layout

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full flex items-center justify-center">
+    <div className="w-full h-full">
       {children}
     </div>
   );
--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -10,20 +10,15 @@ const font = Poppins({
 
 export default function Home() {
   return (
-    <main className="flex h-full flex-col items-center justify-center">
+    <main className="w-full h-full flex items-center justify-center">
       <div className="space-y-6 text-center">
-        <h1
-          className={cn(
-            "text-6xl font-semibold text-white drop-shadow-md",
-            font.className
-          )}
-        >
+        <h1 className={cn("text-6xl font-semibold text-black drop-shadow-md", font.className)}>
           🔐 Auth
         </h1>
-        <p className="text-white text-lg">A simple authentication service</p>
+        <p className="text-black text-lg">A simple authentication service</p>
         <div>
           <LoginButton>
-            <Button variant="secondary" size="lg">
+            <Button variant="default" size="lg">
               Sign in
             </Button>
           </LoginButton>
