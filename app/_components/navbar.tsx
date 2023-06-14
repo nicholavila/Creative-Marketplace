8f31abda31403909a70097327b3d41124ba4c1f4
nicholavila <nicholavila1@outlook.com>
2023-06-14 11:23:16 +0000

    feat: set navbar sticky

--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -8,7 +8,7 @@ import { FaSignal } from "react-icons/fa";
 
 export const Navbar = () => {
   return (
-    <nav className="w-full pb-6 flex items-end">
+    <nav className="w-full pb-6 flex items-end bg-gray-50 sticky top-6 box-border z-50">
       <div className="w-full flex">
         <div className="w-full flex items-end gap-4">
           <Avatar className="w-16 h-16 rounded-xl">
