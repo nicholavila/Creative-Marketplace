98652cee456a7ca89d6a4e0db7892543f53210e0
nicholavila <nicholavila1@outlook.com>
2023-06-15 11:24:25 +0000

    rearrange: add s3 client to lib directory

---
+++ lib/s3.ts
@@ -0,0 +1,11 @@
+import { S3Client } from "@aws-sdk/client-s3";
+
+const s3 = new S3Client({
+  region: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID as string,
+  credentials: {
+    accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID as string,
+    secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY as string
+  }
+});
+
+export default s3;
