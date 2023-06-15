2a3769cf74173fda979ccfb66904a1ca9132b2da
nicholavila <nicholavila1@outlook.com>
2023-06-15 12:03:58 +0000

    feat: add putObjectCommand

--- data/aws.ts
+++ data/aws.ts
@@ -1,3 +1,17 @@
-const bucketName = process.env.NEXT_PUBLIC_AWS_AWS_BUCKET_NAME;
+import s3 from "@/lib/s3";
+import { PutObjectCommand } from "@aws-sdk/client-s3";
 
-export const uploadFileToS3 = async () => {};
+const Bucket = process.env.NEXT_PUBLIC_AWS_AWS_BUCKET_NAME;
+
+export const uploadFileToS3 = async (file: File) => {
+  const Body = (await file.arrayBuffer()) as Buffer;
+
+  const command = new PutObjectCommand({
+    Bucket,
+    Key: file.name,
+    Body
+  });
+
+  try {
+  } catch (error) {}
+};
