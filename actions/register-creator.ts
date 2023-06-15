5202716b079c55fa3887f153810b073c94d9598d
nicholavila <nicholavila1@outlook.com>
2023-06-15 13:29:32 +0000

    rearrange: add regiser-creator

---
+++ actions/register-creator.ts
@@ -0,0 +1,21 @@
+"use server";
+
+import { z } from "zod";
+import bcrypt from "bcryptjs";
+
+import { RegisterSchema, CreatorRegisterSchema } from "@/schemas/auth";
+import { createUser, getUserByEmail } from "@/data/user";
+import { sendVerificationEmail } from "@/lib/mail";
+// import { generateVerificationToken } from "@/lib/tokens";
+
+export const registerCreator = async (
+  values: z.infer<typeof CreatorRegisterSchema>
+) => {
+  const validateFields = CreatorRegisterSchema.safeParse(values);
+  if (!validateFields.success) {
+    return { error: "Server Says Fields are Invalid!" };
+  }
+
+  const { avatar } = validateFields.data;
+  console.log("__registerCreator", avatar);
+};
