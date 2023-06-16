c759199ec1289f9a5f76d06e907cd7e561d28c28
nicholavila <nicholavila1@outlook.com>
2023-06-16 13:59:23 +0000

    fix: fix variable setting on s3Client

--- lib/s3.ts
+++ lib/s3.ts
@@ -1,7 +1,7 @@
 import { S3Client } from "@aws-sdk/client-s3";
 
 const s3 = new S3Client({
-  region: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID as string,
+  region: process.env.NEXT_PUBLIC_AWS_REGION as string,
   credentials: {
     accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID as string,
     secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY as string
