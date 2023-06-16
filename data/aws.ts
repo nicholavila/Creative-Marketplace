614c96fa2606770e711062820c9b63de223adde5
nicholavila <nicholavila1@outlook.com>
2023-06-16 14:17:01 +0000

    fix: aws s3 settings

--- data/aws.ts
+++ data/aws.ts
@@ -1,14 +1,16 @@
 import s3 from "@/lib/s3";
 import { PutObjectCommand } from "@aws-sdk/client-s3";
 
-const Bucket = process.env.NEXT_PUBLIC_AWS_AWS_BUCKET_NAME;
+const Bucket = process.env.NEXT_PUBLIC_AWS_BUCKET_NAME;
 
 export const uploadFileToS3 = async (file: File) => {
   const Body = (await file.arrayBuffer()) as Buffer;
 
+  console.log("__uploadFileToS3__FILE_NAME", file.name, Body);
+
   const command = new PutObjectCommand({
     Bucket,
-    Key: file.name,
+    Key: `${file.name}`,
     Body,
     ContentType: "image/jpg"
   });
@@ -16,5 +18,7 @@ export const uploadFileToS3 = async (file: File) => {
   try {
     const response = await s3.send(command);
     console.log("__uploadFileToS3__RESPONSE", response);
-  } catch (error) {}
+  } catch (error) {
+    console.log("__uploadFileToS3__ERROR", error);
+  }
 };
