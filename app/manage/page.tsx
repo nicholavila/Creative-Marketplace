087d8d71a84e27330e7946c7d452a30863e6e23d
nicholavila <nicholavila1@outlook.com>
2023-06-19 12:10:58 +0000

    rearrange: add management page

---
+++ app/manage/page.tsx
@@ -0,0 +1,13 @@
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { LoginButton } from "@/components/auth/login-button";
+import { Navbar } from "../_components/navbar";
+
+export default function Manage() {
+    return (
+        <main className="w-full h-full flex flex-col">
+            Management Page
+        </main>
+    );
+}
