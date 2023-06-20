1d86010cafd56ad3b44e5c0055766cb451854b1d
nicholavila <nicholavila1@outlook.com>
2023-06-20 15:17:07 +0000

    rearrange: add paypal for route

---
+++ app/api/payment/paypal/capture_order/route.ts
@@ -0,0 +1,32 @@
+import client from "@/lib/paypal";
+import paypal from "@paypal/checkout-server-sdk";
+import { NextResponse } from "next/server";
+
+type Params = {
+  order_id?: string;
+};
+
+export const POST = async (req: Request, context: { params: Params }) => {
+  if (!context.params.order_id) {
+    return NextResponse.json(
+      { success: false, message: "Please provide order ID" },
+      { status: 400 }
+    );
+  }
+
+  const orderId = context.params.order_id;
+  const PaypalClient = client();
+  const request = new paypal.orders.OrdersCaptureRequest(orderId);
+  request.requestBody({});
+  const response = await PaypalClient.execute(request);
+  if (!response) {
+    return NextResponse.json(
+      { success: false, message: "Some Error occured" },
+      { status: 500 }
+    );
+  }
+
+  // Your custom code to update order status
+
+  return NextResponse.json({ success: true });
+};
