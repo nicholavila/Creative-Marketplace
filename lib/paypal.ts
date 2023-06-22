b8a97babaec89da063a3d997e6a99991f7e88cd3
nicholavila <nicholavila1@outlook.com>
2023-06-22 11:46:20 +0000

    refactor: refactor paypal library

--- lib/paypal.ts
+++ lib/paypal.ts
@@ -1,16 +1,16 @@
-import checkoutNodeJssdk from "@paypal/checkout-server-sdk";
+import paypal from "@paypal/checkout-server-sdk";
 
 const configureEnvironment = function () {
   const clientId = process.env.PAYPAL_CLIENT_ID as string;
   const clientSecret = process.env.PAYPAL_CLIENT_SECRET as string;
 
   return process.env.NODE_ENV === "production"
-    ? new checkoutNodeJssdk.core.LiveEnvironment(clientId, clientSecret)
-    : new checkoutNodeJssdk.core.SandboxEnvironment(clientId, clientSecret);
+    ? new paypal.core.LiveEnvironment(clientId, clientSecret)
+    : new paypal.core.SandboxEnvironment(clientId, clientSecret);
 };
 
-const client = function () {
-  return new checkoutNodeJssdk.core.PayPalHttpClient(configureEnvironment());
+const paypalClient = function () {
+  return new paypal.core.PayPalHttpClient(configureEnvironment());
 };
 
-export default client;
+export default paypalClient;
