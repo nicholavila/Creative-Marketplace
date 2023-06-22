cf88c3e953b4e3d5b83b5834c95855af1d60226b
nicholavila <nicholavila1@outlook.com>
2023-06-22 10:43:09 +0000

    rearrange: add payment button

---
+++ components/payment/payment-button.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { PaymentForm } from "@/components/payment/payment-form";
+
+interface PaymentButtonProps {
+	children: React.ReactNode;
+	mode?: "modal" | "redirect";
+	asChild?: boolean;
+}
+
+export const PaymentButton = ({
+	children,
+	mode = "modal",
+	asChild
+}: PaymentButtonProps) => {
+	const router = useRouter();
+
+	const onClick = () => {
+		router.push("/payment/pay");    // payment page
+	};
+
+	if (mode === "modal") {
+		return (
+			<Dialog>
+				<DialogTrigger asChild={asChild}>{children}</DialogTrigger>
+				<DialogContent className="p-0 w-auto bg-transparent border-none">
+					<PaymentForm />
+				</DialogContent>
+			</Dialog>
+		);
+	}
+
+	return (
+		<span onClick={onClick} className="cursor-pointer">
+			{children}
+		</span>
+	);
+};
