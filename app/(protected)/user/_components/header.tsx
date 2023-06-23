f95759074b762058059b4f804aa793371c297b82
nicholavila <nicholavila1@outlook.com>
2023-06-23 10:56:01 +0000

    rearrange: add sub header to user settings page

---
+++ app/(protected)/user/_components/header.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+type HeaderParams = {
+    title: string;
+    content: string;
+}
+
+export const Header = ({ title, content }: HeaderParams) => {
+    return (
+        <header className="flex flex-col gap-y-1">
+            <p className="text-xl text-black font-medium drop-shadow-md">{title}</p>
+            <p className="text-sm text-gray-600">{content}</p>
+        </header>
+    );
+};
