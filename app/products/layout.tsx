0e341a1dc97758910d5c6f5bcce77fe49a975d3a
nicholavila <nicholavila1@outlook.com>
2023-06-23 08:38:45 +0000

    refactor: remove navbar from layout for products

--- app/products/layout.tsx
+++ app/products/layout.tsx
@@ -1,10 +1,8 @@
 import * as React from "react";
-import { Navbar } from "./_components/navbar";
 
 const ProductsLayout = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<div className="w-full">
-			<Navbar title="Products" content="You can select buy your favorite" />
 			{children}
 		</div>
 	);
