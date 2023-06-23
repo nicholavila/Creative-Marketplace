3c3ec972e3728601dea9f69c733c26f923baa803
nicholavila <nicholavila1@outlook.com>
2023-06-23 14:22:07 +0000

    rearragnge:add paymentform to subscription

--- app/(protected)/user/subscription/page.tsx
+++ app/(protected)/user/subscription/page.tsx
@@ -2,12 +2,14 @@
 
 import { Separator } from "@/components/ui/separator";
 import { Header } from "../_components/header";
+import { PaymentForm } from "@/components/payment/payment-form";
 
 const Settings = () => {
   return (
     <main className="w-full h-full pl-8 flex flex-col gap-y-5">
       <Header title="Subscription" content="Select your plan for a membership" />
       <Separator />
+      <PaymentForm />
     </main>
   )
 };
