2b0ef8c4cc3381f5d5b078a9b6bf49e6a80364d8
nicholavila <nicholavila1@outlook.com>
2023-06-23 12:44:27 +0000

    refactor: simplify customer, subscriptioin settings page

--- app/(protected)/user/customer/page.tsx
+++ app/(protected)/user/customer/page.tsx
@@ -1,16 +1,12 @@
 "use client";
 
 import { Separator } from "@/components/ui/separator";
+import { Header } from "../_components/header";
 
 const Customer = () => {
   return (
     <main className="w-full h-full pl-8 flex flex-col gap-y-5">
-      <header className="flex flex-col gap-y-1">
-        <p className="text-xl text-black font-medium drop-shadow-md">Customer</p>
-        <p className="text-sm text-gray-600">
-          Select how this site you want to serve you
-        </p>
-      </header>
+      <Header title="Customer" content="Select how this site you want to serve you" />
       <Separator />
     </main>
   )
--- app/(protected)/user/subscription/page.tsx
+++ app/(protected)/user/subscription/page.tsx
@@ -1,16 +1,12 @@
 "use client";
 
 import { Separator } from "@/components/ui/separator";
+import { Header } from "../_components/header";
 
 const Settings = () => {
   return (
     <main className="w-full h-full pl-8 flex flex-col gap-y-5">
-      <header className="flex flex-col gap-y-1">
-        <p className="text-xl text-black font-medium drop-shadow-md">Subscription</p>
-        <p className="text-sm text-gray-600">
-          Select your plan for a membership
-        </p>
-      </header>
+      <Header title="Subscription" content="Select your plan for a membership" />
       <Separator />
     </main>
   )
