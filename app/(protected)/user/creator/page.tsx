421e323364e6ad12fce61f8d6a6c581bdc5e0ff1
nicholavila <nicholavila1@outlook.com>
2023-06-23 12:31:20 +0000

    refactor: restructure custome creator and affiliate settings page

--- app/(protected)/user/creator/page.tsx
+++ app/(protected)/user/creator/page.tsx
@@ -1,16 +1,12 @@
 "use client";
 
 import { Separator } from "@/components/ui/separator";
+import { Header } from "../_components/header";
 
 const Creator = () => {
   return (
     <main className="w-full h-full pl-8 flex flex-col gap-y-5">
-      <header className="flex flex-col gap-y-1">
-        <p className="text-xl text-black font-medium drop-shadow-md">Creator</p>
-        <p className="text-sm text-gray-600">
-          Select how this site you want to serve you
-        </p>
-      </header>
+      <Header title="Creator" content="Select how this site you want to serve you" />
       <Separator />
     </main>
   )
