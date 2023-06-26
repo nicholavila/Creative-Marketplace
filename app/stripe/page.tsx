9a7fc9aa75e331410df8b8914abad6db8f7010dc
nicholavila <nicholavila1@outlook.com>
2023-06-26 10:07:41 +0000

    refactor: adjust elements on stripe

--- app/stripe/page.tsx
+++ app/stripe/page.tsx
@@ -2,13 +2,14 @@
 
 import { Elements } from "@stripe/react-stripe-js"
 import { loadStripe } from "@stripe/stripe-js"
+import CheckoutForm from "./CheckoutForm";
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string);
 
 export default function StripePage() {
     return (
         <Elements stripe={stripePromise}>
-            Stripe
+            <CheckoutForm price={10} />
         </Elements>
     )
 }
