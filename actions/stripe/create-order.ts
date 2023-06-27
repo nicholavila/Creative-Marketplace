1ad8980ab2e820f7cb350f45efed6ea580faa2b6
nicholavila <nicholavila1@outlook.com>
2023-06-27 08:56:51 +0000

    refactor: adjust create-order for stripe payment

--- actions/stripe/create-order.ts
+++ actions/stripe/create-order.ts
@@ -13,7 +13,7 @@ export const createOrder = async (params: OrderType) => {
     const taxRate = await stripe.taxRates.create({
       display_name: "Sales Tax",
       description: "Goods & Service Tax",
-      jurisdiction: "AU",
+      jurisdiction: "US - CA",
       percentage: 10,
       inclusive: false
     });
@@ -21,7 +21,7 @@ export const createOrder = async (params: OrderType) => {
     // https://docs.stripe.com/api/checkout/sessions/create
     let payment = await stripe.checkout.sessions.create({
       mode: "payment",
-      client_reference_id: "user._id",
+      client_reference_id: "_user._id",
       payment_method_types: ["card"],
       // customer: customer.id,
       customer_email: "sacreddevking@gmail.com",
@@ -30,10 +30,10 @@ export const createOrder = async (params: OrderType) => {
           price_data: {
             currency: "USD",
             product_data: {
-              name: "Your Product Name",
-              description: "Description of your Product",
+              name: "Product Name",
+              description: "Description of Product",
               // description: membership.description + " - " + moment().add(membership.period, 'months').format("YYYY-MM-DD HH:mm:ss"),
-              images: ["https://answersheet.au/logo.svg"]
+              images: ["https://answersheet.au/logo.svg"] // image would be shown on payment page
             },
             unit_amount: Number(Math.round((tempPrice / 1.1) * 100))
           },
@@ -43,7 +43,7 @@ export const createOrder = async (params: OrderType) => {
       ],
       // success_url: `${process.env.NEXT_PUBLIC_APP_URL}/private-membership`,
       // cancel_url: `${process.env.NEXT_PUBLIC_APP_URL}/private-membership`
-      success_url: `${process.env.NEXT_PUBLIC_APP_URL}${params.redirectUrl}?gateway=stripe&session_id={CHECKOUT_SESSION_ID}&history_id=membershipHistory_id`,
+      success_url: `${process.env.NEXT_PUBLIC_APP_URL}${params.redirectUrl}?gateway=stripe&session_id={CHECKOUT_SESSION_ID}&history_id=history_id`,
       cancel_url: `${process.env.NEXT_PUBLIC_APP_URL}${params.redirectUrl}?gateway=cancelled`
     });
 
