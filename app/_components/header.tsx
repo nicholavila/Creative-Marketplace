451d4720828fef25abbb503b6f223a84e22d5467
nicholavila <nicholavila1@outlook.com>
2023-06-28 14:22:47 +0000

    chore: update header

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -7,34 +7,34 @@ import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import { FaSignal } from "react-icons/fa";
 
 import {
-    NavigationMenu,
-    NavigationMenuContent,
-    NavigationMenuItem,
-    NavigationMenuLink,
-    NavigationMenuList,
-    NavigationMenuTrigger,
-    navigationMenuTriggerStyle,
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu"
 import React from "react";
 
 export const Header = () => {
-    return (
-        <nav className="w-full pb-6 flex items-end bg-gray-50 sticky top-6 box-border z-50">
-            <div className="w-full flex">
-                <div className="w-full flex items-end gap-4">
-                    <Avatar className="w-16 h-16 rounded-xl">
-                        <AvatarImage src="/logo.svg" />
-                        <AvatarFallback className="bg-sky-500">
-                            <FaSignal className="text-white" />
-                        </AvatarFallback>
-                    </Avatar>
+  return (
+    <nav className="w-full pb-6 flex items-end bg-gray-50 sticky top-6 box-border z-50">
+      <div className="w-full flex">
+        <div className="w-full flex items-end gap-4">
+          {/* <Avatar className="w-16 h-16 rounded-xl">
+            <AvatarImage src="/logo.svg" />
+            <AvatarFallback className="bg-sky-500">
+              <FaSignal className="text-white" />
+            </AvatarFallback>
+          </Avatar> */}
 
-                    <Link href="/">
-                        <p className="text-4xl font-semibold text-black drop-shadow-md">Kre8tive</p>
-                    </Link>
-                </div>
-            </div>
-            <UserButton />
-        </nav>
-    );
+          <Link href="/">
+            <p className="text-4xl font-semibold text-black drop-shadow-md">Kre8tive</p>
+          </Link>
+        </div>
+      </div>
+      <UserButton />
+    </nav>
+  );
 };
