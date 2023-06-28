5d33d2ed3da81975af8caaac911cc12307dba659
nicholavila <nicholavila1@outlook.com>
2023-06-28 09:59:42 +0000

    refactor: adjust route for file upload

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -7,20 +7,14 @@ type RequestType = {
 
 export const POST = async (req: RequestType) => {
   try {
-    console.log("__POST__UPLOAD__ROUTE__");
-
     const formData = await req.formData();
     const file = formData.get("file");
-
-    console.log("__POST__UPLOAD__ROUTE__", file);
 
     if (!file) {
       return NextResponse.json({ error: "File is required" }, { status: 400 });
     }
 
-    const response = await uploadFileToS3(file); // upload connection issue
-
-    console.log("__POST__UPLOAD__ROUTE__", response);
+    const response = await uploadFileToS3(file);
 
     if (response.success) {
       return NextResponse.json(response, { status: 200 });
@@ -28,6 +22,6 @@ export const POST = async (req: RequestType) => {
       return NextResponse.json(response, { status: 500 });
     }
   } catch (error) {
-    return NextResponse.json({ error }, { status: 500 });
+    return NextResponse.json({ success: false, error }, { status: 500 });
   }
 };
