dd3ae68c794f3220cbb0a35b55151cd57a462fc5
nicholavila <nicholavila1@outlook.com>
2023-06-28 09:31:19 +0000

    refactor: adjust upload-image

--- actions/upload-image.ts
+++ actions/upload-image.ts
@@ -4,11 +4,7 @@ import { PutObjectCommand } from "@aws-sdk/client-s3";
 const Bucket = process.env.AWS_BUCKET_NAME;
 
 export const uploadFileToS3 = async (file: File) => {
-  console.log("__upload__File__to__S3", file);
-
   const Body = Buffer.from(await file.arrayBuffer());
-
-  console.log("__upload__File__to__S3");
 
   const command = new PutObjectCommand({
     Bucket,
@@ -19,10 +15,8 @@ export const uploadFileToS3 = async (file: File) => {
 
   try {
     const response = await s3Client.send(command);
-    console.log("__uploadFileToS3__RESPONSE", response);
-    return { success: true, response }; // Need to be updated with the actual file name got from response
+    return { success: true, response };
   } catch (error) {
-    console.log("__uploadFileToS3__ERROR", error);
     return { success: false, error };
   }
 };
