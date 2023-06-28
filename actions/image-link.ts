f7c97d04143c2db89fed8fdd15aefa9b1dab150f
nicholavila <nicholavila1@outlook.com>
2023-06-28 12:39:22 +0000

    chore: get image link from aws s3

--- actions/image-link.ts
+++ actions/image-link.ts
@@ -6,7 +6,7 @@ import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 const Bucket = process.env.AWS_BUCKET_NAME;
 
-export const downloadFileFromS3 = async (keyName: string) => {
+export const getS3ImageLink = async (keyName: string) => {
   const command = new GetObjectCommand({
     Bucket,
     Key: keyName
@@ -14,8 +14,6 @@ export const downloadFileFromS3 = async (keyName: string) => {
 
   try {
     const response = await getSignedUrl(s3Client, command, { expiresIn: 3600 });
-    console.log("RESPONSE", response);
-
     return { success: true, response };
   } catch (error) {
     return { success: false, error };
