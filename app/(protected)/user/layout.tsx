10bd562ab2de6edbf866ce22e00287ab42330e74
nicholavila <nicholavila1@outlook.com>
2023-06-29 14:53:41 +0000

    style: fix style of user layout

--- app/(protected)/user/layout.tsx
+++ app/(protected)/user/layout.tsx
@@ -7,7 +7,7 @@ interface ProtectedLayoutProps {
 
 const ProtectedLayout = ({ children }: ProtectedLayoutProps) => {
   return (
-    <div className="w-full flex flex-col">
+    <div className="w-full flex flex-col pt-6">
       <Navbar />
       <div className="w-full flex">
         <Sdiebar />
