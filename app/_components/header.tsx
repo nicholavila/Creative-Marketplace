5dc0e0c5fdbd8153c3e89892c8548601eeba0816
nicholavila <nicholavila1@outlook.com>
2023-06-29 09:04:23 +0000

    chore: remove unused modules in header

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -3,18 +3,6 @@
 import { UserButton } from "@/components/auth/user-button";
 import Link from "next/link";
 
-import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
-import { FaSignal } from "react-icons/fa";
-
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  navigationMenuTriggerStyle,
-} from "@/components/ui/navigation-menu"
 import React from "react";
 import { Navbar } from "./navbar";
 
