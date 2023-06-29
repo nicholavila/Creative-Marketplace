487ce543f1698d4dfc8e59f24c23a87b3b4913c0
nicholavila <nicholavila1@outlook.com>
2023-06-29 08:21:49 +0000

    refactor: refactor navbar items

--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -1,10 +1,6 @@
 "use client";
 
-import { UserButton } from "@/components/auth/user-button";
 import Link from "next/link";
-
-import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
-import { FaSignal } from "react-icons/fa";
 
 import {
   NavigationMenu,
@@ -16,35 +12,74 @@ import {
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu"
 import React from "react";
+import { ListItem } from "@/components/list-item";
+
+type MenuItem = {
+  title: string;
+  href: string;
+};
 
 export const Navbar = () => {
+  const menuList: MenuItem[] = [
+    {
+      title: "Fonts",
+      href: "/product/Fonts"
+    },
+    {
+      title: "PNGs",
+      href: "/product/PNGs"
+    },
+    {
+      title: "Graphics",
+      href: "/product/Graphics"
+    },
+    {
+      title: "3D",
+      href: "/product/3D"
+    },
+    {
+      title: "Spark",
+      href: "/product/Spark"
+    },
+    {
+      title: "Crafts",
+      href: "/product/Crafts"
+    },
+    {
+      title: "Templates",
+      href: "/product/Templates"
+    },
+    {
+      title: "Themes",
+      href: "/product/Themes"
+    },
+    {
+      title: "Illustrations",
+      href: "/product/Illustrations"
+    },
+    {
+      title: "Mockups",
+      href: "/product/Mockups"
+    },
+    {
+      title: "Videos",
+      href: "/product/Videos"
+    },
+  ]
   return (
     <nav className="w-full flex items-end py-2 sticky top-6 z-50 box-border border-b-[1px]">
       <NavigationMenu>
         <NavigationMenuList>
-          <NavigationMenuItem>
-            <Link href="/dashboard" legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Dashboard
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Link href="/products" legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Products
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Link href="/manage" legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Manage
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
+          {menuList.map((menuItem, index) => (
+            <NavigationMenuItem key={menuItem.title}>
+              <NavigationMenuTrigger>{menuItem.title}</NavigationMenuTrigger>
+              <NavigationMenuContent>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          ))}
         </NavigationMenuList>
       </NavigationMenu>
     </nav>
   );
 };
+
