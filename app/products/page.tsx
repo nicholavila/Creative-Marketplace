e3ce678a6f13d0265971bd96f9ab03777352ec7a
nicholavila <nicholavila1@outlook.com>
2023-06-30 10:16:12 +0000

    refactor: populate with dummy data on products page

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -2,44 +2,140 @@ import { Poppins } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { LoginButton } from "@/components/auth/login-button";
-import { ProductItem } from "./_components/productitem";
+import { ProductItem } from "@/components/profile/productitem";
 import { Navbar } from "./_components/navbar";
 
 export default function Products() {
-	const products = [
-		{
-			imgPath: '/images/1.jpg',
-			title: 'Product 1',
-			description: 'Product Description 1',
-			price: 100
-		},
-		{
-			imgPath: '/images/2.jpg',
-			title: 'Product 2',
-			description: 'Product Description 2',
-			price: 100
-		},
-		{
-			imgPath: '/images/3.jpg',
-			title: 'Product 3',
-			description: 'Product Description 3',
-			price: 100
-		}
-	]
+  const products = [
+    {
+      imgPath: '/images/1.jpg',
+      title: 'Product 1',
+      description: 'Product Description 1',
+      price: 100
+    },
+    {
+      imgPath: '/images/2.jpg',
+      title: 'Product 2',
+      description: 'Product Description 2',
+      price: 100
+    },
+    {
+      imgPath: '/images/3.jpg',
+      title: 'Product 3',
+      description: 'Product Description 3',
+      price: 100
+    },
+    {
+      imgPath: '/images/1.jpg',
+      title: 'Product 1',
+      description: 'Product Description 1',
+      price: 100
+    },
+    {
+      imgPath: '/images/2.jpg',
+      title: 'Product 2',
+      description: 'Product Description 2',
+      price: 100
+    },
+    {
+      imgPath: '/images/3.jpg',
+      title: 'Product 3',
+      description: 'Product Description 3',
+      price: 100
+    },
+    {
+      imgPath: '/images/1.jpg',
+      title: 'Product 1',
+      description: 'Product Description 1',
+      price: 100
+    },
+    {
+      imgPath: '/images/2.jpg',
+      title: 'Product 2',
+      description: 'Product Description 2',
+      price: 100
+    },
+    {
+      imgPath: '/images/3.jpg',
+      title: 'Product 3',
+      description: 'Product Description 3',
+      price: 100
+    },
+    {
+      imgPath: '/images/1.jpg',
+      title: 'Product 1',
+      description: 'Product Description 1',
+      price: 100
+    },
+    {
+      imgPath: '/images/2.jpg',
+      title: 'Product 2',
+      description: 'Product Description 2',
+      price: 100
+    },
+    {
+      imgPath: '/images/3.jpg',
+      title: 'Product 3',
+      description: 'Product Description 3',
+      price: 100
+    },
+    {
+      imgPath: '/images/1.jpg',
+      title: 'Product 1',
+      description: 'Product Description 1',
+      price: 100
+    },
+    {
+      imgPath: '/images/2.jpg',
+      title: 'Product 2',
+      description: 'Product Description 2',
+      price: 100
+    },
+    {
+      imgPath: '/images/3.jpg',
+      title: 'Product 3',
+      description: 'Product Description 3',
+      price: 100
+    },
+    {
+      imgPath: '/images/1.jpg',
+      title: 'Product 1',
+      description: 'Product Description 1',
+      price: 100
+    },
+    {
+      imgPath: '/images/2.jpg',
+      title: 'Product 2',
+      description: 'Product Description 2',
+      price: 100
+    },
+    {
+      imgPath: '/images/3.jpg',
+      title: 'Product 3',
+      description: 'Product Description 3',
+      price: 100
+    },
+    {
+      imgPath: '/images/3.jpg',
+      title: 'Product 3',
+      description: 'Product Description 3',
+      price: 100
+    }
+  ]
 
-	return (
-		<main className="w-full flex flex-col pt-6">
-			<Navbar title="Products" content="You can see all products here" />
-			<div className="flex gap-4 pt-6">
-				{products.map((product, index) => (
-					<ProductItem
-						key={index}
-						imgPath={product.imgPath}
-						title={product.title}
-						description={product.description}
-						price={product.price} />
-				))}
-			</div>
-		</main>
-	);
+  return (
+    <main className="w-full flex flex-col pt-6">
+      <Navbar title="Products" content="You can see all products here" />
+      <div className="flex flex-wrap gap-4 py-6">
+        {products.map((product, index) => (
+          <ProductItem
+            key={index}
+            imgPath={product.imgPath}
+            title={product.title}
+            description={product.description}
+            price={product.price} />
+        ))}
+      </div>
+    </main>
+  );
 }
