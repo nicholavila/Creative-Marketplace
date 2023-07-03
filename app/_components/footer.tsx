a7369865614856cfa24adf4d1ca54941d9de0b8b
nicholavila <nicholavila1@outlook.com>
2023-07-03 12:36:10 +0000

    rearrange: add footer

---
+++ app/_components/footer.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import Link from "next/link"
+import { usePathname } from "next/navigation";
+
+export const Footer = () => {
+  return (
+    <div className="w-full h-12 flex items-center justify-center bg-gray-200">
+      <div className="w-5/6 flex justify-between">
+        <p>
+          Copyright © 2024 Kre8tive, Inc. All rights reserved.
+        </p>
+        <div className="flex items-center gap-x-4 underline underline-offset-4">
+          <Link href="/privacy-policy">
+            <p>Privacy Policy</p>
+          </Link>
+          <Link href="/terms-of-service">
+            <p>Terms of Service</p>
+          </Link>
+          <Link href="/cookie-policy">
+            <p>Cookie Policy</p>
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
