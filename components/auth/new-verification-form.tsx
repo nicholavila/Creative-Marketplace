b19a632add7f6e7c18b95a9da3f433eb17435381
nicholavila <nicholavila1@outlook.com>
2023-07-04 16:04:35 +0000

    chore: test and review the code & update path of verification form

--- components/auth/new-verification-form.tsx
+++ components/auth/new-verification-form.tsx
@@ -6,15 +6,14 @@ import { useSearchParams } from "next/navigation";
 
 import { newVerification } from "@/actions/new-verification";
 import { CardWrapper } from "@/components/auth/card-wrapper";
-import { FormError } from "@/components/form-error";
-import { FormSuccess } from "@/components/form-success";
+import { FormError } from "@/components/utils/form-error";
+import { FormSuccess } from "@/components/utils/form-success";
 
 export const NewVerificationForm = () => {
   const [error, setError] = useState<string | undefined>();
   const [success, setSuccess] = useState<string | undefined>();
 
   const searchParams = useSearchParams();
-
   const token = searchParams.get("token");
 
   const onSubmit = useCallback(() => {
