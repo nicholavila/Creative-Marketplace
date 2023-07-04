2aab35f24ba4380706362c8856fda88ff73da4de
nicholavila <nicholavila1@outlook.com>
2023-07-04 14:45:21 +0000

    refactor: refactor new password form and update path

--- components/auth/new-password-form.tsx
+++ components/auth/new-password-form.tsx
@@ -18,8 +18,8 @@ import {
 } from "@/components/ui/form";
 import { CardWrapper } from "@/components/auth/card-wrapper";
 import { Button } from "@/components/ui/button";
-import { FormError } from "@/components/form-error";
-import { FormSuccess } from "@/components/form-success";
+import { FormError } from "@/components/utils/form-error";
+import { FormSuccess } from "@/components/utils/form-success";
 import { newPassword } from "@/actions/new-password";
 
 export const NewPasswordForm = () => {
