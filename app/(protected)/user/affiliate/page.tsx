a144138fac2f0ba95e021d13b12bb68ec284a894
nicholavila <nicholavila1@outlook.com>
2023-07-05 10:05:34 +0000

    update: update user settings page for affiliate

--- app/(protected)/user/affiliate/page.tsx
+++ app/(protected)/user/affiliate/page.tsx
@@ -2,14 +2,30 @@
 
 import { Separator } from "@/components/ui/separator";
 import { Header } from "../_components/header";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel } from "@/components/ui/form";
+import { useState, useTransition } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { ProfileSchema } from "@/schemas/user";
+import { Switch } from "@/components/ui/switch";
+import { FormError } from "@/components/utils/form-error";
+import { FormSuccess } from "@/components/utils/form-success";
+import { Button } from "@/components/ui/button";
+import { SwitchBox } from "@/components/utils/switch-box";
 
-const Affiliate = () => {
+const AffiliateSettings = () => {
+  const [error, setError] = useState<string | undefined>("");
+  const [success, setSuccess] = useState<string | undefined>("");
+  const [isChecked, setIsChecked] = useState<boolean>(false);
+  const [isPending, startTransition] = useTransition();
+
   return (
     <main className="w-full pl-8 flex flex-col gap-y-5">
-      <Header title="Affiliate" content="Select how this site you want to serve you" />
-      <Separator />
+      {/* <Header title="Creator" content="Select how this site you want to serve you" /> */}
+      {/* <Separator /> */}
     </main>
   )
 };
 
-export default Affiliate;
+export default AffiliateSettings;
