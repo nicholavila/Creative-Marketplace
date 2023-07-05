be2282983d522ba3a5611a82a6568d2f2c2569bd
nicholavila <nicholavila1@outlook.com>
2023-07-05 12:17:57 +0000

    update: restructure creator page

--- app/(protected)/user/creator/page.tsx
+++ app/(protected)/user/creator/page.tsx
@@ -2,14 +2,32 @@
 
 import { Separator } from "@/components/ui/separator";
 import { Header } from "../_components/header";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel } from "@/components/ui/form";
+import { useState, useTransition } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { ProfileSchema } from "@/schemas/user";
+import { Switch } from "@/components/ui/switch";
+import { FormError } from "@/components/utils/form-error";
+import { FormSuccess } from "@/components/utils/form-success";
+import { Button } from "@/components/ui/button";
+import { SwitchBox } from "@/components/utils/switch-box";
+import EditCreator from "@/components/profile/edit-creator";
 
-const Creator = () => {
+const CreatorSettings = () => {
+  const [error, setError] = useState<string | undefined>("");
+  const [success, setSuccess] = useState<string | undefined>("");
+  const [isChecked, setIsChecked] = useState<boolean>(false);
+  const [isPending, startTransition] = useTransition();
+
   return (
-    <main className="w-full pl-8 flex flex-col gap-y-5">
-      <Header title="Creator" content="Select how this site you want to serve you" />
-      <Separator />
+    <main className="w-full pl-8 pb-6 flex flex-col gap-y-6">
+      {/* <Header title="Creator" content="Select how this site you want to serve you" /> */}
+      {/* <Separator /> */}
+      <EditCreator />
     </main>
   )
 };
 
-export default Creator;
+export default CreatorSettings;
