5912e39caa20bf0641f4758acff74f672963e0f4
nicholavila <nicholavila1@outlook.com>
2023-07-06 11:12:59 +0000

    update: update switchbox

--- components/utils/switch-box.tsx
+++ components/utils/switch-box.tsx
@@ -1,16 +1,19 @@
 import { Dispatch, SetStateAction } from "react";
 import { Switch } from "../ui/switch"
+import { Alert } from "./alert";
 
 type ParamsType = {
   title: string;
   content: string;
   isChecked: boolean;
-  onCheckedChange?: (checked: boolean) => void;
+  onCheckedChange?: () => void;
   mode?: "small" | "big"
+  alertTitle?: string;
+  alertMessage?: string;
 }
 
 export const SwitchBox = ({
-  title, content, isChecked, onCheckedChange, mode = "big"
+  title, content, isChecked, onCheckedChange, mode = "big", alertTitle, alertMessage
 }: ParamsType) => {
   return (
     <div className="w-full flex items-center justify-between rounded-lg border p-4">
@@ -18,10 +21,11 @@ export const SwitchBox = ({
         <p className={`text-black font-medium ${mode === 'big' ? "text-xl" : "text-base"}`}>{title}</p>
         <p className="text-sm text-gray-500">{content}</p>
       </div>
-      <Switch
-        checked={isChecked}
-        onCheckedChange={onCheckedChange}
-      />
+      <Alert title={alertTitle} message={alertMessage} onContinue={onCheckedChange}>
+        <Switch
+          checked={isChecked}
+        />
+      </Alert>
     </div>
   )
 }
