eb8f29c858afb841da1e19fed8c17d29c78dd9bd
nicholavila <nicholavila1@outlook.com>
2023-07-06 15:19:18 +0000

    rearrange: add component for linked site

---
+++ components/profile/linked-sites.tsx
@@ -0,0 +1,10 @@
+import { CreativeSite } from "./creative-site"
+
+export const LinkedSites = () => {
+  return (
+    <div className="w-full flex flex-col gap-y-4">
+      <CreativeSite title="Behance" description="Behance is leading market for creative workers" />
+      <CreativeSite title="Behance" description="Behance is leading market for creative workers" />
+    </div>
+  )
+}
