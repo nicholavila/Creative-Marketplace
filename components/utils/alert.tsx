df273d999ad118f4bac2bfc2a32f05f35894f66d
nicholavila <nicholavila1@outlook.com>
2023-07-06 10:44:57 +0000

    add custom alert component

---
+++ components/utils/alert.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+
+interface LoginButtonProps {
+  children: React.ReactNode;
+  asChild?: boolean;
+  title?: string;
+  message?: string;
+}
+
+export const Alert = ({ children, asChild, title, message }: LoginButtonProps) => {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild={asChild}>
+        {children}
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{title}</AlertDialogTitle>
+          <AlertDialogDescription>{message}</AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
