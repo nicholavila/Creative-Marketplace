e80f958e0dac4bb76405d0c076e2ffef70ac3df1
nicholavila <nicholavila1@outlook.com>
2023-07-07 08:22:56 +0000

    update: update creative site

--- components/profile/creative-site.tsx
+++ components/profile/creative-site.tsx
@@ -8,19 +8,40 @@ import {
 } from "@/components/ui/card"
 
 interface Params {
+  href: string;
   title: string;
   description: string;
+  userId: string;
+  numberOfProducts: number;
+  numberOfFollowers: number;
 }
 
-export const CreativeSite = ({ title, description }: Params) => {
+function Bold({ children }: { children: React.ReactNode }) {
+  return (
+    <span className="font-bold text-sm">
+      {children}
+    </span>
+  )
+}
+
+export const CreativeSite = ({
+  href,
+  title,
+  description,
+  userId,
+  numberOfProducts,
+  numberOfFollowers
+}: Params) => {
   return (
     <Card className="w-full">
       <CardHeader>
         <CardTitle>{title}</CardTitle>
         <CardDescription>{description}</CardDescription>
       </CardHeader>
-      <CardContent>
-
+      <CardContent className="text-sm">
+        <p>Username: <Bold>{userId}</Bold></p>
+        <p>Products: <Bold>{numberOfProducts}</Bold></p>
+        <p>Followers: <Bold>{numberOfFollowers}</Bold></p>
       </CardContent>
       <CardFooter className="flex justify-between">
       </CardFooter>
