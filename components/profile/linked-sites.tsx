3ce3aeec8dafe55e9dc020c7f090b0f9465ae71a
nicholavila <nicholavila1@outlook.com>
2023-07-07 09:02:12 +0000

    update: inject dummy data into linked site

--- components/profile/linked-sites.tsx
+++ components/profile/linked-sites.tsx
@@ -1,10 +1,50 @@
 import { CreativeSite } from "./creative-site"
 
 export const LinkedSites = () => {
+  const siteList = [
+    {
+      href: "",
+      title: "Behance",
+      description: "Behance is leading market for creative workers",
+      userId: "behancewarrior2325",
+      numberOfProducts: 17,
+      numberOfFollowers: 212
+    },
+    {
+      href: "",
+      title: "Creative Market",
+      description: "Creative Market is leading market for creative workers",
+      userId: "creativemarketwarrior2325",
+      numberOfProducts: 71,
+      numberOfFollowers: 220
+    },
+    {
+      href: "",
+      title: "Creative Fabric",
+      description: "Creative Fabric is leading market for creative workers",
+      userId: "creativefabricwarrior2325",
+      numberOfProducts: 127,
+      numberOfFollowers: 242
+    },
+    {
+      href: "",
+      title: "Art Station",
+      description: "Art Station is leading market for creative workers",
+      userId: "atationwarrior2325",
+      numberOfProducts: 347,
+      numberOfFollowers: 922
+    },
+  ]
+
   return (
     <div className="w-full flex flex-col gap-y-4">
-      <CreativeSite title="Behance" description="Behance is leading market for creative workers" />
-      <CreativeSite title="Behance" description="Behance is leading market for creative workers" />
+      {siteList.map((site) => (
+        <CreativeSite
+          key={site.title}
+          {...site}
+        />
+      ))}
+
     </div>
   )
 }
