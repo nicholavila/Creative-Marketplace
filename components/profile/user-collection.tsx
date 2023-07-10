4a705ffabee1e74739ddee53d631a48c2c2c64fd
nicholavila <nicholavila1@outlook.com>
2023-07-10 15:24:27 +0000

    feat: make ADD button live on creator profile page

--- components/profile/user-collection.tsx
+++ components/profile/user-collection.tsx
@@ -2,14 +2,17 @@ import { FaPlus } from "react-icons/fa"
 import { Button } from "../ui/button"
 import { Card, CardContent, CardFooter, CardHeader } from "../ui/card"
 import { ProductItem } from "./productitem"
+import Link from "next/link"
 
 export const UserCollection = () => {
   return (
     <Card className="border-0 rounded-none">
       <CardHeader className="flex flex-row items-center justify-between">
         <p className="text-xl font-bold">Your Collections</p>
-        <Button variant="default" className="flex gap-x-1">
-          <FaPlus /> Add
+        <Button variant="default" asChild className="flex gap-x-1">
+          <Link href="/">
+            <FaPlus /> Add
+          </Link>
         </Button>
       </CardHeader>
       <CardContent className="flex flex-wrap gap-6">
