b56abe73f8ef4239cbb009b59270d63735f153f8
nicholavila <nicholavila1@outlook.com>
2023-07-11 08:40:56 +0000

    refactor: add position for avatar on header

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -11,7 +11,12 @@ export const Header = () => {
     <nav className="w-full z-10">
       <div className="w-full flex items-end justify-between pb-4 box-border border-b-[1px]">
         <div className="flex">
-
+          {/* <Avatar className="w-16 h-16 rounded-xl">
+            <AvatarImage src="/logo.svg" />
+            <AvatarFallback className="bg-sky-500">
+              <FaSignal className="text-white" />
+            </AvatarFallback>
+          </Avatar> */}
 
           <Link href="/">
             <p className="text-4xl font-semibold text-black drop-shadow-md">Kre8tive</p>
