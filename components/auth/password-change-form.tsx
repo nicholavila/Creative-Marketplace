1800445f941971653ac4e73862d91deab9e4bc93
nicholavila <nicholavila1@outlook.com>
2023-07-12 08:20:50 +0000

    feat: add UI into password reset

--- components/auth/password-change-form.tsx
+++ components/auth/password-change-form.tsx
@@ -3,6 +3,9 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { startTransition, useState } from "react"
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../ui/card";
+import { Form } from "../ui/form";
+import { Button } from "../ui/button";
 
 export const PasswordChangeForm = () => {
   const [error, setError] = useState<string | undefined>("");
@@ -25,4 +28,23 @@ export const PasswordChangeForm = () => {
 
     });
   };
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Change Password</CardTitle>
+        <CardDescription>It's recommended to update your password regularly for security</CardDescription>
+      </CardHeader>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          <CardContent>
+
+          </CardContent>
+          <CardFooter>
+            <Button type="submit" variant="default">Change Password</Button>
+          </CardFooter>
+        </form>
+      </Form>
+    </Card>
+  )
 }
