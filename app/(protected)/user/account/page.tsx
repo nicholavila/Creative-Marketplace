21b5a6c0998cc301569c9aba372b6c1bb91570b8
nicholavila <nicholavila1@outlook.com>
2023-07-12 09:16:11 +0000

    chore: inject password change form into account settings page

--- app/(protected)/user/account/page.tsx
+++ app/(protected)/user/account/page.tsx
@@ -2,12 +2,14 @@
 
 import { Separator } from "@/components/ui/separator";
 import { Header } from "../_components/header";
+import { PasswordChangeForm } from "@/components/auth/password-change-form";
 
 const Account = () => {
   return (
     <main className="w-full pl-8 flex flex-col gap-y-5">
       <Header title="Account" content="Your account information here" />
       <Separator />
+      <PasswordChangeForm />
     </main>
   )
 };
