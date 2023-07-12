0d7ecdff543d2956de8fcc6e55ed702ea31fc846
nicholavila <nicholavila1@outlook.com>
2023-07-12 11:52:59 +0000

    feat: add schema for crypto pref

--- schemas/user.ts
+++ schemas/user.ts
@@ -11,3 +11,43 @@ export const ProfileSchema = z.object({
     .min(10, { message: "Bio must be at least 10 characters long" })
     .max(160, { message: "Bio must be a maximum of 160 characters" })
 });
+
+// for 'Crypto Preference' Form
+export const CryptoPrefSchema = z.object({
+  bitcoin: z
+    .string()
+    .min(1, "An address is required")
+    .max(72, "Address must be a maximum of 72 characters"),
+  ethereum: z
+    .string()
+    .min(1, "An address is required")
+    .max(72, "Address must be a maximum of 72 characters"),
+  litecoin: z
+    .string()
+    .min(1, "An address is required")
+    .max(72, "Address must be a maximum of 72 characters"),
+  dogecoin: z
+    .string()
+    .min(1, "An address is required")
+    .max(72, "Address must be a maximum of 72 characters"),
+  xrp: z
+    .string()
+    .min(1, "An address is required")
+    .max(72, "Address must be a maximum of 72 characters"),
+  sol: z
+    .string()
+    .min(1, "An address is required")
+    .max(72, "Address must be a maximum of 72 characters"),
+  usdcoin: z
+    .string()
+    .min(1, "An address is required")
+    .max(72, "Address must be a maximum of 72 characters"),
+  avax: z
+    .string()
+    .min(1, "An address is required")
+    .max(72, "Address must be a maximum of 72 characters"),
+  tron: z
+    .string()
+    .min(1, "An address is required")
+    .max(72, "Address must be a maximum of 72 characters")
+});
