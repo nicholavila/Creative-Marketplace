f7632aeef510352fbaeb87f5077dd0fb92a6ad5c
nicholavila <nicholavila1@outlook.com>
2023-07-12 08:55:19 +0000

    feat: add form-field into account settings page

--- components/auth/password-change-form.tsx
+++ components/auth/password-change-form.tsx
@@ -1,15 +1,17 @@
 import { PasswordChangeSchema } from "@/schemas/auth";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { startTransition, useState } from "react"
+import { useState, useTransition } from "react"
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../ui/card";
-import { Form } from "../ui/form";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
 import { Button } from "../ui/button";
+import { Input } from "../ui/input";
 
 export const PasswordChangeForm = () => {
   const [error, setError] = useState<string | undefined>("");
   const [success, setSuccess] = useState<string | undefined>("");
+  const [isPending, startTransition] = useTransition();
 
   const form = useForm<z.infer<typeof PasswordChangeSchema>>({
     resolver: zodResolver(PasswordChangeSchema),
@@ -38,7 +40,24 @@ export const PasswordChangeForm = () => {
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
           <CardContent>
-
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      disabled={isPending}
+                      placeholder="******"
+                      type="password"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
           </CardContent>
           <CardFooter>
             <Button type="submit" variant="default">Change Password</Button>
