12d5266c0f22534d812def1c9c2b743ed1acf685
nicholavila <nicholavila1@outlook.com>
2023-07-12 13:10:06 +0000

    refactor: update style and structure of account apge

--- app/(protected)/user/account/page.tsx
+++ app/(protected)/user/account/page.tsx
@@ -3,13 +3,18 @@
 import { Separator } from "@/components/ui/separator";
 import { Header } from "../_components/header";
 import { PasswordChangeForm } from "@/components/auth/password-change-form";
+import { CryptoPrefForm } from "@/components/profile/cryptopref-form";
 
 const Account = () => {
   return (
     <main className="w-full pl-8 flex flex-col gap-y-5">
       <Header title="Account" content="Your account information here" />
       <Separator />
-      <PasswordChangeForm />
+      <div className="w-full flex gap-x-4">
+        <PasswordChangeForm />
+        <CryptoPrefForm />
+      </div>
+
     </main>
   )
 };
