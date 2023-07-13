78d4e73d6da896a9a0d67b363f20a36021f7eea8
nicholavila <nicholavila1@outlook.com>
2023-07-13 11:04:50 +0000

    update: update account settings page

--- app/(protected)/user/account/page.tsx
+++ app/(protected)/user/account/page.tsx
@@ -4,18 +4,21 @@ import { Separator } from "@/components/ui/separator";
 import { Header } from "../_components/header";
 import { PasswordChangeForm } from "@/components/auth/password-change-form";
 import { CryptoPrefForm } from "@/components/profile/cryptopref-form";
+import { PaymentPrefForm } from "@/components/profile/paymentpref-form";
 
 const Account = () => {
   return (
     <main className="w-full pl-8 pb-6 flex flex-col gap-y-5">
       <Header title="Account" content="Your account information here" />
       <Separator />
-      <div className="w-full flex gap-x-4">
-        <div className="w-1/2">
+      <div className="w-full flex gap-x-6">
+        <div className="w-1/2 flex flex-col gap-y-6">
           <PasswordChangeForm />
+          <PaymentPrefForm />
         </div>
-
-        <CryptoPrefForm />
+        <div className="w-1/2">
+          <CryptoPrefForm />
+        </div>
       </div>
 
     </main>
