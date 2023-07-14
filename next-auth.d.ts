1a47850f930f76659d0306da19ca108bfacd0cfe
nicholavila <nicholavila1@outlook.com>
2023-07-14 10:35:39 +0000

    update: update token type for auth

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,8 +1,7 @@
-import { UserRole } from "@prisma/client";
 import NextAuth, { type DefaultSession } from "next-auth";
 
 export type ExtendedUser = DefaultSession["user"] & {
-  role: UserRole;
+  userId: string;
   isTwoFactorEnabled: boolean;
   isOAuth: boolean;
 };
