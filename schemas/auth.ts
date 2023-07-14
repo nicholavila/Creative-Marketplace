31f672a5ca102ad1796a05607cafcd5e7e0c36e4
nicholavila <nicholavila1@outlook.com>
2023-07-14 08:49:25 +0000

    update: update schema for signup form

--- schemas/auth.ts
+++ schemas/auth.ts
@@ -26,15 +26,23 @@ export const LoginSchema = z.object({
 
 // for `Signup` Form
 export const RegisterSchema = z.object({
+  username: z
+    .string()
+    .min(1, "A name is required")
+    .max(72, "Name must be a maximum of 72 characters"),
+  firstname: z
+    .string()
+    .min(1, "A name is required")
+    .max(72, "Name must be a maximum of 72 characters"),
+  lastname: z
+    .string()
+    .min(1, "A name is required")
+    .max(72, "Name must be a maximum of 72 characters"),
   email: z.string().email({ message: "Please enter a valid email" }),
   password: z
     .string()
     .min(6, "Passwords must be at least 6 characters long")
-    .max(32, "Passwords must be a maximum of 32 characters"),
-  name: z
-    .string()
-    .min(1, "A name is required")
-    .max(72, "Name must be a maximum of 72 characters")
+    .max(32, "Passwords must be a maximum of 32 characters")
 });
 
 // for `Account Setting` Form
