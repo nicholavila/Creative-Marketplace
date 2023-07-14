a9de747574cf72f5b1b8b062bfa2637d6d65cd84
nicholavila <nicholavila1@outlook.com>
2023-07-14 13:43:12 +0000

    update: server action for new verification

--- actions/new-verification.ts
+++ actions/new-verification.ts
@@ -1,15 +1,15 @@
 "use server";
 
 import { getUserById, updateUserVerification } from "@/data/user";
-// import { getVerificationTokenByToken } from "@/data/verificiation-token";
+import { getUserIdFromToken } from "@/lib/tokens";
 
 export const newVerification = async (token: string) => {
-  if (!token || token.length !== 36 * 2) {
-    // twice of length of uuidv4
+  if (!token || token.length <= 36) {
     return { error: "Missing token!" };
   }
 
-  const userId = token.slice(0, 36); // length of uuidv4
+  const userId = getUserIdFromToken(token);
+  console.log("__newVerification__getUserIdFromToken", userId);
 
   const existingUser = await getUserById(userId);
   if (!existingUser) {
@@ -31,31 +31,4 @@ export const newVerification = async (token: string) => {
   }
 
   return { success: "Email verified!" };
-
-  // const existingToken = await getVerificationTokenByToken(token);
-  // if (!existingToken) {
-  //   return { error: "Token does not exist!" };
-  // }
-
-  // const hasExpired = new Date(existingToken.expires) < new Date();
-  // if (hasExpired) {
-  //   return { error: "Token has expired!" };
-  // }
-
-  // const existingUser = await getUserByEmail(existingToken.email);
-  // if (!existingUser) {
-  //   return { error: "Email does not exist!" };
-  // }
-
-  // await db.user.update({
-  //   where: { id: existingUser.id },
-  //   data: {
-  //     emailVerified: new Date(),
-  //     email: existingToken.email
-  //   }
-  // });
-
-  // await db.verificationToken.delete({
-  //   where: { id: existingToken.id }
-  // });
 };
