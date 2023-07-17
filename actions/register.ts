e5c976efab1b1436bc940f538b81e67c0e6705f7
nicholavila <nicholavila1@outlook.com>
2023-07-17 09:02:23 +0000

    update: update register's variables

--- actions/register.ts
+++ actions/register.ts
@@ -4,9 +4,8 @@ import { z } from "zod";
 import bcrypt from "bcryptjs";
 
 import { RegisterSchema } from "@/schemas/auth";
-import { createUser, getUserByEmail } from "@/data/user";
+import { createUser, getUserByEmail, getAllUsernames } from "@/data/user";
 import { sendVerificationEmail } from "@/lib/mail";
-// import { generateVerificationToken } from "@/lib/tokens";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validateFields = RegisterSchema.safeParse(values);
@@ -14,7 +13,17 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: "Server Says Fields are Invalid!" };
   }
 
-  const { email, password, name } = validateFields.data;
+  // Check whether username selected is available
+  const response = await getAllUsernames();
+  const similarId = response.items.find((item: { username: string }) =>
+    item.username.includes(values.username)
+  );
+  if (similarId) {
+    return { error: "Same Username Already Exists!" };
+  }
+
+  const { email, password, username, firstname, lastname } =
+    validateFields.data;
   const hashedPassword = await bcrypt.hash(password, 10);
   const existingUser = await getUserByEmail(email);
 
@@ -23,15 +32,10 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   }
 
   const verificationToken = await createUser({
-    name,
+    username,
+    firstname,
+    lastname,
     email,
     password: hashedPassword
   });
-
-  // const verificationToken = await generateVerificationToken(email);
-
-  if (verificationToken) {
-    const response = await sendVerificationEmail(email, verificationToken);
-    return { success: "New user registered, Check your mailbox!" };
-  } else return { error: "Server Error!" };
 };
