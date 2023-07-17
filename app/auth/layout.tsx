5404a4d83ff0255bcce6d3e167c4734eb16d9240
nicholavila <nicholavila1@outlook.com>
2023-07-17 09:59:53 +0000

    style: adjust style of auth layout

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="fixed top-0 right-0 w-full h-screen flex items-center justify-center z-0">
+    <div className="w-full flex justify-center pt-16 py-6">
       {children}
     </div>
   );
