5f18db00a975ffcdb350f87884ee3f9ebe29db60
nicholavila <nicholavila1@outlook.com>
2023-07-18 08:52:57 +0000

    feat: add fetch action into creator profile page

--- app/(protected)/user/creator/page.tsx
+++ app/(protected)/user/creator/page.tsx
@@ -1,15 +1,28 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { SwitchBox } from "@/components/utils/switch-box";
 import EditCreator from "@/components/profile/edit-creator";
+import { useCurrentUser } from "@/hooks/use-current-user";
+import { getUserById } from "@/data/user";
+import { CreatorInterface } from "@/shared/user";
 
 const CreatorSettings = () => {
+  const user = useCurrentUser();
   const [isChecked, setIsChecked] = useState<boolean>(false);
+  const [creator, setCreator] = useState<CreatorInterface>();
 
   const onSwitch = () => {
     setIsChecked(!isChecked);
   }
+
+  useEffect(() => {
+    if (user) {
+      getUserById(user.id).then(data => {
+        setCreator(data);
+      });
+    }
+  }, []);
 
   return (
     <main className="w-full pl-8 pb-6 flex flex-col gap-y-6">
