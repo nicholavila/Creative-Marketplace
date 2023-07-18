67a2469566a198e8beda2b9414e97fb8748b25ab
nicholavila <nicholavila1@outlook.com>
2023-07-18 14:40:52 +0000

    rearrange: add customer interface

--- shared/user.ts
+++ shared/user.ts
@@ -6,3 +6,11 @@ export type CreatorInterface = {
   username: string;
   bio: string;
 };
+
+export type CustomerInterface = {
+  userId: string;
+  email: string;
+  firstname: string;
+  lastname: string;
+  username: string;
+};
