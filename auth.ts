19ff65fa01b586a759fdd881609d29ffa35e99eb
nicholavila <nicholavila1@outlook.com>
2023-07-19 09:54:29 +0000

    update: update user table on dynamodb

--- auth.ts
+++ auth.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import authConfig from "@/auth.config";
 import adapter from "@/adapter";
-import { getUserByEmail } from "./data/user";
+import { getUserByEmail } from "./data/user/user-by-email";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter,
