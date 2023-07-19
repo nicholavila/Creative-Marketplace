4be235c75c42039839bb3f9f4825b58f30a8b875
nicholavila <nicholavila1@outlook.com>
2023-07-19 13:22:22 +0000

    style: individual item on product detail page

--- components/product/product-item.tsx
+++ components/product/product-item.tsx
@@ -14,23 +14,23 @@ interface PropsParams {
 
 export const ProductItem = ({ imgPath, title, description, price }: PropsParams) => {
   return (
-    <Link href={`/products/details/${title}`}>
-      <Card className="flex flex-col items-center shadow-md cursor-pointer hover:drop-shadow-lg hover:bg-gray-100 hover:translate-x-[-1px] hover:translate-y-[-1px]">
-        <CardHeader>
-          <p>{title}</p>
-        </CardHeader>
-        <CardContent>
-          <Avatar className="w-48 h-48 rounded-xl">
+    <Link href={`/products/details/${title}`} className="w-full">
+      <Card className="w-full flex flex-col items-center px-0 rounded-none shadow-md cursor-pointer hover:drop-shadow-lg hover:bg-gray-100 hover:translate-x-[-1px] hover:translate-y-[-1px]">
+        <CardContent className="w-full p-0 flex flex-col gap-y-4">
+          <Avatar className="w-full h-52 rounded-none">
             <AvatarImage src={imgPath} />
             <AvatarFallback className="bg-sky-500">
-              <FaShoppingBasket className="text-white" />
+              <div className="w-full h-full bg-green-700"></div>
             </AvatarFallback>
           </Avatar>
+          <div className="flex flex-col px-4 pb-4">
+            <div className="flex gap-4">
+              <p>{title}</p>
+              <p className="text-md text-gray-600">Price: ${price}</p>
+            </div>
+            <p className="text-lg text-black drop-shadow-md">{description}</p>
+          </div>
         </CardContent>
-        <CardFooter className="flex flex-col">
-          <p className="text-lg text-black drop-shadow-md">{description}</p>
-          <p className="text-md text-gray-600">Price: ${price}</p>
-        </CardFooter>
       </Card>
     </Link>
   );
