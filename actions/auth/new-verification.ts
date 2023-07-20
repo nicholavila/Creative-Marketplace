b663c05ae6ba82b68a0941dc82f5014b7c908d11
nicholavila <nicholavila1@outlook.com>
2023-07-20 15:50:56 +0000

    refactor: new verification page

---
+++ actions/auth/new-verification.ts
@@ -0,0 +1,35 @@
+"use server";
+
+import { getUserById } from "@/data/user/user-by-id";
+import { updateUserVerification } from "@/data/user/verification-update";
+import { getUserIdFromToken } from "@/lib/tokens";
+
+export const newVerification = async (token: string) => {
+  if (!token || token.length <= 36) {
+    return { error: "Missing token!" };
+  }
+
+  const userId = getUserIdFromToken(token);
+  console.log("__newVerification__getUserIdFromToken", userId);
+
+  const existingUser = await getUserById(userId);
+  if (!existingUser) {
+    return { error: "Invalid Token!" };
+  }
+
+  const expires = Date.parse(existingUser.expires);
+  if (expires < new Date().getTime()) {
+    return { error: "Token is expired!" };
+  }
+
+  if (token !== existingUser.verificationToken) {
+    return { error: "Invalid Token!" };
+  }
+
+  const updatedUser = await updateUserVerification(userId);
+  if (!updatedUser) {
+    return { error: "Server error!" };
+  }
+
+  return { success: "Email verified!" };
+};
