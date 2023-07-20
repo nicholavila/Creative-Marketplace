6b04c5690e4f424d57a940b2fe35891e023915d3
nicholavila <nicholavila1@outlook.com>
2023-07-20 10:33:17 +0000

    update: auth verification form

--- components/auth/new-verification-form.tsx
+++ components/auth/new-verification-form.tsx
@@ -4,7 +4,7 @@ import { useCallback, useEffect, useState } from "react";
 import { BeatLoader } from "react-spinners";
 import { useSearchParams } from "next/navigation";
 
-import { newVerification } from "@/actions/new-verification";
+import { newVerification } from "@/actions/auth/new-verification";
 import { CardWrapper } from "@/components/auth/card-wrapper";
 import { FormError } from "@/components/utils/form-error";
 import { FormSuccess } from "@/components/utils/form-success";
