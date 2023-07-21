6dd0ec79da2b5c8cb6c84b8f317f8f72c4d08170
nicholavila <nicholavila1@outlook.com>
2023-07-21 09:28:36 +0000

    update: update and test verification flow

---
+++ data/user/verification-update.ts
@@ -0,0 +1,29 @@
+"use server";
+
+import db from "@/lib/db";
+import { UpdateCommand } from "@aws-sdk/lib-dynamodb";
+
+const TableName = process.env.AWS_DYNAMODB_TABLE_NAME;
+
+export const updateUserVerification = async (userId: string) => {
+  const command = new UpdateCommand({
+    TableName,
+    Key: {
+      userId
+    },
+    UpdateExpression: "SET emailVerified = :emailVerified",
+    ExpressionAttributeValues: {
+      ":emailVerified": new Date().toISOString()
+    },
+    ReturnValues: "ALL_NEW"
+  });
+
+  try {
+    const response = await db.send(command);
+    console.log("__updateUserVerification__UpdateCommand__RESPONSE", response);
+    return response.Attributes;
+  } catch (error) {
+    console.log("__updateUserVerification__UpdateCommand__ERROR", error);
+    return null;
+  }
+};
