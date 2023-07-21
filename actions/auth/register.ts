181c0055fd03230b0754add4ee4b356cec2d5cdb
nicholavila <nicholavila1@outlook.com>
2023-07-21 11:51:24 +0000

    update: update register server action

---
+++ actions/auth/register.ts
@@ -0,0 +1,54 @@
+"use server";
+
+import { z } from "zod";
+import bcrypt from "bcryptjs";
+
+import { RegisterSchema } from "@/schemas/auth";
+import { createUser } from "@/data/user/user-create";
+import { sendVerificationEmail } from "@/lib/mail";
+import { getAllUsernames } from "@/data/user/all-usernames";
+import { getUserByEmail } from "@/data/user/user-by-email";
+
+export const register = async (values: z.infer<typeof RegisterSchema>) => {
+  const validateFields = RegisterSchema.safeParse(values);
+  if (!validateFields.success) {
+    return { error: "Server Says Fields are Invalid!" };
+  }
+
+  // Check whether username selected is available
+  const response = await getAllUsernames();
+  const similarId = response.items.find((item: { username: string }) =>
+    item.username.includes(values.username)
+  );
+  if (similarId) {
+    return { error: "Same Username Already Exists!" };
+  }
+
+  const { email, password, username, firstname, lastname } =
+    validateFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  const verificationToken = await createUser({
+    username,
+    firstname,
+    lastname,
+    email,
+    password: hashedPassword
+  });
+
+  if (verificationToken) {
+    await sendVerificationEmail(email, verificationToken);
+    return {
+      success: "New user registered, Check your mailbox!"
+    };
+  } else {
+    return {
+      error: "Server Error!"
+    };
+  }
+};
