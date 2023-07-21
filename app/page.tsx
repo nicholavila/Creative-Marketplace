6222c4f6925212b689f401a8d41d6a6a13b74ed9
nicholavila <nicholavila1@outlook.com>
2023-07-21 13:21:45 +0000

    update: update homepage

--- app/page.tsx
+++ app/page.tsx
@@ -17,28 +17,26 @@ const Home = () => {
   const user = useCurrentUser();
   const tempImagePath = ["/profile-back-example.jpg", "/product-example.jpg", "/product-example-2.jpg"];
 
+  const images = [
+    "top-0 left-0 rotate-[6deg]",
+    "top-0 left-0 rotate-[6deg]",
+    "top-0 left-0 rotate-[6deg]",
+  ]
+
   return (
     <main className="relative w-full flex justify-center pt-24 pb-6">
-      <Avatar className="absolute top-0 left-0 w-96 h-56 rounded-lg">
-        {/* <AvatarImage src={imgPath} /> */}
-        <AvatarImage
-          src={tempImagePath[Math.floor(Math.random() * 100) % 3]}
-          className="object-fill aspect-auto"
-        />
-        <AvatarFallback className="bg-sky-500">
-          <div className="w-full h-full bg-green-700"></div>
-        </AvatarFallback>
-      </Avatar>
-      <Avatar className="absolute top-96 left-0 w-96 h-56 rounded-lg">
-        {/* <AvatarImage src={imgPath} /> */}
-        <AvatarImage
-          src={tempImagePath[Math.floor(Math.random() * 100) % 3]}
-          className="object-fill aspect-auto"
-        />
-        <AvatarFallback className="bg-sky-500">
-          <div className="w-full h-full bg-green-700"></div>
-        </AvatarFallback>
-      </Avatar>
+      {images.map(_style => (
+        <Avatar className={`absolute w-96 h-56 rounded-lg ${_style}`}>
+          {/* <AvatarImage src={imgPath} /> */}
+          <AvatarImage
+            src={tempImagePath[Math.floor(Math.random() * 100) % 3]}
+            className="object-fill aspect-auto"
+          />
+          <AvatarFallback className="bg-sky-500">
+            <div className="w-full h-full bg-green-700"></div>
+          </AvatarFallback>
+        </Avatar>
+      ))}
       <div className="w-1/2 flex flex-col items-center gap-y-24 text-center">
         <h1 className={cn("text-7xl font-semibold text-black drop-shadow-md", font.className)}>
           Bring your creative ideas to life!
