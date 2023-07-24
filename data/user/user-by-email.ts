99e40404b7cdd2a3e3783a98caeeefcc5d050b61
nicholavila <nicholavila1@outlook.com>
2023-07-24 10:12:26 +0000

    rearrange: token

---
+++ data/user/user-by-email.ts
@@ -0,0 +1,30 @@
+"use server";
+
+import db from "@/lib/db";
+import { ScanCommand } from "@aws-sdk/lib-dynamodb";
+
+const TableName = process.env.AWS_DYNAMODB_TABLE_NAME;
+
+export const getUserByEmail = async (email: string) => {
+  const command = new ScanCommand({
+    TableName,
+    // ProjectionExpression: "email, emailVerified", // attr names to get
+    // ProjectionExpression: "email, emailVerified, #name",
+    // ExpressionAttributeNames: { "#name": "name" }, // for reserved attr names
+    FilterExpression: "email = :email",
+    ExpressionAttributeValues: {
+      ":email": email
+    }
+    // Limit: 1 // just number of scanned items, not result
+  });
+
+  try {
+    const response = await db.send(command);
+    console.log("__getUserByEmail__ScanCommand__RESPONSE", response);
+    if (response.Count) return response.Items[0];
+    else return null;
+  } catch (error) {
+    console.log("__getUserByEmail__ScanCommand__ERROR", error);
+    return null;
+  }
+};
---
+++ data/user/user-by-id.ts
@@ -0,0 +1,24 @@
+"use server";
+
+import db from "@/lib/db";
+import { GetCommand } from "@aws-sdk/lib-dynamodb";
+
+const TableName = process.env.AWS_DYNAMODB_TABLE_NAME;
+
+export const getUserById = async (userId: string) => {
+  const command = new GetCommand({
+    TableName,
+    Key: {
+      userId
+    }
+  });
+
+  try {
+    const response = await db.send(command);
+    console.log("__getUserById__GetCommand__RESPONSE", response);
+    return response.Item;
+  } catch (error) {
+    console.log("__getUserById__GetCommand__ERROR", error);
+    return null;
+  }
+};
