9ed9415d7f7ec2f16d70fd80ef0376cb016946e3
nicholavila <nicholavila1@outlook.com>
2023-07-26 13:26:31 +0000

    style: hover action on image preview

--- components/product/image-preview.tsx
+++ components/product/image-preview.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Button } from "../ui/button";
 
 type PropsParams = {
@@ -5,10 +6,12 @@ type PropsParams = {
 }
 
 export const ImagePreview = ({ src }: PropsParams) => {
+  const [isHover, setHover] = useState<boolean>(false);
+
   return (
-    <div className="relative">
+    <div className="relative cursor-pointer" onMouseOver={() => setHover(true)} onMouseOut={() => setHover(false)}>
       <img src={src} className="h-28" />
-      <div className="absolute top-0 right-0 z-10 w-full h-full flex items-center justify-center bg-gray-700/70">
+      <div className={`absolute top-0 right-0 z-10 w-full h-full flex items-center justify-center bg-gray-700/70 ${!isHover && 'hidden'}`}>
         <div className="flex flex-col items-center gap-y-2">
           <Button variant="outline" className="w-fit h-6 px-1 py-0 bg-transparent hover:bg-transparent hover border border-green-400 text-sm text-white hover:text-white hover:font-bold">
             Preview
