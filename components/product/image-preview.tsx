4c34a3730fd6567fc4f782f51b94bb1d90b6c91f
nicholavila <nicholavila1@outlook.com>
2023-07-26 15:14:40 +0000

    add litener to buttons of image preview

--- components/product/image-preview.tsx
+++ components/product/image-preview.tsx
@@ -7,7 +7,7 @@ type PropsParams = {
   onDelete: () => void;
 }
 
-export const ImagePreview = ({ src }: PropsParams) => {
+export const ImagePreview = ({ src, onPreview, onDelete }: PropsParams) => {
   const [isHover, setHover] = useState<boolean>(false);
 
   return (
@@ -15,10 +15,18 @@ export const ImagePreview = ({ src }: PropsParams) => {
       <img src={src} className="h-28" />
       <div className={`absolute top-0 right-0 z-10 w-full h-full flex items-center justify-center bg-gray-700/70 ${!isHover && 'hidden'}`}>
         <div className="flex flex-col items-center gap-y-2">
-          <Button variant="outline" className="w-fit h-6 px-1 py-0 bg-transparent hover:bg-transparent hover border border-green-400 text-sm text-white hover:text-white hover:font-bold">
+          <Button
+            variant="outline"
+            className="w-fit h-6 px-1 py-0 bg-transparent hover:bg-transparent hover border border-green-400 text-sm text-white hover:text-white hover:font-bold"
+            onClick={onPreview}
+          >
             Preview
           </Button>
-          <Button variant="outline" className="w-fit h-6 px-1 py-0 bg-transparent hover:bg-transparent hover border border-red-400 text-sm text-white hover:text-white hover:font-bold">
+          <Button
+            variant="outline"
+            className="w-fit h-6 px-1 py-0 bg-transparent hover:bg-transparent hover border border-red-400 text-sm text-white hover:text-white hover:font-bold"
+            onClick={onDelete}
+          >
             Delete
           </Button>
         </div>
