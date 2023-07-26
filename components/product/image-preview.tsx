8dd4d123520dcd58099472ec21b21c2a7e7c0b20
nicholavila <nicholavila1@outlook.com>
2023-07-26 11:16:42 +0000

    update button style

--- components/product/image-preview.tsx
+++ components/product/image-preview.tsx
@@ -9,11 +9,11 @@ export const ImagePreview = ({ src }: PropsParams) => {
     <div className="relative">
       <img src={src} className="h-28" />
       <div className="absolute top-0 right-0 z-10 w-full h-full flex items-center justify-center bg-gray-700/50">
-        <div className="flex flex-col">
-          <Button variant="link" className="h-6 p-0 text-sm text-white">
+        <div className="flex flex-col items-center">
+          <Button variant="outline" className="w-fit h-6 p-0 bg-transparent text-sm text-white">
             Preview
           </Button>
-          <Button variant="link" className="h-6 p-0 text-sm text-white">
+          <Button variant="outline" className="w-fit h-6 p-0 bg-transparent text-sm text-white">
             Delete
           </Button>
         </div>
