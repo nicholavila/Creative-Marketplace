1e853a4151cf0461906a480226e4052f6fba0ba0
nicholavila <nicholavila1@outlook.com>
2023-07-26 11:46:39 +0000

    style: flex for image preview

--- components/product/image-preview.tsx
+++ components/product/image-preview.tsx
@@ -8,12 +8,12 @@ export const ImagePreview = ({ src }: PropsParams) => {
   return (
     <div className="relative">
       <img src={src} className="h-28" />
-      <div className="absolute top-0 right-0 z-10 w-full h-full flex items-center justify-center bg-gray-700/50">
-        <div className="flex flex-col items-center">
-          <Button variant="outline" className="w-fit h-6 p-0 bg-transparent text-sm text-white">
+      <div className="hidden hover:flex absolute top-0 right-0 z-10 w-full h-full items-center justify-center bg-gray-700/70">
+        <div className="flex flex-col items-center gap-y-2">
+          <Button variant="outline" className="w-fit h-6 px-1 py-0 bg-transparent border border-green-400 text-sm text-white">
             Preview
           </Button>
-          <Button variant="outline" className="w-fit h-6 p-0 bg-transparent text-sm text-white">
+          <Button variant="outline" className="w-fit h-6 px-1 py-0 bg-transparent border border-red-400 text-sm text-white">
             Delete
           </Button>
         </div>
