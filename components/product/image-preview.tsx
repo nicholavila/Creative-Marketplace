07a040251c7749659820736536e48c99e9ae6a75
nicholavila <nicholavila1@outlook.com>
2023-07-26 09:56:50 +0000

    feat: add function btn

--- components/product/image-preview.tsx
+++ components/product/image-preview.tsx
@@ -1,9 +1,21 @@
+import { Button } from "../ui/button";
+
 type PropsParams = {
   src: string;
 }
 
 export const ImagePreview = ({ src }: PropsParams) => {
   return (
-    <img src={src} className="h-28" />
+    <div className="relative">
+      <img src={src} className="h-28" />
+      <div className="absolute top-0 right-0 z-10 w-full h-full bg-gray-700/50">
+        <Button variant="link">
+          Preview
+        </Button>
+        <Button variant="outline">
+          Preview
+        </Button>
+      </div>
+    </div>
   )
 }
