a819d16ec2cc29a46aeeb6cf70852384d2eaa33e
nicholavila <nicholavila1@outlook.com>
2023-07-26 10:29:44 +0000

    style: fix button style

--- components/product/image-preview.tsx
+++ components/product/image-preview.tsx
@@ -8,13 +8,15 @@ export const ImagePreview = ({ src }: PropsParams) => {
   return (
     <div className="relative">
       <img src={src} className="h-28" />
-      <div className="absolute top-0 right-0 z-10 w-full h-full bg-gray-700/50">
-        <Button variant="link">
-          Preview
-        </Button>
-        <Button variant="outline">
-          Preview
-        </Button>
+      <div className="absolute top-0 right-0 z-10 w-full h-full flex items-center justify-center bg-gray-700/50">
+        <div className="flex flex-col">
+          <Button variant="link" className="h-6 p-0 text-sm text-white">
+            Preview
+          </Button>
+          <Button variant="link" className="h-6 p-0 text-sm text-white">
+            Delete
+          </Button>
+        </div>
       </div>
     </div>
   )
