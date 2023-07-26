bb6221e389da1174cd132b578c13c6a74722d2cd
nicholavila <nicholavila1@outlook.com>
2023-07-26 12:02:49 +0000

    style: hover hidden

--- components/product/image-preview.tsx
+++ components/product/image-preview.tsx
@@ -8,7 +8,7 @@ export const ImagePreview = ({ src }: PropsParams) => {
   return (
     <div className="relative">
       <img src={src} className="h-28" />
-      <div className="hidden hover:flex absolute top-0 right-0 z-10 w-full h-full items-center justify-center bg-gray-700/70">
+      <div className="absolute top-0 right-0 z-10 w-full h-full flex items-center justify-center bg-gray-700/70">
         <div className="flex flex-col items-center gap-y-2">
           <Button variant="outline" className="w-fit h-6 px-1 py-0 bg-transparent border border-green-400 text-sm text-white">
             Preview
