e813d2929fd2138dec99906c5bdf4ce5a4c3252d
nicholavila <nicholavila1@outlook.com>
2023-07-31 12:11:47 +0000

    feat: api for product upload

--- app/api/new-product/route.ts
+++ app/api/new-product/route.ts
@@ -1,4 +1,5 @@
 import { uploadFileToS3 } from "@/actions/s3/upload-image";
+import { createProduct } from "@/data/products/product-create";
 import { NextResponse } from "next/server";
 import { v4 as uuidv4 } from "uuid";
 
@@ -9,28 +10,37 @@ type RequestType = {
 export const POST = async (req: RequestType) => {
   try {
     const formData = await req.formData();
+
+    const userId = formData.get("userId");
+    const productData = JSON.parse(formData.get("product") as string);
+
+    const productFiles: string[] = [];
     const formDataEntryValues = Array.from(formData.values());
 
-    const productFiles = [];
-
-    formDataEntryValues.forEach(async (value) => {
+    await Promise.all(formDataEntryValues.map(async (value) => {
       if (value instanceof File) {
         const keyName = uuidv4();
         const response = await uploadFileToS3(value, keyName);
-        productFiles.push(keyName);
-      } else {
-        const product = JSON.parse(value as string);
-        console.log(product);
+        if (response.success) {
+          productFiles.push(keyName);
+        }
       }
+    }));
+
+    if(productFiles.length === 0) {
+      return NextResponse.json({ success: false, error: "Failed to upload images" }, { status: 500 });
+    }
+
+    const response = await createProduct(userId, {
+      ...productData,
+      imageList: productFiles
     });
 
-    // # Save product files to user data
-
-    // if (response.success) {
-    //   return NextResponse.json(response, { status: 200 });
-    // } else {
-    //   return NextResponse.json(response, { status: 500 });
-    // }
+    if (response.success) {
+      return NextResponse.json(response, { status: 200 });
+    } else {
+      return NextResponse.json(response, { status: 500 });
+    }
   } catch (error) {
     return NextResponse.json({ success: false, error }, { status: 500 });
   }
