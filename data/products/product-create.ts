3cccf20e2aef8edada84f2d57e3b8667fe506c06
nicholavila <nicholavila1@outlook.com>
2023-07-31 14:50:24 +0000

    refacotr product craete

--- data/products/product-create.ts
+++ data/products/product-create.ts
@@ -3,15 +3,7 @@
 import { v4 as uuidv4 } from "uuid";
 
 import db from "@/lib/db";
-import {
-  GetCommand,
-  PutCommand,
-  ScanCommand,
-  UpdateCommand
-} from "@aws-sdk/lib-dynamodb";
-import { generateVerificationToken } from "@/lib/tokens";
-import { z } from "zod";
-import { CreatorRegisterSchema } from "@/schemas/auth";
+import { PutCommand } from "@aws-sdk/lib-dynamodb";
 
 const TableName = process.env.AWS_DYNAMODB_PRODUCTS_TABLE_NAME;
 
