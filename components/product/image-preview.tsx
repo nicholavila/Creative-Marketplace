66e7f5e4a241240f0c71fb20edece21371167a00
nicholavila <nicholavila1@outlook.com>
2023-07-31 11:23:34 +0000

    style: update image-preview

--- components/product/image-preview.tsx
+++ components/product/image-preview.tsx
@@ -2,12 +2,13 @@ import { useState } from "react";
 import { Button } from "../ui/button";
 
 type PropsParams = {
+  disabled: boolean;
   src: string;
   onPreview: () => void;
   onDelete: () => void;
 }
 
-export const ImagePreview = ({ src, onPreview, onDelete }: PropsParams) => {
+export const ImagePreview = ({ disabled, src, onPreview, onDelete }: PropsParams) => {
   const [isHover, setHover] = useState<boolean>(false);
 
   return (
@@ -17,14 +18,15 @@ export const ImagePreview = ({ src, onPreview, onDelete }: PropsParams) => {
         <div className="flex flex-col items-center gap-y-2">
           <Button
             variant="outline"
-            className="w-fit h-6 px-1 py-0 bg-transparent hover:bg-transparent hover border border-green-400 text-sm text-white hover:text-white hover:px-2"
+            className="w-fit h-6 px-1 py-0 bg-transparent hover:bg-transparent rounded-none border-0 hover:border border-green-400 text-sm text-white hover:text-white"
             onClick={onPreview}
           >
             Preview
           </Button>
           <Button
+            disabled={disabled}
             variant="outline"
-            className="w-fit h-6 px-1 py-0 bg-transparent hover:bg-transparent hover border border-red-400 text-sm text-white hover:text-white hover:px-2"
+            className="w-fit h-6 px-1 py-0 bg-transparent hover:bg-transparent rounded-none border-0 hover:border border-red-400 text-sm text-white hover:text-white"
             onClick={onDelete}
           >
             Delete
