bc29d4444695153542235eeea750e34a74ded05a
nicholavila <nicholavila1@outlook.com>
2023-07-31 09:17:22 +0000

    move to dynamic data from dummy

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,140 +1,148 @@
+"use client"
+
 import { Poppins } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { LoginButton } from "@/components/auth/login-button";
 import { ProductItem } from "@/components/product/product-item";
 import { Navbar } from "./_components/navbar";
+import { useEffect, useState } from "react";
+import { Product } from "@/shared/product-type";
+import { getAllProducts } from "@/data/products/all-products";
 
 export default function Products() {
-  const products = [
-    {
-      imgPath: '/images/1.jpg',
-      title: 'Product 1',
-      description: 'Product Description 1',
-      price: 100
-    },
-    {
-      imgPath: '/images/2.jpg',
-      title: 'Product 2',
-      description: 'Product Description 2',
-      price: 100
-    },
-    {
-      imgPath: '/images/3.jpg',
-      title: 'Product 3',
-      description: 'Product Description 3',
-      price: 100
-    },
-    {
-      imgPath: '/images/1.jpg',
-      title: 'Product 1',
-      description: 'Product Description 1',
-      price: 100
-    },
-    {
-      imgPath: '/images/2.jpg',
-      title: 'Product 2',
-      description: 'Product Description 2',
-      price: 100
-    },
-    {
-      imgPath: '/images/3.jpg',
-      title: 'Product 3',
-      description: 'Product Description 3',
-      price: 100
-    },
-    {
-      imgPath: '/images/1.jpg',
-      title: 'Product 1',
-      description: 'Product Description 1',
-      price: 100
-    },
-    {
-      imgPath: '/images/2.jpg',
-      title: 'Product 2',
-      description: 'Product Description 2',
-      price: 100
-    },
-    {
-      imgPath: '/images/3.jpg',
-      title: 'Product 3',
-      description: 'Product Description 3',
-      price: 100
-    },
-    {
-      imgPath: '/images/1.jpg',
-      title: 'Product 1',
-      description: 'Product Description 1',
-      price: 100
-    },
-    {
-      imgPath: '/images/2.jpg',
-      title: 'Product 2',
-      description: 'Product Description 2',
-      price: 100
-    },
-    {
-      imgPath: '/images/3.jpg',
-      title: 'Product 3',
-      description: 'Product Description 3',
-      price: 100
-    },
-    {
-      imgPath: '/images/1.jpg',
-      title: 'Product 1',
-      description: 'Product Description 1',
-      price: 100
-    },
-    {
-      imgPath: '/images/2.jpg',
-      title: 'Product 2',
-      description: 'Product Description 2',
-      price: 100
-    },
-    {
-      imgPath: '/images/3.jpg',
-      title: 'Product 3',
-      description: 'Product Description 3',
-      price: 100
-    },
-    {
-      imgPath: '/images/1.jpg',
-      title: 'Product 1',
-      description: 'Product Description 1',
-      price: 100
-    },
-    {
-      imgPath: '/images/2.jpg',
-      title: 'Product 2',
-      description: 'Product Description 2',
-      price: 100
-    },
-    {
-      imgPath: '/images/3.jpg',
-      title: 'Product 3',
-      description: 'Product Description 3',
-      price: 100
-    },
-    {
-      imgPath: '/images/3.jpg',
-      title: 'Product 3',
-      description: 'Product Description 3',
-      price: 100
-    }
-  ]
+  // const products = [
+  //   {
+  //     imgPath: '/images/1.jpg',
+  //     title: 'Product 1',
+  //     description: 'Product Description 1',
+  //     price: 100
+  //   },
+  //   {
+  //     imgPath: '/images/2.jpg',
+  //     title: 'Product 2',
+  //     description: 'Product Description 2',
+  //     price: 100
+  //   },
+  //   {
+  //     imgPath: '/images/3.jpg',
+  //     title: 'Product 3',
+  //     description: 'Product Description 3',
+  //     price: 100
+  //   },
+  //   {
+  //     imgPath: '/images/1.jpg',
+  //     title: 'Product 1',
+  //     description: 'Product Description 1',
+  //     price: 100
+  //   },
+  //   {
+  //     imgPath: '/images/2.jpg',
+  //     title: 'Product 2',
+  //     description: 'Product Description 2',
+  //     price: 100
+  //   },
+  //   {
+  //     imgPath: '/images/3.jpg',
+  //     title: 'Product 3',
+  //     description: 'Product Description 3',
+  //     price: 100
+  //   },
+  //   {
+  //     imgPath: '/images/1.jpg',
+  //     title: 'Product 1',
+  //     description: 'Product Description 1',
+  //     price: 100
+  //   },
+  //   {
+  //     imgPath: '/images/2.jpg',
+  //     title: 'Product 2',
+  //     description: 'Product Description 2',
+  //     price: 100
+  //   },
+  //   {
+  //     imgPath: '/images/3.jpg',
+  //     title: 'Product 3',
+  //     description: 'Product Description 3',
+  //     price: 100
+  //   },
+  //   {
+  //     imgPath: '/images/1.jpg',
+  //     title: 'Product 1',
+  //     description: 'Product Description 1',
+  //     price: 100
+  //   },
+  //   {
+  //     imgPath: '/images/2.jpg',
+  //     title: 'Product 2',
+  //     description: 'Product Description 2',
+  //     price: 100
+  //   },
+  //   {
+  //     imgPath: '/images/3.jpg',
+  //     title: 'Product 3',
+  //     description: 'Product Description 3',
+  //     price: 100
+  //   },
+  //   {
+  //     imgPath: '/images/1.jpg',
+  //     title: 'Product 1',
+  //     description: 'Product Description 1',
+  //     price: 100
+  //   },
+  //   {
+  //     imgPath: '/images/2.jpg',
+  //     title: 'Product 2',
+  //     description: 'Product Description 2',
+  //     price: 100
+  //   },
+  //   {
+  //     imgPath: '/images/3.jpg',
+  //     title: 'Product 3',
+  //     description: 'Product Description 3',
+  //     price: 100
+  //   },
+  //   {
+  //     imgPath: '/images/1.jpg',
+  //     title: 'Product 1',
+  //     description: 'Product Description 1',
+  //     price: 100
+  //   },
+  //   {
+  //     imgPath: '/images/2.jpg',
+  //     title: 'Product 2',
+  //     description: 'Product Description 2',
+  //     price: 100
+  //   },
+  //   {
+  //     imgPath: '/images/3.jpg',
+  //     title: 'Product 3',
+  //     description: 'Product Description 3',
+  //     price: 100
+  //   },
+  //   {
+  //     imgPath: '/images/3.jpg',
+  //     title: 'Product 3',
+  //     description: 'Product Description 3',
+  //     price: 100
+  //   }
+  // ]
+
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    getAllProducts().then(res => {
+      setProducts(res.items);
+    })
+  }, []);
 
   return (
     <main className="w-full flex flex-col pt-6">
       <Navbar title="Products" content="You can see all products here" />
       <div className="w-full flex flex-wrap py-6">
         {products.map((product, index) => (
-          <div className="w-1/4 p-2">
-            <ProductItem
-              key={index}
-              imgPath={product.imgPath}
-              title={product.title}
-              description={product.description}
-              price={product.price} />
+          <div key={index} className="w-1/4 p-2">
+            <ProductItem product={product} />
           </div>
         ))}
       </div>
