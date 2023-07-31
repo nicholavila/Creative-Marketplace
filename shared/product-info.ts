6558ee5a28d1efdd77e8ca8685992818cdc77a45
nicholavila <nicholavila1@outlook.com>
2023-07-31 15:59:56 +0000

    rearrange: add product-info global variables

---
+++ shared/product-info.ts
@@ -0,0 +1,19 @@
+export const PRODCUT_TYPES = [
+  "UI Kit",
+  "Wireframe",
+  "Dashboard",
+  "Plugin",
+  "Icon Sets",
+  "Fonts",
+  "Mockups",
+  "Posters",
+  "Scaffolding",
+  "3D Assets",
+  "Illustrations",
+  "Documents",
+  "Spreadsheets",
+  "Photos",
+  "Videos",
+  "Audio",
+  "Sound Effects"
+];
