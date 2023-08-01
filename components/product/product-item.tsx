48f22543eb016167f52afad4077c38ad48b91e3e
nicholavila <nicholavila1@outlook.com>
2023-08-01 13:05:51 +0000

    fix typescript issue

--- components/product/product-item.tsx
+++ components/product/product-item.tsx
@@ -16,7 +16,7 @@ export const ProductItem = ({ product }: PropsParams) => {
   const [imagePath, setImagePath] = useState<string>("");
 
   useEffect(() => {
-    const s3Link = product.imageList[0];
+    const s3Link = product?.imageList[0];
     getS3ImageLink(s3Link).then(res => {
       if (res.success) {
         setImagePath(res.response as string);
@@ -25,7 +25,7 @@ export const ProductItem = ({ product }: PropsParams) => {
   }, []);
 
   return (
-    <Link href={`/products/details/${product.productId}`} className="w-full">
+    <Link href={`/products/details/${product?.productId}`} className="w-full">
       <Card className="w-full flex flex-col items-center px-0 rounded-none shadow-md cursor-pointer hover:drop-shadow-lg hover:bg-gray-100 hover:translate-x-[-1px] hover:translate-y-[-1px]">
         <CardContent className="w-full p-0 flex flex-col gap-y-4">
           <Avatar className="w-full h-56 rounded-none">
@@ -40,10 +40,10 @@ export const ProductItem = ({ product }: PropsParams) => {
           </Avatar>
           <div className="flex flex-col px-4 pb-4">
             <div className="flex gap-4">
-              <p>{product.title}</p>
-              <p className="text-md text-gray-600">Price: ${product.price}</p>
+              <p>{product?.title}</p>
+              <p className="text-md text-gray-600">Price: ${product?.price}</p>
             </div>
-            <p className="text-lg text-black drop-shadow-md">{product.description}</p>
+            <p className="text-lg text-black drop-shadow-md">{product?.description}</p>
           </div>
         </CardContent>
       </Card>
