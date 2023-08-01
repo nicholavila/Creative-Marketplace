6c48c87f784d72683c769171b9f565dafa4e3f34
nicholavila <nicholavila1@outlook.com>
2023-08-01 08:36:54 +0000

    remove whitespaces

--- shared/product-info.ts
+++ shared/product-info.ts
@@ -1,19 +1,19 @@
 export const PRODCUT_TYPES = [
-  "UI Kit",
+  "UI-Kit",
   "Wireframe",
   "Dashboard",
   "Plugin",
-  "Icon Sets",
+  "Icon-Sets",
   "Fonts",
   "Mockups",
   "Posters",
   "Scaffolding",
-  "3D Assets",
+  "3D-Assets",
   "Illustrations",
   "Documents",
   "Spreadsheets",
   "Photos",
   "Videos",
   "Audio",
-  "Sound Effects"
+  "Sound-Effects"
 ];
