1ad112f22e729dff455e8bbed6fa8977bbd63802
nicholavila <nicholavila1@outlook.com>
2023-08-03 10:22:39 +0000

    rearrange: new data func product by type

---
+++ data/products/products-by-type.ts
@@ -0,0 +1,16 @@
+"use server";
+
+import db from "@/lib/db";
+import { QueryCommand } from "@aws-sdk/lib-dynamodb";
+
+const TableName = process.env.AWS_DYNAMODB_PRODUCTS_TABLE_NAME;
+
+export const getProductsByType = async (productType: string) => {
+  const command = new QueryCommand({
+    TableName,
+    KeyConditionExpression: "productType = :productType",
+    ExpressionAttributeValues: {
+      ":productType": productType
+    }
+  });
+};
