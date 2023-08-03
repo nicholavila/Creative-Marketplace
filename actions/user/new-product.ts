b1c7a2737c2a529637a7626e33c8dbd8d3f94f1d
nicholavila <nicholavila1@outlook.com>
2023-08-03 14:06:33 +0000

    feat: new product action

---
+++ actions/user/new-product.ts
@@ -0,0 +1,18 @@
+"use server";
+
+import { getUserById } from "@/data/user/user-by-id";
+import { updateUserProducts } from "@/data/user/products-update";
+
+type ParamsType = {
+  productType: string;
+  productId: string;
+};
+
+export const addNewProduct = async (userId: string, product: ParamsType) => {
+  const existingUser = await getUserById(userId);
+  if (!existingUser || !existingUser.email) {
+    return { error: "Internal server error" };
+  }
+
+  const products = existingUser.products || [];
+};
