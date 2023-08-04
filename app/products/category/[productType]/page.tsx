3e9b05e2b0f49bd5bc98e7c4719af61665b8b4f8
nicholavila <nicholavila1@outlook.com>
2023-08-04 13:25:31 +0000

    refactor product type page

--- app/products/category/[productType]/page.tsx
+++ app/products/category/[productType]/page.tsx
@@ -1,14 +1,9 @@
 "use client"
 
-import { Poppins } from "next/font/google";
-import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
-import { LoginButton } from "@/components/auth/login-button";
 import { ProductItem } from "@/components/product/product-item";
 import { Navbar } from "../../_components/navbar";
 import { useEffect, useState } from "react";
 import { Product } from "@/shared/product-interface";
-import { getAllProducts } from "@/data/products/all-products";
 import { getProductsByType } from "@/data/products/products-by-type";
 
 type ParamsType = {
