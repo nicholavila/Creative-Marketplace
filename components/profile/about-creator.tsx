ba924df56795a0337bb7316bb3f183a9366dd74c
nicholavila <nicholavila1@outlook.com>
2023-08-04 12:10:25 +0000

    update: populate dynamic data from db

--- components/profile/about-creator.tsx
+++ components/profile/about-creator.tsx
@@ -3,22 +3,27 @@ import { Button } from "../ui/button"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../ui/card"
 import Link from "next/link"
 import { LinkedSites } from "./linked-sites"
-import { CreatorInterface } from "@/shared/user-interface"
+import { Creator } from "@/shared/user-interface"
+import { useCurrentUser } from "@/hooks/use-current-user"
 
 type PropsParams = {
-  creator: CreatorInterface | undefined;
+  creator: Creator | undefined;
 }
 
 export const AboutCreator = ({ creator }: PropsParams) => {
+  const user = useCurrentUser();
+
   return (
     <Card className="border-0 rounded-none">
       <CardHeader className="flex flex-row items-end justify-between">
         <p className="text-xl font-bold">About the Creator</p>
-        <Button variant="default" asChild>
-          <Link href="/user/creator" className="flex gap-x-1">
-            <FaEdit /> Edit
-          </Link>
-        </Button>
+        {user?.id === creator?.userId && (
+          <Button variant="default" asChild>
+            <Link href="/user/creator" className="flex gap-x-1">
+              <FaEdit /> Edit
+            </Link>
+          </Button>
+        )}
       </CardHeader>
       <CardContent>
         <div className="w-full flex justify-between">
@@ -37,5 +42,5 @@ export const AboutCreator = ({ creator }: PropsParams) => {
       </CardContent>
       <CardFooter>
       </CardFooter>
-    </Card>)
+    </Card >)
 }
