fcdab42b62737a4a8f7e302d35f00e596f009faa
nicholavila <nicholavila1@outlook.com>
2023-08-04 16:02:40 +0000

    feat: getter func

--- components/profile/user-collection.tsx
+++ components/profile/user-collection.tsx
@@ -24,7 +24,11 @@ export const UserCollection = ({ userId }: { userId: string }) => {
     getUserById(userId).then(user => {
       if (!ignore) {
         user?.products.map((item: ProductLink) => {
-
+          getProductById(item.productType).then(res => {
+            if (res) {
+              setProducts(prev => [...prev, res]);
+            }
+          })
         })
       }
     })
