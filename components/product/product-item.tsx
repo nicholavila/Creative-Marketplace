1bae12d919c13b7b445254ea4c03526ea4dc8f2a
nicholavila <nicholavila1@outlook.com>
2023-08-04 14:14:39 +0000

    replace dummy data with db

--- components/product/product-item.tsx
+++ components/product/product-item.tsx
@@ -12,11 +12,10 @@ interface PropsParams {
 }
 
 export const ProductItem = ({ product }: PropsParams) => {
-  // const tempImagePath = ["/profile-back-example.jpg", "/product-example.jpg", "/product-example-2.jpg"];
   const [imagePath, setImagePath] = useState<string>("");
 
   useEffect(() => {
-    const s3Link = product?.imageList[0];
+    const s3Link = product.previewList[0];
     getS3ImageLink(s3Link).then(res => {
       if (res.success) {
         setImagePath(res.response as string);
@@ -25,25 +24,24 @@ export const ProductItem = ({ product }: PropsParams) => {
   }, []);
 
   return (
-    <Link href={`/products/details/${product?.productId}`} className="w-full">
+    <Link href={`/products/details/${product.productType}/${product.productId}`} className="w-full">
       <Card className="w-full flex flex-col items-center px-0 rounded-none shadow-md cursor-pointer hover:drop-shadow-lg hover:bg-gray-100 hover:translate-x-[-1px] hover:translate-y-[-1px]">
         <CardContent className="w-full p-0 flex flex-col gap-y-4">
           <Avatar className="w-full h-56 rounded-none">
-            <AvatarImage src={imagePath} />
-            {/* <AvatarImage
-              src={tempImagePath[Math.floor(Math.random() * 100) % 3]}
+            <AvatarImage
+              src={imagePath}
               className="object-fill aspect-auto"
-            /> */}
+            />
             <AvatarFallback className="bg-sky-500">
               <div className="w-full h-full bg-green-700"></div>
             </AvatarFallback>
           </Avatar>
           <div className="flex flex-col px-4 pb-4">
             <div className="flex gap-4">
-              <p>{product?.title}</p>
-              <p className="text-md text-gray-600">Price: ${product?.price}</p>
+              <p>{product.title}</p>
+              <p className="text-base text-black">Price: ${product.price}</p>
             </div>
-            <p className="text-lg text-black drop-shadow-md">{product?.description}</p>
+            <p className="text-base text-gray-700 drop-shadow-md">{product.description}</p>
           </div>
         </CardContent>
       </Card>
