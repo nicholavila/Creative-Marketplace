708682423402ee2151268a77498579afdc4af0fa
nicholavila <nicholavila1@outlook.com>
2023-08-09 10:13:49 +0000

    populate with data in cart page

--- app/(protected)/cart/page.tsx
+++ app/(protected)/cart/page.tsx
@@ -9,20 +9,41 @@ import { Navbar } from "./_components/navbar";
 import { useEffect, useState } from "react";
 import { Product } from "@/shared/product-interface";
 import { useCurrentUser } from "@/hooks/use-current-user";
+import { getUserById } from "@/data/user/user-by-id";
+import { getProductById } from "@/data/products/product-by-id";
+
+type ProductInfo = {
+  productType: string;
+  productId: string;
+}
 
 export default function Cart() {
   const user = useCurrentUser();
   const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
-
+    getUserById(user?.id as string).then(user => {
+      if (user && user.cart) {
+        user.cart.map((product: ProductInfo) => {
+          getProductById(product.productType, product.productId).then(res => {
+            if (res) {
+              setProducts(prev => [...prev, res]);
+            }
+          })
+        })
+      }
+    })
   }, []);
 
   return (
     <main className="w-full flex flex-col pt-6">
       <Navbar title="Your Cart" content="Here are products in your cart" />
       <div className="w-full flex flex-wrap py-6">
-
+        {products.map((product, index) => (
+          <div key={index} className="w-1/4 p-2">
+            <ProductItem product={product} />
+          </div>
+        ))}
       </div>
     </main>
   );
