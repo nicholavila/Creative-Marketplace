5c252b829385a82fbeb25626cbf7ea08727e6af1
nicholavila <nicholavila1@outlook.com>
2023-08-10 08:42:28 +0000

    feat: remove link to page from cart item

--- app/(protected)/cart/_components/cart-item.tsx
+++ app/(protected)/cart/_components/cart-item.tsx
@@ -27,26 +27,32 @@ export const CartItem = ({ product, onSelected }: PropsParams) => {
   }, []);
 
   return (
-    <Link href={`/products/details/${product.productType}/${product.productId}`} className="w-full">
-      <Card className="w-full flex flex-col items-center px-0 rounded-none shadow-md cursor-pointer hover:drop-shadow-lg hover:bg-gray-100 hover:translate-x-[-1px] hover:translate-y-[-1px]">
-        <CardContent className="w-full p-0 flex gap-y-4">
-          <Avatar className="w-72 h-48 rounded-none">
-            <AvatarImage
-              src={imagePath}
-              className="object-fill aspect-auto"
-            />
-            <AvatarFallback className="bg-sky-500">
-              <div className="w-full h-full bg-green-700"></div>
-            </AvatarFallback>
-          </Avatar>
-          <div className="flex flex-col p-4">
-            <div></div>
+
+    <Card className="w-full flex flex-col items-center px-0 rounded-none shadow-md cursor-pointer hover:drop-shadow-lg hover:bg-gray-100 hover:translate-x-[-1px] hover:translate-y-[-1px]">
+      <CardContent className="w-full p-0 flex gap-y-4">
+        <Avatar className="w-72 h-48 rounded-none">
+          <AvatarImage
+            src={imagePath}
+            className="object-fill aspect-auto"
+          />
+          <AvatarFallback className="bg-sky-500">
+            <div className="w-full h-full bg-green-700"></div>
+          </AvatarFallback>
+        </Avatar>
+        <div className="w-full flex flex-col justify-between p-4">
+          <div className="flex flex-col gap-y-2">
             <p>{product.title}</p>
             <p className="text-base text-black">Price: ${product.price}</p>
             <p className="text-base text-gray-700 drop-shadow-md">{product.description}</p>
           </div>
-        </CardContent>
-      </Card>
-    </Link>
+          <div className="flex items-center justify-between">
+            <Button variant={"outline"} className="border-red-700">
+              Remove
+            </Button>
+            <Switch className="h-3/4" checked={product.selected} onCheckedChange={onSelected}></Switch>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
   );
 };
