cd6fb3fa2490772e5fd220e3c9d8fe0820ca7921
nicholavila <nicholavila1@outlook.com>
2023-08-11 12:52:43 +0000

    update navbar with pending state

--- app/(protected)/cart/_components/navbar.tsx
+++ app/(protected)/cart/_components/navbar.tsx
@@ -5,12 +5,13 @@ import { Separator } from "@/components/ui/separator";
 import { FaArrowRight } from "react-icons/fa";
 
 interface PropsParams {
+  isPending: boolean;
   title: string;
   content: string;
   onCheckout: () => void;
 }
 
-export const Navbar = ({ title, content, onCheckout }: PropsParams) => {
+export const Navbar = ({ isPending, title, content, onCheckout }: PropsParams) => {
   return (
     <nav className="w-full flex flex-col gap-y-6 top-28">
       <div className="flex items-end justify-between">
@@ -18,7 +19,12 @@ export const Navbar = ({ title, content, onCheckout }: PropsParams) => {
           <p className="text-3xl text-black font-medium drop-shadow-md">{title}</p>
           <p className="text-md text-gray-600">{content}</p>
         </div>
-        <Button variant="outline" className="flex items-center gap-x-2 border-green-700">
+        <Button
+          disabled={isPending}
+          onClick={onCheckout}
+          variant="outline"
+          className="flex items-center gap-x-2 border-green-700"
+        >
           <FaArrowRight />
           Go to Checkout
         </Button>
