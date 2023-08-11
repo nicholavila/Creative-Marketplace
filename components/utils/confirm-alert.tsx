6269e475055fe250ad8bfc3d08792683cf1e4f49
nicholavila <nicholavila1@outlook.com>
2023-08-11 11:30:18 +0000

    rearrange: confirm-alert

---
+++ components/utils/confirm-alert.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+
+interface LoginButtonProps {
+  open: boolean;
+  children?: React.ReactNode;
+  asChild?: boolean;
+  title?: string;
+  message?: string;
+  onContinue?: () => void,
+}
+
+export const ConfirmAlert = ({ open, children, asChild, title, message, onContinue }: LoginButtonProps) => {
+  return (
+    <AlertDialog open={open}>
+      <AlertDialogTrigger disabled={!onContinue} asChild={asChild}>
+        {children}
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{title}</AlertDialogTitle>
+          <AlertDialogDescription>{message}</AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogAction onClick={onContinue}>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
