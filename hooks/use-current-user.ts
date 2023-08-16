54e288b11b96d7d4c1487c997be92a41bd79c1f4
nicholavila <nicholavila1@outlook.com>
2023-08-16 08:34:12 +0000

    doc: remove comment

--- hooks/use-current-user.ts
+++ hooks/use-current-user.ts
@@ -2,7 +2,5 @@ import { useSession } from "next-auth/react";
 
 export const useCurrentUser = () => {
   const session = useSession();
-  // console.log("__User__Session", session);
-
   return session.data?.user;
 };
