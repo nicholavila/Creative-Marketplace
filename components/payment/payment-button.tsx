3a77d29ff9f5c9c82236b379e6d0f0d055c2f36b
nicholavila <nicholavila1@outlook.com>
2023-08-17 13:11:23 +0000

    update payment button

--- components/payment/payment-button.tsx
+++ components/payment/payment-button.tsx
@@ -4,38 +4,43 @@ import { useRouter } from "next/navigation";
 
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { PaymentForm } from "@/components/payment/payment-form";
+import { useState } from "react";
 
 interface PaymentButtonProps {
-	children: React.ReactNode;
-	mode?: "modal" | "redirect";
-	asChild?: boolean;
+  children: React.ReactNode;
+  mode?: "modal" | "redirect";
+  asChild?: boolean;
+  disabled: boolean;
 }
 
 export const PaymentButton = ({
-	children,
-	mode = "redirect",
-	asChild
+  children,
+  mode = "redirect",
+  asChild,
+  disabled,
 }: PaymentButtonProps) => {
-	const router = useRouter();
+  const router = useRouter();
 
-	const onClick = () => {
-		router.push("/payment/pay");    // payment page
-	};
+  const onClick = () => {
+    router.push("/payment/pay");    // payment page
+  };
 
-	if (mode === "modal") {
-		return (
-			<Dialog>
-				<DialogTrigger asChild={asChild}>{children}</DialogTrigger>
-				<DialogContent className="p-0 w-auto bg-transparent border-none">
-					<PaymentForm />
-				</DialogContent>
-			</Dialog>
-		);
-	}
+  const [opened, setOpened] = useState(false);
 
-	return (
-		<span onClick={onClick} className="cursor-pointer">
-			{children}
-		</span>
-	);
+  if (mode === "modal") {
+    return (
+      <Dialog open={opened} onOpenChange={open => setOpened(open)}>
+        <DialogTrigger disabled={disabled} asChild={asChild}>{children}</DialogTrigger>
+        <DialogContent className="p-0 w-auto bg-transparent border-none">
+          <PaymentForm onCancel={() => setOpened(false)} />
+        </DialogContent>
+      </Dialog>
+    );
+  }
+
+  return (
+    <span onClick={onClick} className="cursor-pointer">
+      {children}
+    </span>
+  );
 };
