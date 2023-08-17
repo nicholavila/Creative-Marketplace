70e14301f148bbd8cd8bf6fb7edacb83013005b1
nicholavila <nicholavila1@outlook.com>
2023-08-17 10:01:55 +0000

    feat: pending option for payment form

--- components/payment/payment-form.tsx
+++ components/payment/payment-form.tsx
@@ -28,90 +28,121 @@ import { useState, useTransition } from "react";
 import { createOrder as createPaypalOrder } from "@/actions/paypal/create-order";
 import { usePathname } from "next/navigation";
 import { createOrder as createStripeOrder } from "@/actions/stripe/create-order";
+import { useAtom } from "jotai";
+import { orderListAtom } from "@/store/orderList";
+import { useCurrentUser } from "@/hooks/use-current-user";
+import { getS3ImageLink } from "@/actions/s3/image-link";
 
-export const PaymentForm = () => {
-	const Option_Paypal = 'option-paypal';
-	const Option_Stripe = 'option-stripe';
+export const PaymentForm = ({ onCancel }: { onCancel: () => void }) => {
+  const Option_Paypal = 'option-paypal';
+  const Option_Stripe = 'option-stripe';
 
-	const [isPending, startTransition] = useTransition();
-	const [paymentMethod, setPaymentMethod] = useState(Option_Paypal);
+  const currentPath = usePathname();
+  const user = useCurrentUser();
 
-	const currentPath = usePathname();
+  const [isPending, startTransition] = useTransition();
+  const [paymentMethod, setPaymentMethod] = useState(Option_Paypal);
+  const [orderList] = useAtom(orderListAtom);
 
-	const onPurchase = async () => {
-		startTransition(async () => {
-			if (paymentMethod === Option_Paypal) {
-				const createdResponse = await createPaypalOrder({ redirectUrl: currentPath });
-				if (createdResponse.success) {
-					window.location.href = createdResponse.result.links[1].href;
-				}
-			} else {
-				const response = await createStripeOrder({ redirectUrl: currentPath });
-				if (response.success) {
-					window.location.href = response.payment?.url ?? '';
-				}
-			}
-		})
-	}
+  const getTotalPrice = () => {
+    return orderList.reduce((total, product) => total + product.price, 0);
+  }
 
-	return (
-		<Card className="w-[480px]">
-			<CardHeader>
-				<CardTitle>Payment</CardTitle>
-				<CardDescription>Select the payment method you prefer</CardDescription>
-			</CardHeader>
-			<CardContent className="flex flex-col gap-y-6">
-				<RadioGroup defaultValue={Option_Paypal} onValueChange={setPaymentMethod} className="w-full flex justify-between">
-					<div className="flex items-center gap-x-4">
-						<RadioGroupItem value={Option_Stripe} id={Option_Stripe} />
-						<Label htmlFor={Option_Stripe} className="flex items-center gap-x-4 text-5xl">
-							<FaCcVisa className="text-emerald-700" />
-							<FaCcMastercard className="text-emerald-700" />
-							<p className="text-lg">Stripe</p>
-						</Label>
-					</div>
-					<div className="flex items-center gap-x-4">
-						<RadioGroupItem value={Option_Paypal} id={Option_Paypal} />
-						<Label htmlFor={Option_Paypal} className="flex items-center gap-x-4 text-5xl">
-							<FaCcPaypal className="text-sky-700" />
-							<p className="text-lg">Paypal</p>
-						</Label>
-					</div>
-				</RadioGroup>
-				<Table>
-					{/* <TableCaption>Table Name</TableCaption> */}
-					<TableHeader>
-						<TableRow>
-							<TableHead className="w-[150px]">Product ID</TableHead>
-							<TableHead>Product Name</TableHead>
-							<TableHead className="text-right">Price</TableHead>
-						</TableRow>
-					</TableHeader>
-					<TableBody>
-						<TableRow>
-							<TableCell className="font-medium">Product 001</TableCell>
-							<TableCell>Product 001</TableCell>
-							<TableCell className="text-right">$100</TableCell>
-						</TableRow>
-					</TableBody>
-					<TableFooter>
-						<TableRow>
-							<TableCell colSpan={2}>Total</TableCell>
-							<TableCell className="text-right">$100</TableCell>
-						</TableRow>
-					</TableFooter>
-				</Table>
-			</CardContent>
-			<CardFooter className="flex flex-col">
-				<div className="w-full flex justify-between">
-					<Button variant="destructive" className="flex gap-x-2">
-						<MdCancel /> Cancel	{/** Not Working Now */}
-					</Button>
-					<Button className="flex gap-x-2" disabled={isPending} onClick={onPurchase}>
-						<AiFillCreditCard />Purchase
-					</Button>
-				</div>
-			</CardFooter>
-		</Card>
-	)
+  const onPurchase = async () => {
+    startTransition(async () => {
+      // # CHECK FIRST
+      // # Whether this product is already purchased
+
+      if (paymentMethod === Option_Paypal) {
+        const createdResponse = await createPaypalOrder({
+          redirectUrl: currentPath,
+          amount: getTotalPrice()
+        });
+        if (createdResponse.success) {
+          window.location.href = createdResponse.result.links[1].href;
+        }
+      } else {
+        const product = orderList[0];
+        const res = await getS3ImageLink(product.previewList[0]);
+        const imageLink = res.success ? res.response : "";
+
+        const response = await createStripeOrder({
+          redirectUrl: currentPath,
+          userEmail: user?.email as string,
+          product: {
+            name: orderList.length === 1 ? product.title : "Products",
+            description: orderList.length === 1 ? product.description : "Muiple products",
+            images: [imageLink as string]
+          },
+          amount: getTotalPrice()
+        });
+        if (response.success) {
+          window.location.href = response.payment?.url ?? '';
+        }
+      }
+    })
+  }
+
+  return (
+    <Card className="w-[480px]">
+      <CardHeader>
+        <CardTitle>Payment</CardTitle>
+        <CardDescription>Select the payment method you prefer</CardDescription>
+      </CardHeader>
+      <CardContent className="w-full flex flex-col gap-y-6">
+        <RadioGroup defaultValue={Option_Paypal} onValueChange={setPaymentMethod} className="w-full flex justify-between">
+          <div className="flex items-center gap-x-4 cursor-pointer">
+            <RadioGroupItem value={Option_Stripe} id={Option_Stripe} />
+            <Label htmlFor={Option_Stripe} className="flex items-center gap-x-4 text-5xl cursor-pointer">
+              <FaCcVisa className="text-emerald-700" />
+              <FaCcMastercard className="text-emerald-700" />
+              <p className="text-lg">Stripe</p>
+            </Label>
+          </div>
+          <div className="flex items-center gap-x-4 cursor-pointer">
+            <RadioGroupItem value={Option_Paypal} id={Option_Paypal} />
+            <Label htmlFor={Option_Paypal} className="flex items-center gap-x-4 text-5xl cursor-pointer">
+              <FaCcPaypal className="text-sky-700" />
+              <p className="text-lg">Paypal</p>
+            </Label>
+          </div>
+        </RadioGroup>
+        <Table className="w-full flex flex-col">
+          {/* <TableCaption>Table Name</TableCaption> */}
+          <TableHeader className="w-full">
+            <TableRow className="w-full flex">
+              <TableHead className="w-1/4">Product Title</TableHead>
+              <TableHead className="w-7/12">Product Description</TableHead>
+              <TableHead className="w-1/6 text-right">Price</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody className="w-full">
+            {orderList.map(product => (
+              <TableRow className="w-full flex">
+                <TableCell className="w-1/4 truncate font-medium">{product.title}</TableCell>
+                <TableCell className="w-7/12 truncate">{product.description}sdafsadfdsafsdafdsaf</TableCell>
+                <TableCell className="w-1/6 truncate text-right">${product.price}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+          <TableFooter className="w-full">
+            <TableRow className="w-full flex">
+              <TableCell className="w-5/6" colSpan={2}>Total</TableCell>
+              <TableCell className="w-1/6 text-right">${getTotalPrice()}</TableCell>
+            </TableRow>
+          </TableFooter>
+        </Table>
+      </CardContent>
+      <CardFooter className="flex flex-col">
+        <div className="w-full flex justify-between">
+          <Button disabled={isPending} onClick={onCancel} variant="destructive" className="flex gap-x-2">
+            <MdCancel /> Cancel	{/** Not Working Now */}
+          </Button>
+          <Button className="flex gap-x-2" disabled={isPending} onClick={onPurchase}>
+            <AiFillCreditCard />Purchase
+          </Button>
+        </div>
+      </CardFooter>
+    </Card>
+  )
 }
