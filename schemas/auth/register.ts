9c4a41c042be4a7ff764dc3846ded1609a87fd0d
nicholavila <nicholavila1@outlook.com>
2023-08-18 15:57:50 +0000

    update: schema for general details form

--- schemas/auth/register.ts
+++ schemas/auth/register.ts
@@ -4,16 +4,30 @@ import { z } from "zod";
 export const GeneralDetailsSchema = z.object({
   username: z
     .string()
-    .min(1, "A name is required")
-    .max(72, "Name must be a maximum of 72 characters"),
+    .min(1, "Username is required")
+    .max(72, "Username must be a maximum of 72 characters"),
   firstname: z
     .string()
-    .min(1, "A name is required")
-    .max(72, "Name must be a maximum of 72 characters"),
-  lastname: z
+    .min(1, "First name is required")
+    .max(72, "First name must be a maximum of 72 characters"),
+  lastname: z.string().optional(),
+  address1: z
     .string()
-    .min(1, "A name is required")
-    .max(72, "Name must be a maximum of 72 characters"),
+    .min(1, "Address1 is required")
+    .max(72, "Address1 must be a maximum of 72 characters"),
+  address2: z.string().optional(),
+  city: z
+    .string()
+    .min(1, "City is required")
+    .max(72, "City must be a maximum of 72 characters"),
+  postal: z
+    .string()
+    .min(1, "Postal code is required")
+    .max(72, "Postal code must be a maximum of 72 characters"),
+  country: z
+    .string()
+    .min(1, "Country is required")
+    .max(72, "Country must be a maximum of 72 characters"),
   email: z.string().email({ message: "Please enter a valid email" }),
   password: z
     .string()
