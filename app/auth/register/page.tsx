2f8175bfcb5f0de9944fdbf53deaf59194fd6d56
nicholavila <nicholavila1@outlook.com>
2023-08-21 11:29:31 +0000

    update: padding of register page

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -2,7 +2,7 @@ import { GeneralDetailsForm } from "@/components/auth/register/general-details-f
 
 const RegisterPage = () => {
   return (
-    <div className="w-[640px] flex flex-col gap-y-12">
+    <div className="w-[640px] flex flex-col pt-6 gap-y-12">
       <p className="text-4xl font-semibold">Let's get you started</p>
       <GeneralDetailsForm />
     </div>
