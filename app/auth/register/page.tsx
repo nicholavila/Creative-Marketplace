52f9cfe6225e0b11dec12427a31e020ece06415a
nicholavila <nicholavila1@outlook.com>
2023-08-22 08:28:26 +0000

    feat: next step listener

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,10 +1,15 @@
+"use client";
+
 import { GeneralDetailsForm } from "@/components/auth/register/general-details-form";
+import { useState } from "react";
 
 const RegisterPage = () => {
+  const [step, setStep] = useState<number>(0);
+
   return (
     <div className="w-[640px] flex flex-col pt-6 gap-y-12">
       <p className="text-4xl font-semibold">Let's get you started</p>
-      <GeneralDetailsForm />
+      <GeneralDetailsForm onContinue={() => setStep((prev) => prev + 1)} />
     </div>
   );
 };
