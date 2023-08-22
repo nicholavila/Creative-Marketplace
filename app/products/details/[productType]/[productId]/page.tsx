f15aaed517ae9e817b708d2b0cef496be63a1ac0
nicholavila <nicholavila1@outlook.com>
2023-08-22 09:25:23 +0000

    refactor style of product detail page

--- app/products/details/[productType]/[productId]/page.tsx
+++ app/products/details/[productType]/[productId]/page.tsx
@@ -28,40 +28,35 @@ import { orderListAtom } from "@/store/orderList";
 import { addProductToPurchased } from "@/actions/user/add-product-to-purchased";
 
 const Bold = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <span className="font-bold text-xl">
-      {children}
-    </span>
-  )
-}
+  return <span className="font-bold text-xl">{children}</span>;
+};
 
 const Thumbnail = (props: {
-  path: string,
-  focused: boolean,
-  onItemSelected: () => void
+  path: string;
+  focused: boolean;
+  onItemSelected: () => void;
 }) => {
   return (
     <div
       onMouseEnter={props.onItemSelected}
-      className={`w-28 h-16 border-[2px] cursor-pointer ${props.focused && 'border-green-700'}`}
+      className={`w-28 h-16 border-[2px] cursor-pointer ${props.focused && "border-green-700"}`}
     >
-      <Avatar className={`w-full h-full rounded-none border-[1px] border-white`}>
+      <Avatar
+        className={`w-full h-full rounded-none border-[1px] border-white`}
+      >
         <AvatarImage src={props.path} className="object-center object-fill" />
         <AvatarFallback className="bg-sky-500">
           <div className="w-full h-full bg-inherit"></div>
         </AvatarFallback>
       </Avatar>
     </div>
-  )
-}
+  );
+};
 
-export default function ProductDetails({ params }: {
-  params: ProductLink
-}
-) {
-  const Gateway_Paypal = 'paypal';
-  const Gateway_Stripe = 'stripe';
-  const Gateway_Cancelled = 'cancelled';
+export default function ProductDetails({ params }: { params: ProductLink }) {
+  const Gateway_Paypal = "paypal";
+  const Gateway_Stripe = "stripe";
+  const Gateway_Cancelled = "cancelled";
 
   const searchParams = useSearchParams();
   const user = useCurrentUser();
