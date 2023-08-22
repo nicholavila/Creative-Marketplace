18e2945cc9a2bbbc615baa53d85d1bfad76a49ac
nicholavila <nicholavila1@outlook.com>
2023-08-22 13:58:39 +0000

    feat: save user data on main page

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -2,16 +2,31 @@
 
 import { GeneralDetailsForm } from "@/components/auth/register/general-details-form";
 import { SelectAccounts } from "@/components/auth/register/select-accounts";
+import { GeneralDetailsSchema } from "@/schemas/auth/register";
 import { useState } from "react";
+import { z } from "zod";
 
 const RegisterPage = () => {
+  const [userData, setUserData] = useState<any>({});
   const [step, setStep] = useState<number>(0);
+
+  const onGeneralDetailsContinue = (
+    values: z.infer<typeof GeneralDetailsSchema>
+  ) => {
+    setUserData((prev: any) => ({
+      ...prev,
+      ...values
+    }));
+  };
 
   return (
     <div className="w-[640px] flex flex-col pt-6 gap-y-12">
       <p className="text-4xl font-semibold">Let's get you started</p>
       {step === 0 && (
-        <GeneralDetailsForm onContinue={() => setStep((prev) => prev + 1)} />
+        <GeneralDetailsForm
+          defaultValue={userData}
+          onContinue={onGeneralDetailsContinue}
+        />
       )}
       {step === 1 && (
         <SelectAccounts
