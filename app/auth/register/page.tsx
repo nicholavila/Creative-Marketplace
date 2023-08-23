0d9727e52234521eb466b2b580b43f99eeacfef9
nicholavila <nicholavila1@outlook.com>
2023-08-23 15:00:59 +0000

    set default data for each step

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -117,13 +117,13 @@ const RegisterPage = () => {
       <p className="text-4xl font-semibold">Let's get you started</p>
       {step === 0 && (
         <GeneralDetailsForm
-          defaultData={userData}
+          defaultData={userData.generalDetails}
           onContinue={onGeneralDetailsContinue}
         />
       )}
       {step === 1 && (
         <SelectAccounts
-          defaultData={userData}
+          defaultData={userData.selectedAccounts}
           onContinue={onSelectAccountsContinue}
           onBack={onSelectAccountsBack}
         />
