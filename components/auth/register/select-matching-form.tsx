81eebb0f840331572af6a8645553e7a796e149db
nicholavila <nicholavila1@outlook.com>
2023-08-30 12:17:43 +0000

    feat: arrange matching sites

--- components/auth/register/select-matching-form.tsx
+++ components/auth/register/select-matching-form.tsx
@@ -48,6 +48,45 @@ export const SelectMatchingForm = ({
       <p className="text-xl text-green-700">
         4. Please confirm your accounts on other creative markets.
       </p>
+      <Evanto
+        value={matchings.env}
+        setValue={(value) => setMatchings({ ...matchings, env: value })}
+      />
+      <Behance
+        value={matchings.beh}
+        setValue={(value) => setMatchings({ ...matchings, beh: value })}
+      />
+      <ArtStation
+        value={matchings.art}
+        setValue={(value) => setMatchings({ ...matchings, art: value })}
+      />
+      <Dribble
+        value={matchings.drb}
+        setValue={(value) => setMatchings({ ...matchings, drb: value })}
+      />
+      <CreativeMarket
+        value={matchings.cmk}
+        setValue={(value) => setMatchings({ ...matchings, cmk: value })}
+      />
+      <div className="w-full flex items-center justify-between mt-4">
+        <Button
+          disabled={isPending}
+          variant={"outline"}
+          className="w-64 flex gap-x-4 border-red-700"
+          onClick={onBackClicked}
+        >
+          <FaArrowLeft />
+          Back
+        </Button>
+        <Button
+          disabled={isPending}
+          className="w-64 flex gap-x-4"
+          onClick={onContinueClicked}
+        >
+          <FaArrowRight />
+          Next
+        </Button>
+      </div>
     </div>
   );
 };
