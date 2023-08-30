3be954e7b6654464929e8e300dce466ccd894d21
nicholavila <nicholavila1@outlook.com>
2023-08-30 14:59:24 +0000

    feat: complete steps for registration page

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -157,8 +157,215 @@ const RegisterPage = () => {
     setStep((prev) => prev - 1);
   };
 
+  const onSelectMatchingContinue = (
+    values: SignedUpData["creatorMatchings"]
+  ) => {
+    setUserData((prev) => ({ ...prev, creatorMatchings: values }));
+    setStep((prev) => prev + 1);
+  };
+
+  const onSelectMatchingBack = (values: SignedUpData["creatorMatchings"]) => {
+    setUserData((prev) => ({ ...prev, creatorMatchings: values }));
+    setStep((prev) => prev - 1);
+  };
+
+  const uploadImages = async () => {
+    const formData = new FormData();
+    if (userData.creatorDetails.avatar)
+      formData.append("file1", userData.creatorDetails.avatar);
+    if (userData.creatorDetails.cover)
+      formData.append("file2", userData.creatorDetails.cover);
+
+    if (userData.creatorDetails.avatar || userData.creatorDetails.cover) {
+      try {
+        const response = await axiosClient.post(
+          "/multi-upload",
+          formData,
+          axiosConfig
+        );
+        const data = response.data;
+
+        if (data.success) {
+          return data.pathList;
+        } else {
+          return [];
+        }
+      } catch (error) {
+        return [];
+      }
+    } else {
+      return [];
+    }
+  };
+
+  const getCreatorData = () => {
+    const scraped: any = {};
+    if (userData.creatorMatchings.env) {
+      scraped["env"] = {};
+    } else if (userData.creatorMatchings.beh) {
+      scraped["beh"] = {};
+    } else if (userData.creatorMatchings.art) {
+      scraped["art"] = {};
+    } else if (userData.creatorMatchings.drb) {
+      scraped["drb"] = {};
+    } else if (userData.creatorMatchings.cmk) {
+      scraped["cmk"] = {};
+    }
+
+    const creator: any = {
+      ...userData.generalDetails,
+      creator: {
+        ...userData.creatorDetails
+      },
+      scraped
+    };
+
+    if (userData.selectedAccounts.user) {
+      creator["userRoleId"] = uuidv4();
+    }
+
+    if (userData.selectedAccounts.affiliate) {
+      creator["affiliateId"] = uuidv4();
+    }
+
+    return creator;
+  };
+
+  const onCreatorCompleteContinue = () => {
+    startTransition(() => {
+      setIsDisabled(true);
+      const creator = getCreatorData();
+
+      uploadImages().then((pathList) => {
+        if (userData.creatorDetails.avatar) {
+          if (pathList.length > 0) {
+            creator.creator.avatar = pathList[0];
+          } else {
+            delete creator.creator.avatar;
+          }
+
+          if (userData.creatorDetails.cover) {
+            if (pathList.length > 1) {
+              creator.creator.cover = pathList[1];
+            } else {
+              delete creator.creator.cover;
+            }
+          }
+        } else if (userData.creatorDetails.cover) {
+          if (pathList.length > 0) {
+            creator.creator.cover = pathList[0];
+          } else {
+            delete creator.creator.cover;
+          }
+        }
+
+        register(creator).then((res) => {
+          setConfirmOpen(true);
+          setIsDisabled(false);
+          if (res.success) {
+            setConfirmTitle("Success");
+            setConfirmMessage("A new creator was newly registerd!");
+            setStep((prev) => prev + 1);
+          } else {
+            setConfirmTitle("Error");
+            setConfirmMessage(res.error as string);
+          }
+        });
+      });
+    });
+  };
+
+  const onCreatorCompleteBack = () => {
+    setStep((prev) => prev - 1);
+  };
+
+  const onUserCompleteContinue = () => {
+    if (userData.selectedAccounts.creator) {
+      setConfirmOpen(true);
+      setConfirmTitle("Success");
+      setConfirmMessage("A new user was newly registerd!");
+      setStep((prev) => prev + 1);
+    } else {
+      startTransition(() => {
+        const user: any = { ...userData.generalDetails, userRoleId: uuidv4() };
+        if (userData.selectedAccounts.affiliate) {
+          user["affiliateId"] = uuidv4();
+        }
+
+        register(user).then((res) => {
+          setConfirmOpen(true);
+          setIsDisabled(false);
+          if (res.success) {
+            setConfirmTitle("Success");
+            setConfirmMessage("A new user was newly registerd!");
+            setStep((prev) => prev + 1);
+          } else {
+            setConfirmTitle("Error");
+            setConfirmMessage(res.error as string);
+          }
+        });
+      });
+    }
+  };
+
+  const onUserCompleteBack = () => {
+    if (userData.selectedAccounts.creator) {
+      setConfirmOpen(true);
+      setConfirmTitle("Warning");
+      setConfirmMessage(
+        "You can't go backward since you already registered a creator!"
+      );
+    } else {
+      setStep((prev) => prev - 1);
+    }
+  };
+
+  const onAffiliateCompleteContinue = () => {
+    if (userData.selectedAccounts.creator || userData.selectedAccounts.user) {
+      setConfirmOpen(true);
+      setConfirmTitle("Success");
+      setConfirmMessage("A new affiliate was newly registerd!");
+      setStep((prev) => prev + 1);
+    } else {
+      startTransition(() => {
+        register({ ...userData.generalDetails, affiliateId: uuidv4() }).then(
+          (res) => {
+            setConfirmOpen(true);
+            setIsDisabled(false);
+            if (res.success) {
+              setConfirmTitle("Success");
+              setConfirmMessage("A new affiliate was newly registerd!");
+              setStep((prev) => prev + 1);
+            } else {
+              setConfirmTitle("Error");
+              setConfirmMessage(res.error as string);
+            }
+          }
+        );
+      });
+    }
+  };
+
+  const onAffiliateCompleteBack = () => {
+    if (!userData.selectedAccounts.creator && userData.selectedAccounts.user) {
+      setConfirmOpen(true);
+      setConfirmTitle("Warning");
+      setConfirmMessage(
+        "You can't go backward since you already registered a user!"
+      );
+    } else {
+      setStep((prev) => prev - 1);
+    }
+  };
+
   return (
     <div className="w-[640px] flex flex-col pt-6 gap-y-12">
+      <ConfirmAlert
+        open={isConfirmOpen}
+        title={confirmTitle}
+        message={confirmMessage}
+        onOK={() => setConfirmOpen(false)}
+      />
       <p className="text-4xl font-semibold">Let's get you started</p>
       {step === 0 && (
         <GeneralDetailsForm
