ad8c5e29ad80fb366d1b6b85f6bc88000d8c0158
nicholavila <nicholavila1@outlook.com>
2023-08-31 15:24:45 +0000

    rearrange and update: product type page

--- app/products/[productType]/page.tsx
+++ app/products/[productType]/page.tsx
@@ -1,7 +1,7 @@
-"use client"
+"use client";
 
 import { ProductItem } from "@/components/product/product-item";
-import { Navbar } from "../../_components/navbar";
+import { Navbar } from "../_components/navbar";
 import { useEffect, useState } from "react";
 import { Product } from "@/shared/types-product";
 import { getProductsByType } from "@/data/products/products-by-type";
@@ -9,22 +9,25 @@ import { getProductsByType } from "@/data/products/products-by-type";
 type ParamsType = {
   params: {
     productType: string;
-  }
-}
+  };
+};
 
 export default function Products({ params }: ParamsType) {
   const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
-    getProductsByType(params.productType).then(res => {
+    getProductsByType(params.productType).then((res) => {
       console.log(res);
       setProducts(res.items);
-    })
+    });
   }, []);
 
   return (
     <main className="w-full flex flex-col pt-6">
-      <Navbar title={`${params.productType} Products`} content={`You can see all ${params.productType} products here`} />
+      <Navbar
+        title={`${params.productType} Products`}
+        content={`You can see all ${params.productType} products here`}
+      />
       <div className="w-full flex flex-wrap py-6">
         {products.map((product, index) => (
           <div key={index} className="w-1/4 p-2">
