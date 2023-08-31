2ccbfd1650a2e3345ccb51ba00fc7908d57936a7
nicholavila <nicholavila1@outlook.com>
2023-08-31 13:36:27 +0000

    put all product types

--- assets/product-types.ts
+++ assets/product-types.ts
@@ -9,8 +9,8 @@ export const PRODCUT_TYPES: CategoryInfo[] = [
     name: "UI Kits"
   },
   {
-    key: "dashboards-wireframes",
-    name: "Dashboards / Wireframes"
+    key: "dashboard-wireframes",
+    name: "Dashboard / Wireframes"
   },
   {
     key: "themes-spas",
@@ -19,5 +19,49 @@ export const PRODCUT_TYPES: CategoryInfo[] = [
   {
     key: "plugins",
     name: "Plugins"
+  },
+  {
+    key: "icon-sets-brushes-actions",
+    name: "Icon Sets / Brushes / Actions"
+  },
+  {
+    key: "fonts",
+    name: "Fonts"
+  },
+  {
+    key: "mockups-posters",
+    name: "Mockups / Posters"
+  },
+  {
+    key: "scaffolding-starters",
+    name: "Scaffolding / Starters"
+  },
+  {
+    key: "3d-assets",
+    name: "3D Assets"
+  },
+  {
+    key: "illustrations",
+    name: "Illustrations"
+  },
+  {
+    key: "documents-page-templates",
+    name: "Documents / Page Templates"
+  },
+  {
+    key: "spreadsheets",
+    name: "Spreadsheets"
+  },
+  {
+    key: "photos",
+    name: "Photos"
+  },
+  {
+    key: "video",
+    name: "Video"
+  },
+  {
+    key: "audio-sound-effects",
+    name: "Audio / Sound Effects"
   }
 ];
