0d6abb204700f2aa1da2124e3b831fc94c091b39
nicholavila <nicholavila1@outlook.com>
2023-09-04 09:13:54 +0000

    update creator complete form

--- components/auth/register/creator-complete-form.tsx
+++ components/auth/register/creator-complete-form.tsx
@@ -1,20 +1,105 @@
 "use client";
 
-import { useState, useTransition } from "react";
+import { Dispatch, SetStateAction, useState, useTransition } from "react";
 import { Button } from "@/components/ui/button";
 import { FaArrowLeft, FaUser } from "react-icons/fa";
 import { ConfirmAlert } from "@/components/utils/confirm-alert";
+import { SignedUpData } from "@/shared/types-user";
 
 type Props = {
-  pending: boolean;
-  onContinue: () => void;
-  onBack: () => void;
+  userData: SignedUpData;
+  setUserData: Dispatch<SetStateAction<SignedUpData>>;
+  moveStepForward: () => void;
+  moveStepBackward: () => void;
 };
 
-export const CreatorCompleteForm = ({ pending, onContinue, onBack }: Props) => {
+export const CreatorCompleteForm = ({
+  userData,
+  setUserData,
+  moveStepForward,
+  moveStepBackward
+}: Props) => {
   const [isPending, startTransition] = useTransition();
+  const [isDisabled, setDisabled] = useState<boolean>(false);
   const [isConfirmOpen, setConfirmOpen] = useState<boolean>(false);
   const [confirmMessage, setConfirmMessage] = useState<string>("");
+
+  const getCreatorData = () => {
+    const scraped: any = {};
+    if (userData.creatorMatchings.env) {
+      scraped["env"] = {};
+    } else if (userData.creatorMatchings.beh) {
+      scraped["beh"] = {};
+    } else if (userData.creatorMatchings.art) {
+      scraped["art"] = {};
+    } else if (userData.creatorMatchings.drb) {
+      scraped["drb"] = {};
+    } else if (userData.creatorMatchings.cmk) {
+      scraped["cmk"] = {};
+    }
+
+    const creator: any = {
+      ...userData.generalDetails,
+      creator: {
+        ...userData.creatorDetails
+      },
+      scraped
+    };
+
+    if (userData.selectedAccounts.user) {
+      creator["userRoleId"] = uuidv4();
+    }
+
+    if (userData.selectedAccounts.affiliate) {
+      creator["affiliateId"] = uuidv4();
+    }
+
+    return creator;
+  };
+
+  const onContinue = () => {
+    startTransition(() => {
+      setDisabled(true);
+      const creator = getCreatorData();
+
+      uploadImages().then((pathList) => {
+        if (userData.creatorDetails.avatar) {
+          if (pathList.length > 0) {
+            creator.creator.avatar = pathList[0];
+          } else {
+            delete creator.creator.avatar;
+          }
+
+          if (userData.creatorDetails.cover) {
+            if (pathList.length > 1) {
+              creator.creator.cover = pathList[1];
+            } else {
+              delete creator.creator.cover;
+            }
+          }
+        } else if (userData.creatorDetails.cover) {
+          if (pathList.length > 0) {
+            creator.creator.cover = pathList[0];
+          } else {
+            delete creator.creator.cover;
+          }
+        }
+
+        register(creator).then((res) => {
+          setConfirmOpen(true);
+          setDisabled(false);
+          if (res.success) {
+            setConfirmTitle("Success");
+            setConfirmMessage("A new creator was newly registerd!");
+            setStep((prev) => prev + 1);
+          } else {
+            setConfirmTitle("Error");
+            setConfirmMessage(res.error as string);
+          }
+        });
+      });
+    });
+  };
 
   return (
     <div className="w-full flex flex-col gap-y-6">
