d816779af0a0284006a4879354e3c38381d624f6
nicholavila <nicholavila1@outlook.com>
2023-09-04 15:28:34 +0000

    refactor main page

--- app/page.tsx
+++ app/page.tsx
@@ -47,23 +47,44 @@ const Home = () => {
         </Avatar>
       ))} */}
       <div className="w-1/2 flex flex-col items-center gap-y-24 text-center">
-        <h1 className={cn("text-7xl font-semibold text-green-700 drop-shadow-md", font.className)}>
+        <h1
+          className={cn(
+            "text-7xl font-semibold text-green-700 drop-shadow-md",
+            font.className
+          )}
+        >
           Bring your creative ideas to life!
         </h1>
-        <div className='w-full space-y-4 text-left text-black text-lg'>
-          <p>/** This site is scheduled to launch officially on May 15th! - [ Need to be updated ]</p>
-          <p>You can bring creative features coming and Opt In to those programs. - [ Need to be updated ]</p>
-          <p>You can pre-register as a customer and will get big benefits by being a early subscriber! **/</p>
+        <div className="w-full space-y-4 text-left text-black text-lg">
+          <p>
+            /** This site is scheduled to launch officially on May 15th! - [
+            Need to be updated ]
+          </p>
+          <p>
+            You can bring creative features coming and Opt In to those programs.
+            - [ Need to be updated ]
+          </p>
+          <p>
+            You can pre-register as a customer and will get big benefits by
+            being a early subscriber! **/
+          </p>
         </div>
-        <div className='flex flex-col items-center gap-y-4'>
-          <p className='text-2xl font-bold'>You can sign-up now as either a Seller or a User!</p>
-          <Button className='w-1/2 animate-pulse' variant="default" asChild size="lg">
+        <div className="flex flex-col items-center gap-y-4">
+          <p className="text-2xl font-bold">
+            You can sign-up now as either a Seller or a User!
+          </p>
+          <Button
+            className="w-1/2 animate-pulse"
+            variant="default"
+            asChild
+            size="lg"
+          >
             <Link href="/auth/register">Sign-Up Now</Link>
           </Button>
         </div>
       </div>
     </main>
   );
-}
+};
 
-export default Home
+export default Home;
