d5bcc435efd7e381bee73faa581beb24d65325a5
nicholavila <nicholavila1@outlook.com>
2023-09-05 08:19:43 +0000

    move logic into sub compos

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,17 +1,13 @@
 "use client";
 
-import { register } from "@/actions/auth/register/register";
 import { CreatorCompleteForm } from "@/components/auth/register/creator-complete-form";
 import { CreatorDetailsForm } from "@/components/auth/register/creator-details-form";
 import { GeneralDetailsForm } from "@/components/auth/register/general-details-form";
 import { SelectAccountsForm } from "@/components/auth/register/select-accounts-form";
 import { SelectMatchingForm } from "@/components/auth/register/select-matching-form";
 import { UserCompleteForm } from "@/components/auth/register/user-complete-form";
-import { ConfirmAlert } from "@/components/utils/confirm-alert";
-import { axiosClient, axiosConfig } from "@/lib/axios";
 import { SignedUpData } from "@/shared/types-user";
 import { useState, useTransition } from "react";
-import { v4 as uuidv4 } from "uuid";
 import { AffiliateCompleteForm } from "@/components/auth/register/affiliate-complete-form";
 import { RegisterCompleteForm } from "@/components/auth/register/register-complete-form";
 
@@ -60,10 +56,6 @@ const RegisterPage = () => {
   const [step, setStep] = useState<number>(0);
   const [isPending, startTransition] = useTransition();
   const [isDisabled, setIsDisabled] = useState<boolean>(false);
-
-  const [isConfirmOpen, setConfirmOpen] = useState<boolean>(false);
-  const [confirmTitle, setConfirmTitle] = useState<string>("");
-  const [confirmMessage, setConfirmMessage] = useState<string>("");
 
   const moveStepForward = () => {
     setStep(step + 1);
@@ -124,52 +116,8 @@ const RegisterPage = () => {
     return step === _step;
   };
 
-  const onAffiliateCompleteContinue = () => {
-    if (userData.selectedAccounts.creator || userData.selectedAccounts.user) {
-      setConfirmOpen(true);
-      setConfirmTitle("Success");
-      setConfirmMessage("A new affiliate was newly registerd!");
-      setStep((prev) => prev + 1);
-    } else {
-      startTransition(() => {
-        register({ ...userData.generalDetails, affiliateId: uuidv4() }).then(
-          (res) => {
-            setConfirmOpen(true);
-            setIsDisabled(false);
-            if (res.success) {
-              setConfirmTitle("Success");
-              setConfirmMessage("A new affiliate was newly registerd!");
-              setStep((prev) => prev + 1);
-            } else {
-              setConfirmTitle("Error");
-              setConfirmMessage(res.error as string);
-            }
-          }
-        );
-      });
-    }
-  };
-
-  const onAffiliateCompleteBack = () => {
-    if (!userData.selectedAccounts.creator && userData.selectedAccounts.user) {
-      setConfirmOpen(true);
-      setConfirmTitle("Warning");
-      setConfirmMessage(
-        "You can't go backward since you already registered a user!"
-      );
-    } else {
-      setStep((prev) => prev - 1);
-    }
-  };
-
   return (
     <div className="w-[640px] flex flex-col pt-6 gap-y-12">
-      <ConfirmAlert
-        open={isConfirmOpen}
-        title={confirmTitle}
-        message={confirmMessage}
-        onOK={() => setConfirmOpen(false)}
-      />
       <p className="text-4xl font-semibold">Let's get you started</p>
       {step === 0 && (
         <GeneralDetailsForm
@@ -221,10 +169,11 @@ const RegisterPage = () => {
       )}
       {isAffiliateStep() && (
         <AffiliateCompleteForm
-          pending={!isActive()}
           step={step}
-          onContinue={onAffiliateCompleteContinue}
-          onBack={onAffiliateCompleteBack}
+          userData={userData}
+          setUserData={setUserData}
+          moveStepForward={moveStepForward}
+          moveStepBackward={moveStepBackward}
         />
       )}
       {isRegisterCompleteStep() && <RegisterCompleteForm step={step} />}
