e0b410418e1e44dd8c093d9db7f959fbf6464bcd
nicholavila <nicholavila1@outlook.com>
2023-09-07 11:36:37 +0000

    rearrange: layout for approval

---
+++ app/(management)/approval/layout.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const ApprovalLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div className="w-full">{children}</div>;
+};
+
+export default ApprovalLayout;
