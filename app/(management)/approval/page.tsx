ac64b5365e4d0f617fdd0e3193da5b6086f08d1b
nicholavila <nicholavila1@outlook.com>
2023-09-07 09:31:45 +0000

    rearrange: approval page

---
+++ app/(management)/approval/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { ProductItem } from "@/components/product/product-item";
+import { Navbar } from "./_components/navbar";
+import { useEffect, useState } from "react";
+import { Product } from "@/shared/types/types-product";
+import { getAllProducts } from "@/data/products/all-products";
+
+export default function Approval() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    getAllProducts().then((res) => {
+      setProducts(res.items);
+    });
+  }, []);
+
+  return (
+    <main className="w-full flex flex-col pt-6">
+      <Navbar title="Products" content="You can see all products here" />
+      <div className="w-full flex flex-wrap py-6">
+        {products.map((product, index) => (
+          <div key={index} className="w-1/4 p-2">
+            <ProductItem product={product} _url="/approval/product" />
+          </div>
+        ))}
+      </div>
+    </main>
+  );
+}
