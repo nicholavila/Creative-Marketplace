fb1df61293a5e3028a44795396c519db1ec275cb
nicholavila <nicholavila1@outlook.com>
2023-09-07 14:55:08 +0000

    fix check-general details for update data structure

--- actions/auth/register/check-general-details.ts
+++ actions/auth/register/check-general-details.ts
@@ -1,30 +1,24 @@
 "use server";
 
-import { z } from "zod";
 import { getAllUsernames } from "@/data/user/all-usernames";
 import { getUserByEmail } from "@/data/user/user-by-email";
-import { GeneralDetailsSchema } from "@/schemas/auth/register";
 
-export const checkGeneralDetails = async (
-  values: z.infer<typeof GeneralDetailsSchema>
-) => {
-  const validateFields = GeneralDetailsSchema.safeParse(values);
-  if (!validateFields.success) {
-    return { error: "Server Says Fields are Invalid!" };
-  }
+type Params = {
+  username: string;
+  email: string;
+};
 
+export const checkGeneralDetails = async ({ username, email }: Params) => {
   // Check whether username selected is available
   const response = await getAllUsernames();
   const similarId = response.items.find((item: { username: string }) =>
-    item.username.includes(values.username)
+    item.username.includes(username)
   );
   if (similarId) {
     return { error: "Same or Very similar Username Already Exists!" };
   }
 
-  const { email } = validateFields.data;
   const existingUser = await getUserByEmail(email);
-
   if (existingUser) {
     return { error: "Email already in use!" };
   }
