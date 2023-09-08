b155e5408c1dd24dc226049d0d12d3d65a70c44d
nicholavila <nicholavila1@outlook.com>
2023-09-08 09:49:38 +0000

    update nvabar of product page

--- app/products/_components/navbar.tsx
+++ app/products/_components/navbar.tsx
@@ -4,17 +4,19 @@ import { Separator } from "@/components/ui/separator";
 
 interface PropsParams {
   title: string;
-  content: string
+  content: string;
 }
 
 export const Navbar = ({ title, content }: PropsParams) => {
   return (
-    <nav className="w-full flex flex-col gap-y-6 top-28">
+    <nav className="w-full flex flex-col gap-y-4">
       <div className="flex flex-col gap-y-2">
-        <p className="text-3xl text-black font-medium drop-shadow-md">{title}</p>
+        <p className="text-3xl text-black font-medium drop-shadow-md">
+          {title}
+        </p>
         <p className="text-md text-gray-600">{content}</p>
       </div>
       <Separator className="h-[1px]" />
-    </nav >
+    </nav>
   );
 };
