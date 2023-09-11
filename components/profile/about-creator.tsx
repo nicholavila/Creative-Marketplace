b82912f38b65cef7654e9faff8fb5bdf98517fef
nicholavila <nicholavila1@outlook.com>
2023-09-11 14:20:25 +0000

    update about-creator tab

--- components/profile/about-creator.tsx
+++ components/profile/about-creator.tsx
@@ -1,14 +1,14 @@
-import { FaEdit } from "react-icons/fa"
-import { Button } from "../ui/button"
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../ui/card"
-import Link from "next/link"
-import { LinkedSites } from "./linked-sites"
-import { Creator } from "@/shared/types-user"
-import { useCurrentUser } from "@/hooks/use-current-user"
+import { FaEdit } from "react-icons/fa";
+import { Button } from "../ui/button";
+import { Card, CardContent, CardFooter, CardHeader } from "../ui/card";
+import Link from "next/link";
+import { LinkedSites } from "./linked-sites";
+import { useCurrentUser } from "@/hooks/use-current-user";
+import { User } from "@/shared/types/types-user";
 
 type PropsParams = {
-  creator: Creator | undefined;
-}
+  creator: User | undefined;
+};
 
 export const AboutCreator = ({ creator }: PropsParams) => {
   const user = useCurrentUser();
@@ -28,19 +28,29 @@ export const AboutCreator = ({ creator }: PropsParams) => {
       <CardContent>
         <div className="w-full flex justify-between">
           <div className="w-1/2 flex flex-col gap-y-2">
-            <p className="text-sky-700 font-medium">/** Scraped data will be pre-populated here. **/</p>
-            <p>Name: <span className="font-semibold">{`${creator?.firstname} ${creator?.lastname}`}</span></p>
-            <p>Description: {creator?.bio}</p>
+            <p className="text-sky-700 font-medium">
+              /** Scraped data will be pre-populated here. **/
+            </p>
+            <p>
+              Name:{" "}
+              <span className="font-semibold">{`${creator?.firstname} ${creator?.lastname}`}</span>
+            </p>
+            <p>
+              Description:{" "}
+              <span className="font-semibold">{creator?.creator?.bio}</span>
+            </p>
             <p>Specialization: </p>
             <p>Contact: </p>
           </div>
           <div className="w-2/5 flex flex-col gap-y-6">
-            <p className="text-xl font-medium">Your profiles on other Creative markets</p>
+            <p className="text-xl font-medium">
+              Your profiles on other Creative markets
+            </p>
             <LinkedSites disabled showButton={false} />
           </div>
         </div>
       </CardContent>
-      <CardFooter>
-      </CardFooter>
-    </Card >)
-}
+      <CardFooter></CardFooter>
+    </Card>
+  );
+};
