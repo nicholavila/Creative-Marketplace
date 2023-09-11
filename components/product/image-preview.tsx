00c3f7c5f1298e4ffe9a061ae863540d293f9ce3
nicholavila <nicholavila1@outlook.com>
2023-09-11 12:12:40 +0000

    feat: image preview element

--- components/product/image-preview.tsx
+++ components/product/image-preview.tsx
@@ -6,19 +6,30 @@ type PropsParams = {
   src: string;
   onPreview: () => void;
   onDelete: () => void;
-}
+};
 
-export const ImagePreview = ({ disabled, src, onPreview, onDelete }: PropsParams) => {
+export const ImagePreview = ({
+  disabled,
+  src,
+  onPreview,
+  onDelete
+}: PropsParams) => {
   const [isHover, setHover] = useState<boolean>(false);
 
   return (
-    <div className="relative cursor-pointer" onMouseOver={() => setHover(true)} onMouseOut={() => setHover(false)}>
+    <div
+      className="relative cursor-pointer"
+      onMouseOver={() => setHover(true)}
+      onMouseOut={() => setHover(false)}
+    >
       <img src={src} className="h-28" />
-      <div className={`absolute top-0 right-0 z-10 w-full h-full flex items-center justify-center bg-gray-700/70 ${!isHover && 'hidden'}`}>
+      <div
+        className={`absolute top-0 right-0 z-10 w-full h-full flex items-center justify-center bg-gray-700/70 ${!isHover && "hidden"}`}
+      >
         <div className="flex flex-col items-center gap-y-2">
           <Button
             variant="outline"
-            className="w-fit h-6 px-1 py-0 bg-transparent hover:bg-transparent rounded-none border-0 hover:border border-green-400 text-sm text-white hover:text-white"
+            className="w-fit h-6 px-1 py-0 bg-transparent hover:bg-transparent rounded-none border-0 hover:border-b-2 border-green-400 text-sm text-white hover:text-white"
             onClick={onPreview}
           >
             Preview
@@ -26,7 +37,7 @@ export const ImagePreview = ({ disabled, src, onPreview, onDelete }: PropsParams
           <Button
             disabled={disabled}
             variant="outline"
-            className="w-fit h-6 px-1 py-0 bg-transparent hover:bg-transparent rounded-none border-0 hover:border border-red-400 text-sm text-white hover:text-white"
+            className="w-fit h-6 px-1 py-0 bg-transparent hover:bg-transparent rounded-none border-0 hover:border-b-2 border-red-400 text-sm text-white hover:text-white"
             onClick={onDelete}
           >
             Delete
@@ -34,5 +45,5 @@ export const ImagePreview = ({ disabled, src, onPreview, onDelete }: PropsParams
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
