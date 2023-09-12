8d2f226b14684614d10ed1a26977439733703072
nicholavila <nicholavila1@outlook.com>
2023-09-12 15:18:50 +0000

    update payment form

--- components/payment/payment-form.tsx
+++ components/payment/payment-form.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 
 import {
   Card,
@@ -6,8 +6,8 @@ import {
   CardDescription,
   CardFooter,
   CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
+  CardTitle
+} from "@/components/ui/card";
 import {
   Table,
   TableBody,
@@ -16,14 +16,14 @@ import {
   TableFooter,
   TableHead,
   TableHeader,
-  TableRow,
-} from "@/components/ui/table"
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
+  TableRow
+} from "@/components/ui/table";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 
-import { MdCancel } from 'react-icons/md';
+import { MdCancel } from "react-icons/md";
 import { AiFillCreditCard } from "react-icons/ai";
 import { Label } from "@/components/ui/label";
-import { FaCcVisa, FaCcMastercard, FaCcPaypal, } from "react-icons/fa";
+import { FaCcVisa, FaCcMastercard, FaCcPaypal } from "react-icons/fa";
 import { useState, useTransition } from "react";
 import { createOrder as createPaypalOrder } from "@/actions/paypal/create-order";
 import { usePathname } from "next/navigation";
@@ -31,11 +31,11 @@ import { createOrder as createStripeOrder } from "@/actions/stripe/create-order"
 import { useAtom } from "jotai";
 import { orderListAtom } from "@/store/orderList";
 import { useCurrentUser } from "@/hooks/use-current-user";
-import { getS3ImageLink } from "@/actions/s3/image-link";
+import { getLinkFromS3 } from "@/actions/s3/link-from-s3";
 
 export const PaymentForm = ({ onCancel }: { onCancel: () => void }) => {
-  const Option_Paypal = 'option-paypal';
-  const Option_Stripe = 'option-stripe';
+  const Option_Paypal = "option-paypal";
+  const Option_Stripe = "option-stripe";
 
   const currentPath = usePathname();
   const user = useCurrentUser();
@@ -46,7 +46,7 @@ export const PaymentForm = ({ onCancel }: { onCancel: () => void }) => {
 
   const getTotalPrice = () => {
     return orderList.reduce((total, product) => total + product.price, 0);
-  }
+  };
 
   const onPurchase = async () => {
     startTransition(async () => {
@@ -63,7 +63,7 @@ export const PaymentForm = ({ onCancel }: { onCancel: () => void }) => {
         }
       } else {
         const product = orderList[0];
-        const res = await getS3ImageLink(product.previewList[0]);
+        const res = await getLinkFromS3(product.previewList[0]);
         const imageLink = res.success ? res.response : "";
 
         const response = await createStripeOrder({
@@ -71,17 +71,18 @@ export const PaymentForm = ({ onCancel }: { onCancel: () => void }) => {
           userEmail: user?.email as string,
           product: {
             name: orderList.length === 1 ? product.title : "Products",
-            description: orderList.length === 1 ? product.description : "Muiple products",
+            description:
+              orderList.length === 1 ? product.description : "Muiple products",
             images: [imageLink as string]
           },
           amount: getTotalPrice()
         });
         if (response.success) {
-          window.location.href = response.payment?.url ?? '';
+          window.location.href = response.payment?.url ?? "";
         }
       }
-    })
-  }
+    });
+  };
 
   return (
     <Card className="w-[480px]">
