ffa88a650c628bb0c5e24196f505aac33240f28b
nicholavila <nicholavila1@outlook.com>
2023-09-13 08:44:57 +0000

    fix: query command for product type

--- data/products/products-by-type.ts
+++ data/products/products-by-type.ts
@@ -1,23 +1,35 @@
 "use server";
 
 import db from "@/lib/db";
-import { QueryCommand } from "@aws-sdk/lib-dynamodb";
+import { QueryCommand, QueryCommandInput } from "@aws-sdk/lib-dynamodb";
 
 const TableName = process.env.AWS_DYNAMODB_PRODUCTS_TABLE_NAME;
 
-export const getProductsByType = async (productType: string) => {
-  const command = new QueryCommand({
+export const getProductsByType = async (
+  productType: string,
+  exclusiveStartKey: string
+) => {
+  const queryCommand: QueryCommandInput = {
     TableName,
     KeyConditionExpression: "productType = :productType",
     ExpressionAttributeValues: {
       ":productType": productType
-    }
-  });
+    },
+    Limit: 1
+  };
+
+  if (exclusiveStartKey) {
+    // queryCommand.ExclusiveStartKey = exclusiveStartKey;
+  }
+
+  const command = new QueryCommand(queryCommand);
 
   try {
     const response = await db.send(command);
+    console.log(response);
     return {
       items: response.Items
+      // lastEvaluatedKey: response.LastEvaluatedKey
     };
   } catch (error) {
     return {
