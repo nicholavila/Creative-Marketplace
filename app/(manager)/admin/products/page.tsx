573eed020eef015fc57b8c45f9adfdc5f0d2d98c
nicholavila <nicholavila1@outlook.com>
2023-09-19 08:27:30 +0000

    feat: set table on products page

--- app/(manager)/admin/products/page.tsx
+++ app/(manager)/admin/products/page.tsx
@@ -2,18 +2,57 @@
 
 import { ProductItem } from "@/components/product/product-item";
 import { Navbar } from "../_components/navbar";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useTransition } from "react";
 import { Product } from "@/shared/types/types-product";
 import { getAllProducts } from "@/data/products/all-products";
+import { getColumnsForProductsTable } from "@/components/admin/products-column";
+import {
+  ColumnFiltersState,
+  SortingState,
+  VisibilityState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable
+} from "@tanstack/react-table";
 
 export default function Approval() {
+  const [isPending, startTransition] = useTransition();
   const [products, setProducts] = useState<Product[]>([]);
+
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
+  const [rowSelection, setRowSelection] = useState({});
 
   useEffect(() => {
     getAllProducts().then((res) => {
       setProducts(res.items);
     });
   }, []);
+
+  const columns = getColumnsForProductsTable({ isPending });
+
+  const table = useReactTable({
+    data: products,
+    columns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      rowSelection
+    }
+  });
 
   return (
     <main className="w-full flex flex-col">
