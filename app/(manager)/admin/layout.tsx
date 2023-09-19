23f82550bce7910d4763045066406963b6e9d57a
nicholavila <nicholavila1@outlook.com>
2023-09-19 15:31:50 +0000

    fix: admin board style

--- app/(manager)/admin/layout.tsx
+++ app/(manager)/admin/layout.tsx
@@ -2,7 +2,7 @@ import { SideNavBar } from "@/components/admin/side-navbar";
 
 const AdminLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="w-full min-h-full flex">
+    <div className="w-full min-h-[calc(100vh-80px)] flex">
       <SideNavBar />
       <div className="w-full px-12 pt-6 pb-24">{children}</div>
     </div>
