108d3653b054658fffb3a482a4c9535873a82885
nicholavila <nicholavila1@outlook.com>
2023-09-20 11:45:39 +0000

    feat: add column for bundles

--- app/(manager)/admin/bundles/page.tsx
+++ app/(manager)/admin/bundles/page.tsx
@@ -1,6 +1,40 @@
+"use client";
+
 import { Navbar } from "../_components/navbar";
+import { useEffect, useState, useTransition } from "react";
+import {
+  ColumnFiltersState,
+  SortingState,
+  VisibilityState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable
+} from "@tanstack/react-table";
+import { ChevronDown } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow
+} from "@/components/ui/table";
 
 const ManagementBundles = () => {
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
+  const [rowSelection, setRowSelection] = useState({});
+
   return (
     <div className="w-full flex">
       <Navbar title="Bundles" content="You can manage bundles" />
