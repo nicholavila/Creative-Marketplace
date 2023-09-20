63cc044c4acd5d02f76eb68c62fdd08629c752a2
nicholavila <nicholavila1@outlook.com>
2023-09-20 15:52:52 +0000

    feat: create bundle

---
+++ data/bundles/bundle-create.ts
@@ -0,0 +1,23 @@
+"use server";
+
+import db from "@/lib/db";
+import { Bundle } from "@/shared/types/types-bundles";
+import { PutCommand } from "@aws-sdk/lib-dynamodb";
+
+const TableName = process.env.AWS_DYNAMODB_BUNDLES_TABLE_NAME;
+
+export const createBundle = async (data: Bundle) => {
+  const command = new PutCommand({
+    TableName,
+    Item: {
+      ...data
+    }
+  });
+
+  try {
+    await db.send(command);
+    return { success: true };
+  } catch (error) {
+    return { success: false, error };
+  }
+};
