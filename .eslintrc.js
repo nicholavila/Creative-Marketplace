550fc06005ea9122351b845a53756831702096f7
nicholavila <nicholavila1@outlook.com>
2023-09-20 09:42:09 +0000

    chore: eslint setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,9 @@
 module.exports = {
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  extends: [
+    "next/core-web-vitals",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
   root: true,
   parser: "@typescript-eslint/parser",
   parserOptions: {
