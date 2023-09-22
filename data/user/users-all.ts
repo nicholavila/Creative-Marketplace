40d914d80a6a68c463762e711959e5c3d52c33a3
nicholavila <nicholavila1@outlook.com>
2023-09-22 15:57:35 +0000

    update get all users with pagination

--- data/user/users-all.ts
+++ data/user/users-all.ts
@@ -1,19 +1,26 @@
 "use server";
 
 import db from "@/lib/db";
-import { ScanCommand } from "@aws-sdk/lib-dynamodb";
+import { ScanCommand, ScanCommandInput } from "@aws-sdk/lib-dynamodb";
 
 const TableName = process.env.AWS_DYNAMODB_USERS_TABLE_NAME;
 
-export const getAllUsers = async () => {
-  const command = new ScanCommand({
+export const getAllUsers = async (exclusiveStartKey?: string) => {
+  const scanCommandInput: ScanCommandInput = {
     TableName
-  });
+  };
+  if (exclusiveStartKey) {
+    scanCommandInput.ExclusiveStartKey = {
+      userId: exclusiveStartKey as string
+    };
+  }
+  const command = new ScanCommand(scanCommandInput);
 
   try {
     const response = await db.send(command);
     return {
-      items: response.Items
+      items: response.Items,
+      lastEvaluatedKey: response.LastEvaluatedKey
     };
   } catch (error) {
     return {
