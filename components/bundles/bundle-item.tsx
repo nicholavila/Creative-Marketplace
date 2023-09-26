1fad3102e39f230f95060cda6794713487f73aff
nicholavila <nicholavila1@outlook.com>
2023-09-26 11:16:04 +0000

    complete bundle item

--- components/bundles/bundle-item.tsx
+++ components/bundles/bundle-item.tsx
@@ -3,6 +3,8 @@ import { Card, CardContent, CardHeader } from "../ui/card";
 import { useEffect, useState } from "react";
 import { Product } from "@/shared/types/product.type";
 import { getProductById } from "@/data/products/product-by-id";
+import { ProductImage } from "./product-image";
+import Link from "next/link";
 
 type Props = {
   bundle: Bundle;
@@ -29,13 +31,19 @@ export const BundleItem = ({ bundle }: Props) => {
   }, []);
 
   return (
-    <Card className="w-full hover:-translate-x-[2px] hover:-translate-y-[2px] hover:drop-shadow transition all ease-out">
-      <CardHeader>{bundle.title}</CardHeader>
-      <CardContent className="max-w-full w-full">
-        {products.map((product, index) =>
-          index < 4 ? <div className="flex">Something</div> : null
-        )}
-      </CardContent>
-    </Card>
+    <Link href={`/bundles/${bundle.bundleId}`} passHref>
+      <Card className="w-full pr-6 hover:-translate-x-[2px] hover:-translate-y-[2px] hover:drop-shadow transition all ease-out cursor-pointer">
+        <CardHeader>{bundle.title}</CardHeader>
+        <CardContent className="max-w-full w-full flex gap-x-4 overflow-hidden">
+          {products.map((product, index) =>
+            index < 3 ? (
+              <div className="flex">
+                <ProductImage product={product} />
+              </div>
+            ) : null
+          )}
+        </CardContent>
+      </Card>
+    </Link>
   );
 };
