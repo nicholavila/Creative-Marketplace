30023bc7ea3fe16fc4cd168509d0e22bbc9dc616
nicholavila <nicholavila1@outlook.com>
2023-09-26 13:37:54 +0000

    feat: add pagination buttons

--- app/bundles/page.tsx
+++ app/bundles/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import { BundleItem } from "@/components/bundles/bundle-item";
+import { Button } from "@/components/ui/button";
 import { getAllBundles } from "@/data/bundles/bundles-all";
 import { Bundle } from "@/shared/types/bundles.type";
 import { useEffect, useState } from "react";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
 const BundlePage = () => {
   const [bundles, setBundles] = useState<Bundle[]>([]);
@@ -16,7 +18,19 @@ const BundlePage = () => {
 
   return (
     <div className="w-full h-full flex flex-col gap-y-6 p-6 bg-gray-100">
-      <p className="text-2xl font-semibold">Bundles</p>
+      <div className="w-full flex justify-between">
+        <p className="text-2xl font-semibold">Bundles</p>
+        <div className="flex gap-x-4">
+          <Button variant={"outline"} className="flex gap-x-2">
+            <FaArrowLeft />
+            Previous
+          </Button>
+          <Button variant={"outline"} className="flex gap-x-2">
+            Next
+            <FaArrowRight />
+          </Button>
+        </div>
+      </div>
       <div className="w-full flex flex-wrap">
         {bundles.map((bundle, index) => (
           <div
