1b0f9636d7f0bcc1de32a58ec1b1abb05dc59ad0
nicholavila <nicholavila1@outlook.com>
2023-09-26 09:32:11 +0000

    update: bundle page

--- app/bundles/page.tsx
+++ app/bundles/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { BundleItem } from "@/components/bundles/bundle-item";
 import { getAllBundles } from "@/data/bundles/bundles-all";
 import { Bundle } from "@/shared/types/bundles.type";
 import { useEffect, useState } from "react";
@@ -16,7 +17,16 @@ const BundlePage = () => {
   return (
     <div className="w-full flex flex-col gap-y-6 p-6">
       <p className="text-2xl font-semibold">Bundles</p>
-      <div className="w-full flex flex-wrap gap-6"></div>
+      <div className="w-full flex flex-wrap">
+        {bundles.map((bundle, index) => (
+          <div
+            className={`w-1/2 pb-6 ${index % 2 === 0 ? "pr-3" : "pl-3"}`}
+            key={bundle.bundleId}
+          >
+            <BundleItem bundle={bundle} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
