a5d57313bb45553ecf4749ade8f43e185fe06019
nicholavila <nicholavila1@outlook.com>
2023-09-26 14:05:55 +0000

    rearrange: get bundles by state

---
+++ data/bundles/bundles-by-state.ts
@@ -0,0 +1,48 @@
+"use server";
+
+import { QueryCommand, QueryCommandInput } from "@aws-sdk/lib-dynamodb";
+
+import db from "@/lib/db";
+
+import type { Bundle, BundleState } from "@/shared/types/bundles.type";
+
+const TableName = process.env.AWS_DYNAMODB_BUNDLES_TABLE_NAME;
+
+export const getAllBundlesByState = async (
+  state: BundleState,
+  limit?: number,
+  exclusiveStartKey?: string
+) => {
+  const queryCommand: QueryCommandInput = {
+    TableName,
+    KeyConditionExpression: "#state = :state",
+    ExpressionAttributeNames: {
+      "#state": "state"
+    },
+    ExpressionAttributeValues: {
+      ":state": state
+    }
+  };
+
+  if (limit) {
+    queryCommand.Limit = limit;
+  }
+
+  if (exclusiveStartKey) {
+    queryCommand.ExclusiveStartKey = {
+      bundleId: exclusiveStartKey
+    };
+  }
+
+  const command = new QueryCommand(queryCommand);
+
+  try {
+    const response = await db.send(command);
+    return {
+      items: response.Items as Bundle[],
+      lastEvaluatedKey: response.LastEvaluatedKey
+    };
+  } catch (error) {
+    return [];
+  }
+};
