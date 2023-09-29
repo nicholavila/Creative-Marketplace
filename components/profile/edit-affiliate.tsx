02be1fe3b86fee46b493826e5a714f7ed54aba57
nicholavila <nicholavila1@outlook.com>
2023-09-29 10:11:06 +0000

    chore: clean up

--- components/profile/edit-affiliate.tsx
+++ components/profile/edit-affiliate.tsx
@@ -1,13 +1,3 @@
-import { Poppins } from "next/font/google";
-import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
-import { LoginButton } from "@/components/auth/login-button";
-import { Navbar } from "../../app/profile/_components/navbar";
-
 export default function EditAffiliate() {
-  return (
-    <main className="w-full flex flex-col">
-      <Navbar title="Affiliate Registration" content="Register as an affiliate" />
-    </main>
-  );
+  return <main className="w-full flex flex-col">Edit Affiliate</main>;
 }
