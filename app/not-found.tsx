12385225595eea364ebdac62f446c86a8e09ea95
nicholavila <nicholavila1@outlook.com>
2023-09-29 14:31:49 +0000

    update not-found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,7 +5,7 @@ import { Button } from "../components/ui/button";
 const NotFound = () => {
   return (
     <div className="w-full h-full flex flex-col items-center justify-center">
-      <h1>404 - Not Found</h1>
+      <h2 className="font-semibold">404 - Not Found</h2>
       <Button variant={"link"}>
         <Link href="/">Go back home</Link>
       </Button>
