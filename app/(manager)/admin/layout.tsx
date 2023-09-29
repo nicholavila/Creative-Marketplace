8322868251092a73adde2e584ba2bc2a8039098d
nicholavila <nicholavila1@outlook.com>
2023-09-29 12:38:48 +0000

    chore: clean up

--- app/(manager)/admin/layout.tsx
+++ app/(manager)/admin/layout.tsx
@@ -1,16 +1,8 @@
 "use client";
 
 import { SideNavBar } from "@/components/admin/utils/side-navbar";
-import { useCurrentRole } from "@/hooks/use-current-role";
-import { notFound } from "next/navigation";
 
 const AdminLayout = ({ children }: { children: React.ReactNode }) => {
-  const role = useCurrentRole();
-
-  if (!role.isManager) {
-    return notFound();
-  }
-
   return (
     <div className="w-full min-h-[calc(100vh-80px)] flex">
       <SideNavBar />
