307d6dfa3c6b39c483c258bfce397f72e136ae6b
nicholavila <nicholavila1@outlook.com>
2023-10-02 13:46:23 +0000

    update: schema for general details

--- schemas/auth/register.ts
+++ schemas/auth/register.ts
@@ -11,30 +11,30 @@ export const GeneralDetailsSchema = z.object({
     .min(1, "First name is required")
     .max(72, "First name must be a maximum of 72 characters"),
   lastname: z.string().optional(),
-  address1: z
-    .string()
-    .min(1, "Address1 is required")
-    .max(72, "Address1 must be a maximum of 72 characters"),
-  address2: z.string().optional(),
-  city: z
-    .string()
-    .min(1, "City is required")
-    .max(72, "City must be a maximum of 72 characters"),
-  postal: z
-    .string()
-    .min(1, "Postal code is required")
-    .max(72, "Postal code must be a maximum of 72 characters"),
-  country: z
-    .string()
-    .min(1, "Country is required")
-    .max(72, "Country must be a maximum of 72 characters"),
-  phone1: z.string().optional(),
-  phone2: z.string().optional(),
   email: z.string().email({ message: "Please enter a valid email" }),
   password: z
     .string()
     .min(6, "Passwords must be at least 6 characters long")
     .max(32, "Passwords must be a maximum of 32 characters")
+  // address1: z
+  //   .string()
+  //   .min(1, "Address1 is required")
+  //   .max(72, "Address1 must be a maximum of 72 characters"),
+  // address2: z.string().optional(),
+  // city: z
+  //   .string()
+  //   .min(1, "City is required")
+  //   .max(72, "City must be a maximum of 72 characters"),
+  // postal: z
+  //   .string()
+  //   .min(1, "Postal code is required")
+  //   .max(72, "Postal code must be a maximum of 72 characters"),
+  // country: z
+  //   .string()
+  //   .min(1, "Country is required")
+  //   .max(72, "Country must be a maximum of 72 characters"),
+  // phone1: z.string().optional(),
+  // phone2: z.string().optional()
 });
 
 export const SelectAccountsSchema = z.object({
