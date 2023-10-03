fe0f99b65742b3b4883026e108f740dc987b9b88
nicholavila <nicholavila1@outlook.com>
2023-10-03 12:53:51 +0000

    update: user settings page

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useTransition } from "react";
+import { useRef, useState, useTransition } from "react";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 
@@ -19,25 +19,43 @@ import {
   FormMessage
 } from "@/components/ui/form";
 import { Separator } from "@/components/ui/separator";
-import { ProfileSchema } from "@/schemas/user";
-import { Textarea } from "@/components/ui/textarea";
 import { Header } from "./_components/header";
-import { SwitchBox } from "@/components/utils/switch-box";
+import { RoleSwitchBox } from "@/components/utils/role-switch-box";
+import { useCurrentUser } from "@/hooks/use-current-user";
+import { GeneralDetailsSchema } from "@/schemas/auth/register";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { FaUser } from "react-icons/fa";
 
 export default function Profile() {
+  const user = useCurrentUser();
+
   const [error, setError] = useState<string | undefined>("");
   const [success, setSuccess] = useState<string | undefined>("");
   const [isPending, startTransition] = useTransition();
 
-  const form = useForm<z.infer<typeof ProfileSchema>>({
-    resolver: zodResolver(ProfileSchema),
+  const [avatar, setAvatar] = useState<File>();
+  const [avatarPath, setAvatarPath] = useState<string>();
+
+  const hiddenAvatarFileInput = useRef<HTMLInputElement>(null);
+  const onAvatarChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      setAvatarPath(URL.createObjectURL(e.target.files[0]));
+      setAvatar(e?.target?.files?.[0]);
+    }
+  };
+
+  const form = useForm<z.infer<typeof GeneralDetailsSchema>>({
+    resolver: zodResolver(GeneralDetailsSchema),
     defaultValues: {
-      username: "",
-      bio: ""
+      username: user?.username,
+      firstname: user?.firstname,
+      lastname: user?.lastname,
+      email: user?.email,
+      password: user?.password
     }
   });
 
-  const onSubmit = (values: z.infer<typeof ProfileSchema>) => {
+  const onSubmit = (values: z.infer<typeof GeneralDetailsSchema>) => {
     setError("");
     setSuccess("");
 
@@ -56,24 +74,18 @@ export default function Profile() {
       <Separator />
       <div className="w-full flex flex-col gap-y-4">
         <h3 className="text-base font-medium">Your roles opened</h3>
-        <div className="w-full grid grid-cols-3 gap-x-6">
-          <SwitchBox
-            mode="small"
+        <div className="w-1/2 grid grid-cols-3 gap-x-6">
+          <RoleSwitchBox
             title="Creator"
-            content="You have started journey for a creator"
-            isChecked={false}
+            isChecked={user?.creator?.isCreator || false}
           />
-          <SwitchBox
-            mode="small"
+          <RoleSwitchBox
             title="Customer"
-            content="You joined as a customer"
-            isChecked={false}
+            isChecked={user?.customer?.isCustomer || false}
           />
-          <SwitchBox
-            mode="small"
+          <RoleSwitchBox
             title="Affiliate"
-            content="You have started an affiliate's life"
-            isChecked={false}
+            isChecked={user?.affiliate?.isAffiliate || false}
           />
         </div>
       </div>
@@ -82,37 +94,137 @@ export default function Profile() {
           onSubmit={form.handleSubmit(onSubmit)}
           className="w-1/2 flex flex-col gap-y-6"
         >
-          <FormField
-            control={form.control}
-            name="username"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Username</FormLabel>
-                <FormControl>
-                  <Input placeholder="John Doe" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          {/** No NEED to show Bio here. */}
-          <FormField
-            control={form.control}
-            name="bio"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Bio</FormLabel>
-                <FormControl>
-                  <Textarea
-                    {...field}
-                    disabled={isPending}
-                    placeholder="Tell us a little bit about yourself"
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+          <div className="w-full flex items-end gap-x-6">
+            <div className="w-1/2 flex flex-col items-start gap-y-4">
+              <FormLabel>Avatar Image</FormLabel>
+              <div className="flex items-end gap-x-6">
+                <Avatar className="w-24 h-24 rounded-sm">
+                  <AvatarImage src={avatarPath} />
+                  <AvatarFallback className="bg-sky-500 rounded-sm">
+                    <FaUser className="text-white" />
+                  </AvatarFallback>
+                </Avatar>
+                <Button
+                  disabled={isPending}
+                  type="button"
+                  variant={"outline"}
+                  size={"sm"}
+                  className="rounded-none"
+                  onClick={() => hiddenAvatarFileInput.current?.click()}
+                >
+                  Upload New
+                </Button>
+                <Input
+                  className="hidden"
+                  type="file"
+                  accept="image/*"
+                  ref={hiddenAvatarFileInput}
+                  onChange={onAvatarChanged}
+                />
+              </div>
+            </div>
+            <div className="w-1/2">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem className="w-full">
+                    <FormLabel>Username*</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        disabled={isPending}
+                        placeholder="johndoe"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+          </div>
+          <div className="w-full flex gap-x-6">
+            <div className="w-1/2">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem className="w-full">
+                    <FormLabel>Email*</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        disabled={isPending}
+                        placeholder="username@mail.com"
+                        type="email"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="w-1/2">
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem className="w-full">
+                    <FormLabel>Password*</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        disabled={isPending}
+                        placeholder="******"
+                        type="password"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+          </div>
+          <div className="w-full flex gap-x-6">
+            <div className="w-1/2">
+              <FormField
+                control={form.control}
+                name="firstname"
+                render={({ field }) => (
+                  <FormItem className="w-full">
+                    <FormLabel>First Name*</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        disabled={isPending}
+                        placeholder="John"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="w-1/2">
+              <FormField
+                control={form.control}
+                name="lastname"
+                render={({ field }) => (
+                  <FormItem className="w-full">
+                    <FormLabel>Last Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        disabled={isPending}
+                        placeholder="Doe"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+          </div>
           <FormError message={error} />
           <FormSuccess message={success} />
           <Button disabled={isPending} type="submit" className="w-64 self-end">
