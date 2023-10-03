98ca8fc123619a466238bb83e8be720fab34a6f1
nicholavila <nicholavila1@outlook.com>
2023-10-03 11:51:25 +0000

    rearrange: role switch box

---
+++ components/utils/role-switch-box.tsx
@@ -0,0 +1,17 @@
+import { Switch } from "../ui/switch";
+
+type Props = {
+  title: string;
+  isChecked: boolean;
+};
+
+export const RoleSwitchBox = ({ title, isChecked }: Props) => {
+  return (
+    <div className="w-full flex items-center justify-between rounded-lg border p-4">
+      <div className="space-y-0.5">
+        <p className="text-black font-medium text-base">{title}</p>
+      </div>
+      <Switch checked={isChecked} />
+    </div>
+  );
+};
