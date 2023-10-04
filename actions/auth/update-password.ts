3885219e42fb40598d611c68ed1e25e767dd9385
nicholavila <nicholavila1@outlook.com>
2023-10-04 15:58:03 +0000

    feat: new server action update password

---
+++ actions/auth/update-password.ts
@@ -0,0 +1,52 @@
+"use server";
+
+import * as z from "zod";
+import bcrypt from "bcryptjs";
+
+import { PasswordChangeSchema } from "@/schemas/auth/auth";
+import { updateUserPassword } from "@/data/user/password-update";
+import { getUserIdFromToken } from "@/lib/tokens";
+import { getUserById } from "@/data/user/user-by-id";
+
+export const updatePassword = async (
+  userId: string,
+  values: z.infer<typeof PasswordChangeSchema>
+) => {
+  const validatedFields = PasswordChangeSchema.safeParse(values);
+  if (!validatedFields.success) {
+    return { error: "Invalid fields!" };
+  }
+
+  const existingUser = await getUserById(userId);
+  if (!existingUser) {
+    return { error: "Internal server error!" };
+  }
+
+  const { password, newPassword } = validatedFields.data;
+
+  const passwordsMatch = await bcrypt.compare(
+    password,
+    existingUser.password as string
+  );
+
+  if (!passwordsMatch) {
+    return { error: "Please input correct current password." };
+  }
+
+  const hashedPassword = await bcrypt.hash(newPassword, 10);
+  const updatedUser = await updateUserPassword({
+    userId,
+    password: hashedPassword,
+    emailVerified: new Date(existingUser.emailVerified as string)
+  });
+
+  if (!updatedUser) {
+    return {
+      error: "Internal server error!"
+    };
+  }
+
+  return {
+    success: "Password updated!"
+  };
+};
