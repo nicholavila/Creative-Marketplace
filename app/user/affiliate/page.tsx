366265a9e5fa74a0e9e260d5226fc3fe489bafef
nicholavila <nicholavila1@outlook.com>
2023-10-06 13:00:58 +0000

    update: affiliate settings page

--- app/user/affiliate/page.tsx
+++ app/user/affiliate/page.tsx
@@ -1,17 +1,65 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useTransition } from "react";
 import { SwitchBox } from "@/components/utils/switch-box";
+import { useAtom } from "jotai";
+import { userAtom } from "@/store/user";
+import { AffiliateData, User } from "@/shared/types/user.type";
 
 const AffiliateSettings = () => {
-  const [isChecked] = useState<boolean>(false);
+  const [user, setUser] = useAtom(userAtom);
+  const [isPending, startTransition] = useTransition();
+  const [isChecked, setIsChecked] = useState<boolean>(
+    user?.customer?.isCustomer || false
+  );
+
+  const onSwitch = () => {
+    if (isPending) return;
+
+    const newState = !isChecked;
+    let affiliateData = user?.affiliate;
+
+    if (newState && !affiliateData) {
+      affiliateData = {
+        affiliateId: user?.userId as string,
+        isAffiliate: true
+      };
+    }
+
+    affiliateData = {
+      ...affiliateData,
+      isAffiliate: newState
+    } as AffiliateData;
+
+    startTransition(() => {
+      updateAffiliateData({
+        userId: user?.userId as string,
+        affiliateData
+      }).then((res) => {
+        if (res) {
+          setIsChecked(newState);
+          setUser({
+            ...user,
+            affiliate: affiliateData
+          } as User);
+        }
+      });
+    });
+  };
 
   return (
     <main className="w-full pl-8 flex flex-col gap-y-5">
       <SwitchBox
-        title="Start an Affiliate's life"
-        content="You can join as an affiliate and get an extra benefits"
+        disabled={isPending}
+        title="Affiliate Profile"
+        content="you can turn on or off your affiliate profile"
         isChecked={isChecked}
+        onCheckedChange={onSwitch}
+        alertTitle={
+          isChecked
+            ? "Are you sure to close customer account?"
+            : "Are you sure to create customer account?"
+        }
       />
     </main>
   );
