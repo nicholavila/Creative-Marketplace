75451e9cef2f852c6d4a842d8dbe885797acdb9a
nicholavila <nicholavila1@outlook.com>
2023-10-06 09:43:29 +0000

    update: customer settings page

--- app/user/customer/page.tsx
+++ app/user/customer/page.tsx
@@ -2,30 +2,38 @@
 
 import { SwitchBox } from "@/components/utils/switch-box";
 import EditCustomer from "@/components/profile/edit-customer";
-import { useState } from "react";
+import { useState, useTransition } from "react";
+import { useAtom } from "jotai";
+import { userAtom } from "@/store/user";
 
 const CustomerSettings = () => {
-  const [isChecked, setIsChecked] = useState<boolean>(false);
+  const [user, setUser] = useAtom(userAtom);
+  const [isPending, startTransition] = useTransition();
+  const [isChecked, setIsChecked] = useState<boolean>(
+    user?.customer?.isCustomer || false
+  );
 
   const onSwitch = () => {
-    setIsChecked(!isChecked);
-  }
+    if (isPending) return;
+  };
 
   return (
     <main className="w-full pl-8 pb-6 flex flex-col gap-y-6">
       <SwitchBox
-        title="Join as a Customer"
-        content="You can join as a customer and expore millions of creative works"
+        disabled={isPending}
+        title="Customer Profile"
+        content="you can turn on or off your creator profile"
         isChecked={isChecked}
         onCheckedChange={onSwitch}
-        alertTitle={isChecked ?
-          "Are you sure to close customer account?" :
-          "Are you sure to create customer account?"
+        alertTitle={
+          isChecked
+            ? "Are you sure to close customer account?"
+            : "Are you sure to create customer account?"
         }
       />
       <EditCustomer disabled={!isChecked} />
     </main>
-  )
+  );
 };
 
 export default CustomerSettings;
