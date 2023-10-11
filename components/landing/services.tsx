c08a73dd95a3142a8d58e6ed08fafb855770f7ab
nicholavila <nicholavila1@outlook.com>
2023-10-11 15:56:25 +0000

    fix: Image tag in service component

--- components/landing/services.tsx
+++ components/landing/services.tsx
@@ -75,6 +75,8 @@ export const Services = () => (
               alt="..."
               src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80"
               className="w-full align-middle rounded-t-lg"
+              width={1051}
+              height={701}
             />
             <blockquote className="relative p-8">
               <svg
