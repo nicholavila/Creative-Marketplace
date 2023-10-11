420b2cd381bb5ac6226947308c48d7e5cde07009
nicholavila <nicholavila1@outlook.com>
2023-10-11 12:36:22 +0000

    fix: eslint issue

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
