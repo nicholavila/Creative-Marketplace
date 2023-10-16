555d86bedf369240080e1a02bdeaeab1a6f99185
nicholavila <nicholavila1@outlook.com>
2023-10-16 12:43:25 +0000

    update: path for component

--- components/product/preview-card.tsx
+++ components/product/preview-card.tsx
@@ -1,5 +1,5 @@
 import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
-import { ImagePreview } from "./image-preview";
+import { ImagePreview } from "./preview-image";
 
 type Props = {
   previewFiles: File[];
