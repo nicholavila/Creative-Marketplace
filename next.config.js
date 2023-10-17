7f09e34a609070fcc4803293415039b1e1bc2005
nicholavila <nicholavila1@outlook.com>
2023-10-17 12:13:47 +0000

    update: add s3 bucket domain

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   images: {
-    domains: ["images.unsplash.com"]
+    domains: [
+      "images.unsplash.com",
+      "kre8tive-bucket-apne1-20240425.s3.us-east-1.amazonaws.com"
+    ]
   }
 };
