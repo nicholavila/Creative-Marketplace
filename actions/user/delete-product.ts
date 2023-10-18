59cdffbe05585ef0191c2c84ade2845f971e48b1
nicholavila <nicholavila1@outlook.com>
2023-10-18 10:25:00 +0000

    feat: delete product from user

---
+++ actions/user/delete-product.ts
@@ -0,0 +1,35 @@
+"use server";
+
+import { getUserById, updateUserProducts } from "@/data/user";
+import { ProductLink } from "@/shared/types/product.type";
+
+export const deleteProductFromCreator = async (
+  userId: string,
+  productId: string
+) => {
+  const existingUser = await getUserById(userId);
+  if (!existingUser || !existingUser.email) {
+    return { error: "Internal server error" };
+  }
+
+  if (!existingUser.creator) {
+    return { error: "User is not a creator" };
+  }
+
+  const creatorData = existingUser.creator;
+  creatorData.products = creatorData.products || [];
+  creatorData.products = creatorData.products.filter(
+    (item) => item.productId !== productId
+  );
+
+  const response = await updateUserProducts({
+    userId,
+    creator: creatorData
+  });
+
+  if (response) {
+    return { success: "New product was registered successfully" };
+  } else {
+    return { error: "Internal server error" };
+  }
+};
