de739b7ef34f5197b385cc6077a344184c47ff07
nicholavila <nicholavila1@outlook.com>
2023-10-19 13:17:46 +0000

    update: preview card

--- components/product/preview-card.tsx
+++ components/product/preview-card.tsx
@@ -4,26 +4,39 @@ import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 import { Button } from "../ui/button";
 import { ImagePreview } from "./preview-image";
 import { FileOrString } from "@/shared/types/file-preview-types";
+import { Dispatch, SetStateAction, useRef, useState } from "react";
+import { Input } from "../ui/input";
+import { PreviewDialog } from "./preview-dialog";
 
 type Props = {
-  previewFiles: FileOrString[];
   isPending: boolean;
-  onPreviewFile: (index: number) => void;
-  onDeletePreviewFile: (index: number) => void;
-  onPreviewFileBrowse: () => void;
+  previewFiles: FileOrString[];
+  setPreviewFiles: Dispatch<SetStateAction<FileOrString[]>>;
 };
 
 export const PreviewCard = ({
-  previewFiles,
   isPending,
-  onPreviewFileBrowse,
-  onPreviewFile,
-  onDeletePreviewFile
+  previewFiles,
+  setPreviewFiles
 }: Props) => {
   return (
     <Card className="w-full">
+      {/** Preview is not working with images whose width < height  */}
+      <PreviewDialog
+        isPreviewing={isPreviewing}
+        setPreviewing={setPreviewing}
+        image={previewFiles[previewIndex as number]}
+      />
       <CardHeader className="flex-row items-center justify-between space-y-0">
         <CardTitle>Preview Images</CardTitle>
+        <Input
+          className="hidden"
+          type="file"
+          accept="image/*"
+          multiple
+          ref={hiddenPreviewInput}
+          onChange={onPreviewFileAdded}
+        />
         <Button
           type="button"
           className="gap-x-2 border-green-700"
