82a0b3e86c4df225568a56ca1b1c953e9ed47f5b
nicholavila <nicholavila1@outlook.com>
2023-10-19 08:38:02 +0000

    feat: link from s3

---
+++ actions/s3/link-from-s3-server.ts
@@ -0,0 +1,21 @@
+"use server";
+
+import { GetObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
+import s3Client from "@/lib/s3";
+import { AWS_S3_BUCKETS } from "@/shared/constants/server.constant";
+
+export const getLinkFromS3Server = async (keyName: string) => {
+  const command = new GetObjectCommand({
+    Bucket: AWS_S3_BUCKETS.UPLOAD,
+    Key: keyName
+  });
+
+  try {
+    const response = await getSignedUrl(s3Client, command, { expiresIn: 3600 });
+    return { success: true, response };
+  } catch (error) {
+    return { success: false, error };
+  }
+};
