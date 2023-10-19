88d93029e9422c342da7b9f4f2c70378880f2dfc
nicholavila <nicholavila1@outlook.com>
2023-10-19 14:41:32 +0000

    chore: clean up product-add-form

--- components/product/product-add-form.tsx
+++ components/product/product-add-form.tsx
@@ -118,221 +118,30 @@ export const ProductAddForm = () => {
           publish soon!
         </CardDescription>
       </CardHeader>
-
-      <CardContent className="grid grid-cols-2 gap-6">
-        <div className="">
-          <Card className="mb-6 w-full">
-            <CardHeader className="flex-row items-center justify-between space-y-0">
-              <CardTitle>Creative Works</CardTitle>
-              <Button
-                type="button"
-                className="gap-x-2 border-green-700"
-                variant="outline"
-                size="sm"
-                disabled={isPending}
-                onClick={onCreativeFileBrowse}
-              >
-                <FaFileUpload />
-                Upload
-              </Button>
-            </CardHeader>
-            <CardContent>
-              <div className="min-h-32 flex flex-wrap gap-4">
-                {creativeFiles.map((file, index) => (
-                  <Badge
-                    key={index}
-                    className="h-8 flex gap-x-2 px-4 rounded-full"
-                  >
-                    <p>{file.name}</p>
-                    <Button
-                      // asChild
-                      disabled={isPending}
-                      variant="link"
-                      className="p-0 text-base text-black cursor-pointer"
-                      onClick={() => onDeleteCreativeFile(index)}
-                    >
-                      <MdClose />
-                    </Button>
-                  </Badge>
-                ))}
-                {creativeFiles.length === 0 && (
-                  <div className="w-full flex items-center justify-center">
-                    <p>No Creative Files selected</p>
-                  </div>
-                )}
-              </div>
-            </CardContent>
-          </Card>
-          <PreviewCard
-            previewFiles={previewFiles}
+      <CardContent className="w-full flex gap-x-6">
+        <div className="w-1/2 flex flex-col gap-x-6">
+          <FilesCard
             isPending={isPending}
-            onPreviewFileBrowse={onPreviewFileBrowse}
-            onPreviewFile={onPreviewFile}
-            onDeletePreviewFile={onDeletePreviewFile}
+            creativeFiles={creativeFiles}
+            setCreativeFiles={setCreativeFiles}
+          />
+          <PreviewCard
+            isPending={isPending}
+            previewFiles={previewFiles}
+            setPreviewFiles={setPreviewFiles}
           />
         </div>
-
-        <Form {...form}>
-          <form className="space-y-4" onSubmit={form.handleSubmit(onSubmit)}>
-            <FormField
-              control={form.control}
-              name="productType"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Product Type</FormLabel>
-                  <Select
-                    disabled={isPending}
-                    onValueChange={field.onChange}
-                    defaultValue={field.value}
-                  >
-                    <FormControl>
-                      <SelectTrigger>
-                        <SelectValue placeholder="Select a product type" />
-                      </SelectTrigger>
-                    </FormControl>
-                    <SelectContent>
-                      {Object.entries(PRODUCT_TYPE_DISPLAY_TEXT).map(
-                        ([productType, text]) => (
-                          <SelectItem key={productType} value={productType}>
-                            {text}
-                          </SelectItem>
-                        )
-                      )}
-                    </SelectContent>
-                  </Select>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="title"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Title for Product</FormLabel>
-                  <FormControl>
-                    <Input
-                      {...field}
-                      disabled={isPending}
-                      placeholder="Product Name"
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="description"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Description for Product</FormLabel>
-                  <FormControl>
-                    <Textarea
-                      {...field}
-                      disabled={isPending}
-                      placeholder="Product Description..."
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="price"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Price for Product</FormLabel>
-                  <FormControl>
-                    <Input
-                      {...field}
-                      disabled={isPending}
-                      placeholder="Product Price"
-                      type="number"
-                      onChange={(event) => field.onChange(+event.target.value)}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormItem>
-              <FormLabel>Keywords</FormLabel>
-              <FormDescription>
-                You can set as many keywords as you want to improve chance of
-                your product to be found out
-              </FormDescription>
-              <div className="w-full grid grid-cols-[1fr_auto] gap-4">
-                <Input
-                  disabled={isPending}
-                  className=""
-                  type="text"
-                  value={newKeywordVal}
-                  onChange={(e) => setNewKeywordVal(e.target.value)}
-                />
-                <Button
-                  disabled={isPending}
-                  type="button"
-                  variant="link"
-                  className="flex gap-x-2 text-sm"
-                  onClick={onAddNewKeyword}
-                >
-                  <FaPlus />
-                  Add
-                </Button>
-
-                <div className="flex flex-wrap gap-2 col-span-2">
-                  {selectedKeywords.map((keyword, index) => (
-                    <Badge
-                      key={keyword}
-                      className="h-8 flex gap-x-2 px-2 rounded-full"
-                    >
-                      <p>{keyword}</p>
-                      <Button
-                        // asChild
-                        disabled={isPending}
-                        variant="link"
-                        className="p-0 text-base text-black cursor-pointer"
-                        onClick={() => onDeleteKeyword(index)}
-                      >
-                        <MdClose />
-                      </Button>
-                    </Badge>
-                  ))}
-                </div>
-              </div>
-            </FormItem>
-            <div className="w-full flex gap-x-4">
-              <Input
-                className="hidden"
-                type="file"
-                // accept="image/*"
-                multiple
-                ref={hiddenCreativeFileInput}
-                onChange={onCreativeFileAdded}
-              />
-              <Input
-                className="hidden"
-                type="file"
-                accept="image/*"
-                multiple
-                ref={hiddenPreviewInput}
-                onChange={onPreviewFileAdded}
-              />
-            </div>
-            <Button
-              disabled={isPending}
-              type="submit"
-              className="w-full gap-x-2"
-            >
-              <FaPlus />
-              Register
-            </Button>
-            <FormError message={error} />
-            <FormSuccess message={success} />
-          </form>
-        </Form>
+        <div className="w-1/2 flex">
+          <DetailsCard
+            isPending={isPending}
+            form={form}
+            selectedKeywords={selectedKeywords}
+            setSelectedKeywords={setSelectedKeywords}
+            onSubmit={onSubmit}
+            error={error}
+            success={success}
+          />
+        </div>
       </CardContent>
     </Card>
   );
