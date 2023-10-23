a294abea291e938bdf1a0a2b962b2f25621efa6e
nicholavila <nicholavila1@outlook.com>
2023-10-23 12:59:40 +0000

    update: product image

--- components/bundles/product-image.tsx
+++ components/bundles/product-image.tsx
@@ -2,6 +2,8 @@ import { getLinkFromS3 } from "@/actions/s3/link-from-s3";
 import { Product } from "@/shared/types/product.type";
 import { useEffect, useState } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { useAtom } from "jotai";
+import { s3LinkAtom } from "@/store/s3-link";
 
 type Props = {
   product: Product;
@@ -9,9 +11,10 @@ type Props = {
 
 export const ProductImage = ({ product }: Props) => {
   const [image, setImage] = useState<string>("");
+  const [s3Link, setS3Link] = useAtom(s3LinkAtom);
 
   useEffect(() => {
-    getLinkFromS3(product.previewList[0]).then((res) => {
+    getLinkFromS3(product.previewList[0], s3Link, setS3Link).then((res) => {
       if (res.success) {
         setImage(res.response as string);
       }
