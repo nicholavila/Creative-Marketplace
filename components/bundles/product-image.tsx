c37088113ba20af666e7271f184c770bcf83ea40
nicholavila <nicholavila1@outlook.com>
2023-10-24 13:31:43 +0000

    fix: product-image

--- components/bundles/product-image.tsx
+++ components/bundles/product-image.tsx
@@ -14,12 +14,14 @@ export const ProductImage = ({ product }: Props) => {
   const [s3Link, setS3Link] = useAtom(s3LinkAtom);
 
   useEffect(() => {
+    if (!product.previewList || !s3Link || !setS3Link) return;
+
     getLinkFromS3(product.previewList[0], s3Link, setS3Link).then((res) => {
       if (res.success) {
         setImage(res.response as string);
       }
     });
-  }, [product.previewList]);
+  }, [product.previewList, s3Link, setS3Link]);
 
   return (
     <Avatar className="w-72 h-48 rounded-xl">
