ec31aac1e10e181e2ec388dbc5d368045001633a
nicholavila <nicholavila1@outlook.com>
2023-10-25 10:36:37 +0000

    update: product image

--- components/bundles/product-image.tsx
+++ components/bundles/product-image.tsx
@@ -1,9 +1,7 @@
-import { getLinkFromS3 } from "@/actions/s3/link-from-s3";
 import { Product } from "@/shared/types/product.type";
 import { useEffect, useState } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
-import { useAtom } from "jotai";
-import { s3LinkAtom } from "@/store/s3-link";
+import { useLinkFromS3 } from "@/hooks/use-link-from-s3";
 
 type Props = {
   product: Product;
@@ -11,17 +9,17 @@ type Props = {
 
 export const ProductImage = ({ product }: Props) => {
   const [image, setImage] = useState<string>("");
-  const [s3Link, setS3Link] = useAtom(s3LinkAtom);
+  const { getLinkFromS3 } = useLinkFromS3();
 
   useEffect(() => {
-    if (!product.previewList || !s3Link || !setS3Link) return;
+    if (!product.previewList) return;
 
-    getLinkFromS3(product.previewList[0], s3Link, setS3Link).then((res) => {
+    getLinkFromS3(product.previewList[0]).then((res) => {
       if (res.success) {
         setImage(res.response as string);
       }
     });
-  }, [product.previewList, s3Link, setS3Link]);
+  }, [product.previewList]);
 
   return (
     <Avatar className="w-72 h-48 rounded-xl">
