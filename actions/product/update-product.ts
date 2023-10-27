1a3c014173a35d76780b55d5d7e977585b7994b8
nicholavila <nicholavila1@outlook.com>
2023-10-27 13:20:42 +0000

    fix: update product

--- actions/product/update-product.ts
+++ actions/product/update-product.ts
@@ -1,35 +1,38 @@
 "use server";
 
 import { createProduct, deleteProduct } from "@/data/product";
-import { Product } from "@/shared/types/product.type";
+import { Product, ProductLink } from "@/shared/types/product.type";
 import { addNewProduct } from "../user/new-product";
 import { deleteProductFromCreator } from "../user/delete-product";
 
-export const updateProduct = async (product: Product) => {
-  const res_create = await createProduct(product);
+export const updateProduct = async (
+  newProduct: Product,
+  oldProduct: ProductLink
+) => {
+  const res_create = await createProduct(newProduct);
   if (!res_create.success) {
     return { error: "Failed to update product" };
   }
 
   const res_delete = await deleteProduct(
-    product.productType,
-    product.productId
+    oldProduct.productType,
+    oldProduct.productId
   );
   if (res_delete.error) {
     return { error: "Failed to remove original product" };
   }
 
-  const res_add = await addNewProduct(product.ownerId, {
-    productType: product.productType,
-    productId: product.productId
+  const res_add = await addNewProduct(newProduct.ownerId, {
+    productType: newProduct.productType,
+    productId: newProduct.productId
   });
   if (res_add.error) {
     return { error: "Failed to add new product to creator" };
   }
 
   const res_remove = await deleteProductFromCreator(
-    product.ownerId,
-    product.productId
+    newProduct.ownerId,
+    oldProduct.productId
   );
   if (res_remove.error) {
     return { error: "Failed to remove original product from creator" };
