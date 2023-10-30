6a80376739ac8c1fff2f3007a4b48c8f8cfea5b6
nicholavila <nicholavila1@outlook.com>
2023-10-30 10:13:59 +0000

    update: product history

--- components/product/product-history.tsx
+++ components/product/product-history.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { Button } from "../ui/button";
 import { ProductEventCard } from "./product-event";
 
 import type { ProductEvent } from "@/shared/types/product.type";
@@ -7,11 +9,31 @@ type Props = {
 };
 
 export const ProductHistory = ({ history }: Props) => {
+  const _history = history.toReversed();
+
+  const [showMore, setShowMore] = useState<boolean>(false);
+
+  const onShowMore = () => {
+    setShowMore((prev) => !prev);
+  };
+
   return (
     <div className="w-full flex flex-col gap-y-4">
-      {history.map((event, index) => (
-        <ProductEventCard key={index} event={event} />
-      ))}
+      <div className="w-full flex items-end justify-between">
+        <ProductEventCard event={_history[0]} />
+        <Button
+          variant={"outline"}
+          className="h-8 border border-green-700 rounded-none"
+          onClick={onShowMore}
+        >
+          Show {showMore ? "Less" : "More"}
+        </Button>
+      </div>
+      <div className="w-full flex flex-col gap-y-4">
+        {_history.map((event, index) =>
+          index > 0 ? <ProductEventCard key={index} event={event} /> : null
+        )}
+      </div>
     </div>
   );
 };
