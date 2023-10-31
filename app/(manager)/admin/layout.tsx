a1344873f844a5d465998c8f761b306866ad10cf
nicholavila <nicholavila1@outlook.com>
2023-10-31 12:50:19 +0000

    update: admin layout

--- app/(manager)/admin/layout.tsx
+++ app/(manager)/admin/layout.tsx
@@ -4,9 +4,11 @@ import { SideNavBar } from "@/components/admin/utils/side-navbar";
 
 const AdminLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="w-full min-h-[calc(100vh-80px)] flex">
+    <div className="w-full h-fit flex">
       <SideNavBar />
-      <div className="w-full px-12 pt-6 pb-24">{children}</div>
+      <div className="w-full min-h-[calc(100vh-80px)] h-fit px-12 pt-6 pb-12">
+        {children}
+      </div>
     </div>
   );
 };
