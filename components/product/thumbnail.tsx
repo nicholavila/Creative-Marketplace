68f6a60cb535e1e453a483da0887e7e46afd21d3
nicholavila <nicholavila1@outlook.com>
2023-11-02 16:01:22 +0000

    rearrange: thumbnail component

---
+++ components/product/thumbnail.tsx
@@ -0,0 +1,23 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+export const Thumbnail = (props: {
+  path: string;
+  focused: boolean;
+  onItemSelected: () => void;
+}) => {
+  return (
+    <div
+      onMouseEnter={props.onItemSelected}
+      className={`w-28 h-16 border-[2px] cursor-pointer ${props.focused && "border-green-700"}`}
+    >
+      <Avatar
+        className={`w-full h-full rounded-none border-[1px] border-white`}
+      >
+        <AvatarImage src={props.path} className="object-center object-fill" />
+        <AvatarFallback className="bg-sky-500">
+          <div className="w-full h-full bg-inherit"></div>
+        </AvatarFallback>
+      </Avatar>
+    </div>
+  );
+};
