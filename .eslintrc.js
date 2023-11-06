e0aa50612cd02c1ae0c96aadaab595b363648552
nicholavila <nicholavila1@outlook.com>
2023-11-06 11:36:05 +0000

    chore: update eslint for import order

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,9 @@ module.exports = {
   extends: [
     "next/core-web-vitals",
     "eslint:recommended",
-    "plugin:@typescript-eslint/recommended"
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript"
   ],
   root: true,
   parser: "@typescript-eslint/parser",
@@ -14,6 +16,33 @@ module.exports = {
   rules: {
     "no-unused-vars": "off",
     "@typescript-eslint/no-unused-vars": ["error"],
-    "react-hooks/exhaustive-deps": "off"
+    "react-hooks/exhaustive-deps": "off",
+    "no-duplicate-imports": "error",
+    "sort-imports": [
+      "error",
+      { ignoreCase: true, ignoreDeclarationSort: true }
+    ],
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+          "object",
+          "type"
+        ],
+        alphabetize: { order: "asc" },
+        "newlines-between": "always-and-inside-groups"
+      }
+    ]
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {}
+    }
   }
 };
