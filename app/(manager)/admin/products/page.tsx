30d5d624f23c2df9a3b6897db65f9912ecf1698b
nicholavila <nicholavila1@outlook.com>
2023-11-07 09:34:41 +0000

    update: products page of admin panel

--- app/(manager)/admin/products/page.tsx
+++ app/(manager)/admin/products/page.tsx
@@ -36,6 +36,7 @@ import { getAllSubmittedProducts } from "@/data/product";
 import { Navbar } from "../_components/navbar";
 
 import type { Product, ProductLink } from "@/shared/types/product.type";
+import { ProductsTable } from "@/components/admin/products-table";
 
 const ROWS_PER_PAGE = 10;
 
@@ -117,130 +118,7 @@ export default function Approval() {
         title="All Products"
         content="You can check and approve or reject products"
       />
-      <div className="w-full flex flex-col gap-y-4">
-        <div className="flex items-center gap-x-4">
-          <div className="flex gap-x-4">
-            <Input
-              disabled={isPending}
-              placeholder="Filter Creator"
-              value={
-                (table.getColumn("ownerId")?.getFilterValue() as string) ?? ""
-              }
-              onChange={(event) =>
-                table.getColumn("ownerId")?.setFilterValue(event.target.value)
-              }
-              className="max-w-xs"
-            />
-            <Input
-              disabled={isPending}
-              placeholder="Filter Product"
-              value={
-                (table.getColumn("title")?.getFilterValue() as string) ?? ""
-              }
-              onChange={(event) =>
-                table.getColumn("title")?.setFilterValue(event.target.value)
-              }
-              className="max-w-xs"
-            />
-          </div>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="outline" className="ml-auto">
-                Columns <ChevronDown className="ml-2 h-4 w-4" />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              {table
-                .getAllColumns()
-                .filter((column) => column.getCanHide())
-                .map((column) => (
-                  <DropdownMenuCheckboxItem
-                    key={column.id}
-                    className="capitalize"
-                    checked={column.getIsVisible()}
-                    onCheckedChange={(value) =>
-                      column.toggleVisibility(!!value)
-                    }
-                  >
-                    {column.id}
-                  </DropdownMenuCheckboxItem>
-                ))}
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </div>
-        <div className="rounded-md border">
-          <Table>
-            <TableHeader>
-              {table.getHeaderGroups().map((headerGroup) => (
-                <TableRow key={headerGroup.id}>
-                  {headerGroup.headers.map((header) => (
-                    <TableHead key={header.id}>
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(
-                            header.column.columnDef.header,
-                            header.getContext()
-                          )}
-                    </TableHead>
-                  ))}
-                </TableRow>
-              ))}
-            </TableHeader>
-            <TableBody>
-              {table.getRowModel().rows?.length ? (
-                table.getRowModel().rows.map((row) => (
-                  <TableRow
-                    key={row.id}
-                    data-state={row.getIsSelected() && "selected"}
-                  >
-                    {row.getVisibleCells().map((cell) => (
-                      <TableCell key={cell.id}>
-                        {flexRender(
-                          cell.column.columnDef.cell,
-                          cell.getContext()
-                        )}
-                      </TableCell>
-                    ))}
-                  </TableRow>
-                ))
-              ) : (
-                <TableRow>
-                  <TableCell
-                    colSpan={columns.length}
-                    className="h-24 text-center"
-                  >
-                    No results.
-                  </TableCell>
-                </TableRow>
-              )}
-            </TableBody>
-          </Table>
-        </div>
-        <div className="flex items-center justify-end space-x-2 py-4">
-          <div className="flex-1 text-sm text-muted-foreground">
-            {table.getFilteredSelectedRowModel().rows.length} of{" "}
-            {table.getFilteredRowModel().rows.length} row(s) selected.
-          </div>
-          <div className="space-x-2">
-            <Button
-              variant="outline"
-              size="sm"
-              onClick={() => table.previousPage()}
-              disabled={!table.getCanPreviousPage() || isPending}
-            >
-              Previous
-            </Button>
-            <Button
-              variant="outline"
-              size="sm"
-              onClick={onNext}
-              disabled={!isNextAvailable || isPending}
-            >
-              Next
-            </Button>
-          </div>
-        </div>
-      </div>
+      <ProductsTable />
     </main>
   );
 }
