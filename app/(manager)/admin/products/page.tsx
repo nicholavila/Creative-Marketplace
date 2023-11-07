cf2cbbac50ad20a77310e096b27380c759fa998c
nicholavila <nicholavila1@outlook.com>
2023-11-07 11:16:54 +0000

    update: products page of admin

--- app/(manager)/admin/products/page.tsx
+++ app/(manager)/admin/products/page.tsx
@@ -4,6 +4,8 @@ import { getAllSubmittedProducts } from "@/data/product";
 
 import { Navbar } from "../_components/navbar";
 import { ProductsTable } from "@/components/admin/products-table";
+import { Tabs, TabsList } from "@/components/ui/tabs";
+import { TabsTrigger } from "@radix-ui/react-tabs";
 
 export default function Approval() {
   return (
@@ -12,6 +14,19 @@ export default function Approval() {
         title="All Products"
         content="You can check and approve or reject products"
       />
+      <Tabs defaultValue="Submitted">
+        <TabsList className="w-full h-fit flex gap-x-16">
+          <TabsTrigger value="Submitted">
+            <p className="text-base">Submitted</p>
+          </TabsTrigger>
+          <TabsTrigger value="Rejected">
+            <p className="text-base">Rejected</p>
+          </TabsTrigger>
+          <TabsTrigger value="Submitted">
+            <p className="text-base">Submitted</p>
+          </TabsTrigger>
+        </TabsList>
+      </Tabs>
       <ProductsTable getProductsAll={getAllSubmittedProducts} />
     </main>
   );
