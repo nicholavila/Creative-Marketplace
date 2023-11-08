5f1855d2e7239591a5e531254f2d2917f5d5cd05
nicholavila <nicholavila1@outlook.com>
2023-11-08 11:32:38 +0000

    fix: update product

--- actions/product/update-product.ts
+++ actions/product/update-product.ts
@@ -5,38 +5,35 @@ import { Product, ProductLink } from "@/shared/types/product.type";
 import { addNewProduct } from "../user/new-product";
 import { deleteProductFromCreator } from "../user/delete-product";
 
-export const updateProduct = async (
-  newProduct: Product,
-  oldProduct: ProductLink
-) => {
-  const res_create = await createProduct(newProduct);
-  if (!res_create.success) {
-    return { error: "Failed to update product" };
-  }
-
+export const updateProduct = async (product: Product) => {
   const res_delete = await deleteProduct(
-    oldProduct.productType,
-    oldProduct.productId
+    product.productType,
+    product.productId
   );
   if (res_delete.error) {
     return { error: "Failed to remove original product" };
   }
 
-  const res_add = await addNewProduct(newProduct.ownerId, {
-    productType: newProduct.productType,
-    productId: newProduct.productId
-  });
-  if (res_add.error) {
-    return { error: "Failed to add new product to creator" };
+  const res_create = await createProduct(product);
+  if (!res_create.success) {
+    return { error: "Failed to update product" };
   }
 
-  const res_remove = await deleteProductFromCreator(
-    newProduct.ownerId,
-    oldProduct.productId
-  );
-  if (res_remove.error) {
-    return { error: "Failed to remove original product from creator" };
-  }
+  // const res_add = await addNewProduct(product.ownerId, {
+  //   productType: product.productType,
+  //   productId: product.productId
+  // });
+  // if (res_add.error) {
+  //   return { error: "Failed to add new product to creator" };
+  // }
+
+  // const res_remove = await deleteProductFromCreator(
+  //   product.ownerId,
+  //   product.productId
+  // );
+  // if (res_remove.error) {
+  //   return { error: "Failed to remove original product from creator" };
+  // }
 
   return { success: "Product updated successfully" };
 };
