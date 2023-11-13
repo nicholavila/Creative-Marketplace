aac31e89e12dc793b0e569a2aba9bbb126991675
nicholavila <nicholavila1@outlook.com>
2023-11-13 13:53:33 +0000

    update: edit product

--- app/(creator)/creator/edit-product/[productType]/[productId]/page.tsx
+++ app/(creator)/creator/edit-product/[productType]/[productId]/page.tsx
@@ -5,6 +5,8 @@ import { useEffect, useState } from "react";
 import { Loading } from "@/app/_components/loading";
 import { ProductEditForm } from "@/components/creator/products/product-edit-form";
 
+import { FaRecycle, FaSave, FaUpload } from "react-icons/fa";
+
 import { getProductById } from "@/data/product";
 import { Product, ProductLink } from "@/shared/types/product.type";
 
@@ -24,6 +26,65 @@ const ProductEdit = ({ params }: { params: ProductLink }) => {
 
   return (
     <main className="w-full p-6">
+      <CardHeader className="w-full flex flex-row items-end justify-between">
+        <div className="flex flex-col">
+          <CardTitle className="text-2xl font-medium">Edit a Product</CardTitle>
+          <CardDescription>
+            You can edit your product and our admin users will check it and
+            publish soon!
+          </CardDescription>
+        </div>
+        <div className="flex gap-x-6 items-end">
+          <Button
+            disabled={isPending}
+            variant={"outline"}
+            className="w-64 gap-x-4 rounded-none border-green-700"
+            onClick={form.handleSubmit(() => onSubmit("updated"))}
+          >
+            <FaSave />
+            Update Product
+          </Button>
+
+          {isApproved ? (
+            <Button
+              disabled={isPending}
+              className="w-64 gap-x-4 rounded-none"
+              onClick={onPublish}
+            >
+              <FaUpload />
+              Publish
+            </Button>
+          ) : (
+            <Button
+              disabled={isPending}
+              className="w-64 gap-x-4 rounded-none"
+              onClick={form.handleSubmit(() =>
+                onSubmit(isResubmitted ? "resubmitted" : "submitted")
+              )}
+            >
+              <FaUpload />
+              {isResubmitted ? "Resubmit" : "Submit"}
+            </Button>
+          )}
+
+          {!isEverSubmitted && (
+            <QuestionAlert
+              title="Confirmation"
+              message="Are you sure want to delete this item?"
+              onContinue={onDelete}
+            >
+              <Button
+                disabled={isPending}
+                variant={"destructive"}
+                className="w-64 gap-x-4 rounded-none border-green-700"
+              >
+                <FaRecycle />
+                Delete Product
+              </Button>
+            </QuestionAlert>
+          )}
+        </div>
+      </CardHeader>
       <ProductEditForm product={product} setProduct={setProduct} />
     </main>
   );
