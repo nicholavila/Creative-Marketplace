c3025c9e785594403ec9ab7f262ab6f604c7cf2a
nicholavila <nicholavila1@outlook.com>
2023-11-13 10:32:05 +0000

    fix: import path

--- app/(creator)/creator/edit-product/[productType]/[productId]/page.tsx
+++ app/(creator)/creator/edit-product/[productType]/[productId]/page.tsx
@@ -3,9 +3,9 @@
 import { useEffect, useState } from "react";
 
 import { Loading } from "@/app/_components/loading";
-import { ProductEditForm } from "@/components/product/product-edit-form";
 import { getProductById } from "@/data/product";
 import { Product, ProductLink } from "@/shared/types/product.type";
+import { ProductEditForm } from "@/components/creator/products/product-edit-form";
 
 const ProductEdit = ({ params }: { params: ProductLink }) => {
   const [product, setProduct] = useState<Product>();
