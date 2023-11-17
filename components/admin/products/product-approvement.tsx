dd30fdc7d922c075fce450353c1965367e2bf5f5
nicholavila <nicholavila1@outlook.com>
2023-11-17 08:38:51 +0000

    update: product approvement form

--- components/admin/products/product-approvement.tsx
+++ components/admin/products/product-approvement.tsx
@@ -3,6 +3,7 @@ import { Dispatch, SetStateAction } from "react";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
 import { QuestionAlert } from "@/components/utils/question-alert";
+import { Card } from "@/components/ui/card";
 
 type Props = {
   isPending: boolean;
@@ -18,37 +19,46 @@ export const ProductApprovement = ({
   onCommentProduct
 }: Props) => {
   return (
-    <div className="w-full flex flex-col gap-y-4">
+    <Card className="w-full p-6 flex flex-col gap-y-4">
       <p className="text-2xl font-semibold">Product Approvement</p>
-      <div className="flex flex-col gap-y-1">
-        <p>Your Comment</p>
-        <Textarea
-          disabled={isPending}
-          value={comment}
-          onChange={(e) => setComment(e.target.value)}
-          className="w-1/2"
-        />
+      <div className="w-full flex items-end justify-between gap-x-8">
+        <div className="w-full flex flex-col gap-y-1">
+          <p>Your Comment</p>
+          <Textarea
+            disabled={isPending}
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+          />
+        </div>
+        <div className="flex items-center gap-x-4">
+          <QuestionAlert
+            title="Approve Product"
+            message="Are you sure to approve this product?"
+            onContinue={() => onCommentProduct(true)}
+          >
+            <Button
+              disabled={isPending}
+              variant={"default"}
+              className="w-32 rounded-none"
+            >
+              Approve
+            </Button>
+          </QuestionAlert>
+          <QuestionAlert
+            title="Reject Product"
+            message="Are you sure to reject this product?"
+            onContinue={() => onCommentProduct(false)}
+          >
+            <Button
+              disabled={isPending}
+              variant={"destructive"}
+              className="w-32 rounded-none"
+            >
+              Reject
+            </Button>
+          </QuestionAlert>
+        </div>
       </div>
-      <div className="flex items-center gap-x-4">
-        <QuestionAlert
-          title="Approve Product"
-          message="Are you sure to approve this product?"
-          onContinue={() => onCommentProduct(true)}
-        >
-          <Button disabled={isPending} variant={"default"}>
-            Approve
-          </Button>
-        </QuestionAlert>
-        <QuestionAlert
-          title="Reject Product"
-          message="Are you sure to reject this product?"
-          onContinue={() => onCommentProduct(false)}
-        >
-          <Button disabled={isPending} variant={"destructive"}>
-            Reject
-          </Button>
-        </QuestionAlert>
-      </div>
-    </div>
+    </Card>
   );
 };
