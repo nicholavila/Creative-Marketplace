c16d33c78963e0c484fe18eb9c58eb46aecc8e00
nicholavila <nicholavila1@outlook.com>
2023-11-20 10:12:17 +0000

    feat: remove from s3

---
+++ actions/s3/remove-file.ts
@@ -0,0 +1,20 @@
+"use server";
+
+import { DeleteObjectCommand } from "@aws-sdk/client-s3";
+
+import s3Client from "@/lib/s3";
+import { AWS_S3_BUCKETS } from "@/shared/constants/server.constant";
+
+export const removeFileFromS3 = async (keyName: string) => {
+  const command = new DeleteObjectCommand({
+    Bucket: AWS_S3_BUCKETS.UPLOAD,
+    Key: keyName
+  });
+
+  try {
+    const response = await s3Client.send(command);
+    return { success: true, response };
+  } catch (error) {
+    return { success: false, error };
+  }
+};
