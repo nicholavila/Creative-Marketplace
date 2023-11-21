3c285299cdf7e56b549c06a487e0f1f15dc25493
nicholavila <nicholavila1@outlook.com>
2023-11-21 08:20:52 +0000

    feat: add social link

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,11 +1,15 @@
 "use client";
 
+import Image from "next/image";
 import Link from "next/link";
+
+import { Input } from "@/components/ui/input";
+import { GradientButton } from "@/components/utils/gradient-button";
 
 export const Footer = () => {
   return (
     <footer className="w-full flex flex-col">
-      <div className="w-full px-14 py-6 rounded-[40px] flex bg-white/30">
+      <div className="w-full px-14 py-6 rounded-[40px] flex justify-between bg-white/30">
         <div className="flex flex-col gap-y-6">
           <p className="text-lg font-semibold text-[]">Links</p>
           <div className="flex gap-x-12 text-sm text-[#3054B0] font-bold">
@@ -22,6 +26,104 @@ export const Footer = () => {
               <Link href="/">Latest News</Link>
               <Link href="/">Careers</Link>
               <Link href="/">For Creators</Link>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col gap-y-6">
+          <p className="text-lg font-semibold text-[]">Trusted & Secure</p>
+          <div className="flex gap-x-12">
+            <div className="flex flex-col gap-y-3">
+              <Image
+                src="./footer/trustmark_x2F_engagement_x2F_certified.svg"
+                width={121}
+                height={51}
+                alt="TrustedSite"
+              />
+            </div>
+            <div className="flex flex-col gap-y-3">
+              <Image
+                src="./footer/trustpilot-logo.svg"
+                width={96}
+                height={46}
+                alt="TrustPilot"
+              />
+              <Image
+                src="./footer/Norton Secure Seal 1.svg"
+                width={96}
+                height={46}
+                alt="TrustPilot"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col gap-y-16">
+          <div className="flex flex-col gap-y-6">
+            <p className="text-lg font-semibold text-[]">Join Our Newsletter</p>
+            <div className="flex items-center gap-x-3">
+              <Image
+                src="./footer/newsletter.svg"
+                width={46}
+                height={35}
+                alt="newsletter"
+              />
+              <Input
+                className="w-60 h-[35px] bg-[#E6CCE8]"
+                placeholder="Enter your email address"
+              />
+              <GradientButton label="Subscribe" onClick={() => {}} />
+            </div>
+          </div>
+          <div className="flex flex-col gap-y-6">
+            <p className="text-lg font-semibold text-[]">Social</p>
+            <div className="flex items-center gap-x-16">
+              <Link href="/">
+                <Image
+                  src="./social/linkedin.svg"
+                  width={26}
+                  height={26}
+                  alt="linkedin"
+                />
+              </Link>
+              <Link href="/">
+                <Image
+                  src="./social/x.svg"
+                  width={27}
+                  height={27}
+                  alt="linkedin"
+                />
+              </Link>
+              <Link href="/">
+                <Image
+                  src="./social/facebook.svg"
+                  width={26}
+                  height={26}
+                  alt="linkedin"
+                />
+              </Link>
+              <Link href="/">
+                <Image
+                  src="./social/youtube.svg"
+                  width={29}
+                  height={20}
+                  alt="linkedin"
+                />
+              </Link>
+              <Link href="/">
+                <Image
+                  src="./social/instagram.svg"
+                  width={25}
+                  height={25}
+                  alt="linkedin"
+                />
+              </Link>
+              <Link href="/">
+                <Image
+                  src="./social/pinterest.svg"
+                  width={24}
+                  height={24}
+                  alt="linkedin"
+                />
+              </Link>
             </div>
           </div>
         </div>
