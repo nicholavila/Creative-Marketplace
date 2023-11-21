d1fe56fb8c439391e6dda65cc98c38f607ddcc2d
nicholavila <nicholavila1@outlook.com>
2023-11-21 10:26:21 +0000

    fix: background color

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,19 @@
 "use client";
 
-import { Poppins } from "next/font/google";
 import React from "react";
 
 import { CreatorSignUp } from "@/components/landing/creator-singup";
 import { Featured } from "@/components/landing/featured";
 import { Finisher } from "@/components/landing/finisher";
 import { Hero } from "@/components/landing/hero";
-
 import { MemberSignUp } from "@/components/landing/member-signup";
-import { cn } from "@/lib/utils";
 
 import { Footer } from "./_components/footer";
 
-const font = Poppins({
-  subsets: ["latin"],
-  weight: ["600"]
-});
-
 const Home = () => {
   return (
-    <>
-      <main className={cn("w-full", font.className)}>
+    <div className="w-full landing-bg">
+      <main>
         <Hero />
         <section className="w-full flex flex-col gap-y-6">
           <MemberSignUp />
@@ -31,7 +23,7 @@ const Home = () => {
         <Finisher />
       </main>
       <Footer />
-    </>
+    </div>
   );
 };
 
