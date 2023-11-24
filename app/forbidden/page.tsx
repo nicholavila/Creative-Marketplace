38586d49cc1d9fc79e138c91db7ad02026c16a68
nicholavila <nicholavila1@outlook.com>
2023-11-24 09:21:24 +0000

    fix: forbidden page

--- app/forbidden/page.tsx
+++ app/forbidden/page.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 
 const NotFound = () => {
   return (
-    <div className="w-full h-full flex flex-col items-center justify-center">
+    <div className="w-full h-full pt-12 flex flex-col items-center justify-center">
       <h2 className="font-semibold">You are forbidden to access this page.</h2>
       <Button variant={"link"}>
         <Link href="/">Go back home</Link>
