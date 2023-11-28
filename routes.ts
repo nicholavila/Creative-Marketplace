1ae5dace8857b60f053fbf4b994cd04ca0aafefe
nicholavila <nicholavila1@outlook.com>
2023-11-28 15:46:16 +0000

    fix: update login redirect route

--- routes.ts
+++ routes.ts
@@ -29,4 +29,4 @@ export const apiAuthPrefix = "/api/auth";
  * The default redirect path after logging in
  * @type {string}
  */
-export const DEFAULT_LOGIN_REDIRECT = "/products";
+export const DEFAULT_LOGIN_REDIRECT = "/onboarding";
