86d7b5029d4fc9490b07ab950d4a4b06b68d412a
nicholavila <nicholavila1@outlook.com>
2023-11-29 11:12:48 +0000

    fix: add onboarding page state

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -1,9 +1,68 @@
 "use client";
 
+import { useState } from "react";
+
 import { TransitionInOut } from "@/components/utils/transition-in-out";
 
 const OnboardingPage = () => {
   // affiliate, creator, user forms
+
+  const [step, setStep] = useState<number>(0);
+
+  const moveStepForward = () => {
+    setStep(step + 1);
+  };
+
+  const moveStepBackward = () => {
+    setStep(step - 1);
+  };
+
+  const isCreatorStep = () => {
+    return userData.selectedAccounts.creator && step === 2;
+  };
+
+  const isMatchingStep = () => {
+    return userData.selectedAccounts.creator && step === 3;
+  };
+
+  const isCreatorCompleteStep = () => {
+    return userData.selectedAccounts.creator && step === 4;
+  };
+
+  const isUserStep = () => {
+    let _step = 2;
+    if (userData.selectedAccounts.creator) {
+      _step += 3;
+    }
+
+    return userData.selectedAccounts.user && step === _step;
+  };
+
+  const isAffiliateStep = () => {
+    let _step = 2;
+    if (userData.selectedAccounts.creator) {
+      _step += 3;
+    }
+    if (userData.selectedAccounts.user) {
+      _step += 1;
+    }
+
+    return userData.selectedAccounts.affiliate && step === _step;
+  };
+
+  const isRegisterCompleteStep = () => {
+    let _step = 2;
+    if (userData.selectedAccounts.creator) {
+      _step += 3;
+    }
+    if (userData.selectedAccounts.user) {
+      _step += 1;
+    }
+    if (userData.selectedAccounts.affiliate) {
+      _step += 1;
+    }
+    return step === _step;
+  };
 
   return (
     <div className="w-[640px]">
