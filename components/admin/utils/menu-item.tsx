9b4133debad3f5463b2d60a3278cd85d9f971410
nicholavila <nicholavila1@outlook.com>
2023-12-01 14:03:41 +0000

    update: menu item

--- components/admin/utils/menu-item.tsx
+++ components/admin/utils/menu-item.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useMemo } from "react";
 
 type Props = {
   link: string;
@@ -7,12 +9,19 @@ type Props = {
 };
 
 export const MenuItem = ({ link, icon, title }: Props) => {
+  const pathname = usePathname();
+
+  const isSelected = useMemo(() => {
+    return pathname === link;
+  }, [pathname]);
+
   return (
     <Link href={link}>
       <div
-        className="w-full flex items-center gap-x-4 px-4 py-2 rounded-md cursor-pointer
+        className={`w-full flex items-center gap-x-4 px-4 py-2 my-2 rounded-md cursor-pointer
         text-gray-700 text-base drop-shadow-md
-        hover:bg-sky-100 hover:text-sky-500 transition-all duration-200 ease-in-out"
+        hover:bg-sky-100 hover:text-sky-500 transition-all duration-200 ease-in-out
+        ${isSelected ? "bg-sky-200 text-sky-500" : ""}`}
       >
         {icon}
         {title}
