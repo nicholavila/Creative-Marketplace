3f7dc13d84b6670e2212afc0ffc5b6d773a8a06a
nicholavila <nicholavila1@outlook.com>
2023-12-07 13:07:51 +0000

    chore: clean up

--- actions/product/update-product.ts
+++ actions/product/update-product.ts
@@ -6,29 +6,19 @@ import { Product } from "@/shared/types/product.type";
 export const updateProduct = async (product: Product) => {
   const res_delete = await deleteProduct(product);
   if (res_delete.error) {
-    return { error: "Failed to remove original product" };
+    return {
+      error: "Failed to remove original product"
+    };
   }
 
   const res_create = await createProduct(product);
   if (res_create.error) {
-    return { error: "Failed to update product" };
+    return {
+      error: "Failed to update product"
+    };
   }
 
-  // const res_add = await addNewProduct(product.ownerId, {
-  //   productType: product.productType,
-  //   productId: product.productId
-  // });
-  // if (res_add.error) {
-  //   return { error: "Failed to add new product to creator" };
-  // }
-
-  // const res_remove = await deleteProductFromCreator(
-  //   product.ownerId,
-  //   product.productId
-  // );
-  // if (res_remove.error) {
-  //   return { error: "Failed to remove original product from creator" };
-  // }
-
-  return { success: "successful" };
+  return {
+    success: "successful"
+  };
 };
