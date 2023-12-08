32933c8e69ddbf227a8e2cd8228695dd337d3cd7
nicholavila <nicholavila1@outlook.com>
2023-12-08 13:34:40 +0000

    feat: add w8 ind/cor radio group

--- components/auth/register/w8-form.tsx
+++ components/auth/register/w8-form.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+
 export const W8Form = () => {
   return (
     <div className="w-full">
@@ -7,6 +10,21 @@ export const W8Form = () => {
         As you have indicated that you are are not a US person, please complete
         the Form W-8 below which will be stored securely.
       </p>
+
+      <RadioGroup>
+        <div className="flex items-center gap-x-4 cursor-pointer">
+          <RadioGroupItem value="true" id="us" />
+          <Label htmlFor="individual" className="cursor-pointer">
+            An individual
+          </Label>
+        </div>
+        <div className="flex items-center gap-x-4 cursor-pointer">
+          <RadioGroupItem value="false" id="notUs" />
+          <Label htmlFor="corporation" className="cursor-pointer">
+            A Corporation
+          </Label>
+        </div>
+      </RadioGroup>
     </div>
   );
 };
