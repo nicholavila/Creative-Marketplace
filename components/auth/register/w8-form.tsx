d32dbb1c278f3ed033272bc9a3532baee2c47fdf
nicholavila <nicholavila1@outlook.com>
2023-12-08 12:52:48 +0000

    fix: update w8 form base

---
+++ components/auth/register/w8-form.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+export const W8Form = () => {
+  return (
+    <div className="w-full">
+      <p className="t-body -size-m mb-8">
+        We can only accept letters, numbers and special characters &amp; - , / #
+        ( ) . Please use the English equivalent of accented characters, eg. a
+        for à.
+      </p>
+    </div>
+  );
+};
