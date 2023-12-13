707bc6feb141facf89af5d61a955ec6462ebd3a6
nicholavila <nicholavila1@outlook.com>
2023-12-13 10:12:17 +0000

    fix: update w8 corporation component

--- components/auth/register/w8-form.tsx
+++ components/auth/register/w8-form.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import { useAtom } from "jotai";
 import { useState } from "react";
 
 import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { userAtom } from "@/store/user";
 
 import { W8CorporationForm } from "./w8-corporation-form";
 import { W8IndividualForm } from "./w8-individual-form";
@@ -17,6 +19,10 @@ type Props = {
 };
 
 export const W8Form = (props: Props) => {
+  const [user] = useAtom(userAtom);
+
+  console.log(user);
+
   const [registerType, setRegisterType] = useState("");
 
   return (
